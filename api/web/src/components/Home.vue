f93409f1d0dabb7402d03c59beaf53d64bc6fb15
ingalls <nick@ingalls.ca>
2022-11-26 16:03:20 -0700

    Setup ETL UI

---
+++ api/web/src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+    <div class='page-wrapper'>
+        <div class="page-header d-print-none">
+            <div class="container-xl">
+                <div class="row g-2 align-items-center">
+                    <div class="col">
+                        <h2 class="page-title">Home</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class='page-body'>
+        <div class='container-xl'>
+            <div class='row row-deck row-cards'>
+                <div class="col-lg-12">
+               </div>
+            </div>
+        </div>
+    </div>
+
+    <PageFooter/>
+</div>
+</template>
+
+<script>
+import PageFooter from './PageFooter.vue';
+
+export default {
+    name: 'Home',
+    data: function() {
+        return {
+            err: false,
+        }
+    },
+    components: {
+        PageFooter,
+    }
+}
+</script>
---
+++ api/web/src/components/util/Modal.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class='z2 fixed top right bottom left scroll-auto bg-darken50 shadow-darken25'>
+        <div class='flex flex--center-main pt36'>
+            <div class='flex-child bg-white round relative w600'>
+                <button @click='$emit("close")' class='absolute top right px12 py12'>
+                    <svg class='icon link color-darken50'><use xlink:href='#icon-close'></use></svg>
+                </button>
+
+                <div class='col col--12 grid px12 py12'>
+                    <slot></slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Modal',
+}
+</script>
