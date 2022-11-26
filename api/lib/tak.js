d9ba18f1cc1807d203229f02fc38b659cd9ba438
ingalls <nick@ingalls.ca>
2022-11-25 17:05:48 -0700

    Continue to sketch out client connection

--- api/lib/tak.js
+++ api/lib/tak.js
@@ -1,11 +1,50 @@
-export default class TAK {
-    constructor() {
+import EventEmitter from 'events';
+import tls from 'tls';
 
+export default class TAK extends EventEmitter {
+    constructor(type, opts) {
+        this.type = type;
+        this.opts = opts;
+
+        if (type === 'ssl') {
+
+        } else {
+            throw new Error('Unknown Connection Type');
+        }
     }
 
-    static connect() {
-        const tak = new TAK();
+    static connect_ssl(url, cert, key) {
+        if (!(url instanceof URL)) throw new Error('SSL url must be URL instance');
+        if (typeof cert !== 'string') throw new Error('Cert must be a String');
+        if (typeof key !== 'string') throw new Error('Key must be a String');
 
-        return tak;
+        const tak = new TAK('ssl', { url, cert, key });
+
+        return new Promise((resolve, reject) => {
+            tak.client = tls.connect({
+                rejectUnauthorized: false,
+                host: url.host,
+                port: url.port,
+                cert,
+                key
+            }, () => {
+                if (!tak.client.authorized) throw tak.client.authorizationError;
+            });
+
+            let buff;
+            tak.client.on('data', (data) => {
+                // Eventually Parse ProtoBuf
+                data = buff + data.toString();
+            });
+
+            tak.client.on('error', (err) => { tak.emit('error', err); })
+            tak.client.on('close', () => { tak.emit('close'); })
+
+            return resolve(tak);
+        });
+    }
+
+    write() {
+
     }
 }
