16795c5bf75708d5e8039977cbbe78616df825e9
ingalls <nick@ingalls.ca>
2022-11-30 16:37:34 -0700

    Add Connection Components

---
+++ api/web/src/components/Connections.vue
@@ -0,0 +1,118 @@
+<template>
+<div>
+    <div class='page-wrapper'>
+        <div class="page-header d-print-none">
+            <div class="container-xl">
+                <div class="row g-2 align-items-center">
+                    <div class="col d-flex">
+                        <ol class="breadcrumb" aria-label="breadcrumbs">
+                            <li class="breadcrumb-item" aria-current="page"><a @click='$router.push("/")' class='cursor-pointer'>Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page"><a href="#">Connections</a></li>
+                        </ol>
+
+                        <div class='ms-auto'>
+                            <div class='btn-list'>
+                                <a @click='query.shown = !query.shown' class="cursor-pointer btn btn-secondary">
+                                    <SearchIcon/>
+                                </a>
+
+                                <a @click='$router.push("/connection/new")' class="cursor-pointer btn btn-primary">
+                                    New Connection
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class='page-body'>
+        <div class='container-xl'>
+            <div class='row row-deck row-cards'>
+                <div v-if='query.shown' class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <label class="form-label">Connection Search</label>
+                            <div class="input-icon mb-3">
+                                <input v-text='query.search' type="text" value="" class="form-control" placeholder="Search…">
+                                <span class="input-icon-addon">
+                                    <SearchIcon/>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div :key='connection.id' v-for='connection in list.connections' class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <span class="status-indicator status-green status-indicator-animated">
+                                  <span class="status-indicator-circle"></span>
+                                  <span class="status-indicator-circle"></span>
+                                  <span class="status-indicator-circle"></span>
+                            </span>
+
+                            <a @click='$router.push(`/connetion/${connection.id}`)' class="card-title cursor-pointer" v-text='connection.name'></a>
+
+                            <div class='ms-auto'>
+                                <div class='btn-list'>
+                                    <SettingsIcon class='cursor-pointer' @click='$router.push(`/connection/${connection.id}/edit`)'/>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body" v-text='connection.description'>
+                        </div>
+                        <div class="card-footer">
+                            Last updated 3 mins ago
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <PageFooter/>
+</div>
+</template>
+
+<script>
+import PageFooter from './PageFooter.vue';
+import {
+    SettingsIcon,
+    SearchIcon
+} from 'vue-tabler-icons'
+
+export default {
+    name: 'Connections',
+    data: function() {
+        return {
+            err: false,
+            query: {
+                shown: false,
+                search: ''
+            },
+            list: {
+                connections: []
+            }
+        }
+    },
+    mounted: function() {
+        this.fetchList();
+    },
+    methods: {
+        fetchList: async function() {
+            try {
+                this.list = await window.std('/api/connection');
+            } catch (err) {
+                this.err = err;
+            }
+        }
+    },
+    components: {
+        SettingsIcon,
+        SearchIcon,
+        PageFooter,
+    }
+}
+</script>
