6a5f01ed4cc5166c2b6eda857ef07752c52c0064
ingalls <nick@ingalls.ca>
2022-12-05 07:45:40 -0700

    Add single layer component

--- api/web/src/main.js
+++ api/web/src/main.js
@@ -16,6 +16,7 @@ const router = new VueRouter.createRouter({
 
         { path: '/layer', name: 'layers', component: () => import('./components/Layers.vue') },
         { path: '/layer/new', name: 'layer-new', component: () => import('./components/LayerNew.vue') },
+        { path: '/layer/:layerid', name: 'layer', component: () => import('./components/Layer.vue') },
 
         { path: '/connection', name: 'connections', component: () => import('./components/Connections.vue') },
         { path: '/connection/new', name: 'connection-new', component: () => import('./components/ConnectionEdit.vue') },
