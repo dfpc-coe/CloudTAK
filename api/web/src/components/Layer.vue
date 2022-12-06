40853625e907423eb0988f621631c082755dd93c
ingalls <nick@ingalls.ca>
2022-12-06 10:19:13 -0700

    Get Last Updated working from actual Layer data

--- api/web/src/components/Layer.vue
+++ api/web/src/components/Layer.vue
@@ -41,7 +41,7 @@
                         <div class="card-body" v-text='layer.description'>
                         </div>
                         <div class="card-footer">
-                            Last updated 3 mins ago
+                            Last updated <span v-text='timeDiff(layer.updated)'/>
                         </div>
                     </div>
                 </div>
@@ -76,6 +76,21 @@ export default {
         cronstr: function(cron) {
             if (!cron) return;
             return cronstrue.toString(cron);
+        },
+        timeDiff: function(updated) {
+            const msPerMinute = 60 * 1000;
+            const msPerHour = msPerMinute * 60;
+            const msPerDay = msPerHour * 24;
+            const msPerMonth = msPerDay * 30;
+            const msPerYear = msPerDay * 365;
+            const elapsed = +(new Date()) - updated;
+
+            if (elapsed < msPerMinute) return Math.round(elapsed/1000) + ' seconds ago';
+            if (elapsed < msPerHour) return Math.round(elapsed/msPerMinute) + ' minutes ago';
+            if (elapsed < msPerDay ) return Math.round(elapsed/msPerHour ) + ' hours ago';
+            if (elapsed < msPerMonth) return '~' + Math.round(elapsed/msPerDay) + ' days ago';
+            if (elapsed < msPerYear) return '~' + Math.round(elapsed/msPerMonth) + ' months ago';
+            return '~' + Math.round(elapsed/msPerYear ) + ' years ago';
         },
         fetch: async function() {
             try {
