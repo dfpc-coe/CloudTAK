ae522a2638e54871af833073fd77d69588942148
ingalls <nick@ingalls.ca>
2022-12-06 09:21:39 -0700

    Editable Layers Page

--- api/web/src/main.js
+++ api/web/src/main.js
@@ -15,8 +15,9 @@ const router = new VueRouter.createRouter({
         { path: '/', name: 'home', component: () => import('./components/Home.vue') },
 
         { path: '/layer', name: 'layers', component: () => import('./components/Layers.vue') },
-        { path: '/layer/new', name: 'layer-new', component: () => import('./components/LayerNew.vue') },
+        { path: '/layer/new', name: 'layer-new', component: () => import('./components/LayerEdit.vue') },
         { path: '/layer/:layerid', name: 'layer', component: () => import('./components/Layer.vue') },
+        { path: '/layer/:layerid/edit', name: 'layer-edit', component: () => import('./components/LayerEdit.vue') },
 
         { path: '/connection', name: 'connections', component: () => import('./components/Connections.vue') },
         { path: '/connection/new', name: 'connection-new', component: () => import('./components/ConnectionEdit.vue') },
