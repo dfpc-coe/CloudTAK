bd64be47105d49c8c4da232fe9a30e35b3cade28
ingalls <nick@ingalls.ca>
2022-12-09 08:02:55 -0700

    Add Geometry Type Selection

--- api/web/src/components/util/Styles.vue
+++ api/web/src/components/util/Styles.vue
@@ -2,6 +2,26 @@
 <div class='card'>
     <div class='card-body'>
         <div class='row'>
+            <div class="d-flex justify-content-center mb-4">
+                <div class="btn-list">
+                    <div class="btn-group" role="group">
+                        <input v-model='mode' type="radio" class="btn-check" name="btn-radio-toolbar" value='point'>
+                        <label @click='mode="point"' class="btn btn-icon px-3">
+                            <PointIcon/> Points
+                        </label>
+                        <input v-model='mode' type="radio" class="btn-check" name="btn-radio-toolbar" value='line'>
+                        <label @click='mode="line"' class="btn btn-icon px-3">
+                            <LineIcon/> Lines
+                        </label>
+                        <input v-model='mode' type="radio" class="btn-check" name="btn-radio-toolbar" value='polygon'>
+                        <label @click='mode="polygon"' class="btn btn-icon px-3">
+                            <PolygonIcon/> Polygons
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+
             <div class='col-md-6'>
                 <label class="form-label">Color Input</label>
                 <div class="row g-2">
@@ -42,16 +62,25 @@
 import {
     Input
 } from '@tak-ps/vue-tabler'
+import {
+    PointIcon,
+    LineIcon,
+    PolygonIcon
+} from 'vue-tabler-icons'
 
 export default {
     name: 'StyleUtil',
     data: function() {
         return {
+            mode: 'point',
             remarks: ''
         };
     },
     components: {
-        TablerInput: Input
+        TablerInput: Input,
+        PointIcon,
+        LineIcon,
+        PolygonIcon
     }
 }
 </script>
