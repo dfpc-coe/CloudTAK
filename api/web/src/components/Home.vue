2e26d0eba6ad775ca17f886b31a725cbdd13832a
ingalls <nick@ingalls.ca>
2022-12-09 09:11:08 -0700

    Add Layer Card

--- api/web/src/components/Home.vue
+++ api/web/src/components/Home.vue
@@ -4,19 +4,25 @@
         <div class='container-xl'>
             <div class='row row-deck row-cards'>
                 <div class='col-sm-6 col-lg-3'>
-                    <CardConnections/>
+                    <CardConnections @err='err = $event'/>
+                </div>
+                <div class='col-sm-6 col-lg-3'>
+                    <CardLayers @err='err = $event'/>
                 </div>
             </div>
         </div>
     </div>
 
+    <Err v-if='err' :err='err' @close='err = null'/>
     <PageFooter/>
 </div>
 </template>
 
 <script>
+import { Err } from '@tak-ps/vue-tabler';
 import PageFooter from './PageFooter.vue';
 import CardConnections from './cards/Connections.vue';
+import CardLayers from './cards/Layers.vue';
 
 export default {
     name: 'Home',
@@ -26,8 +32,10 @@ export default {
         }
     },
     components: {
+        Err,
         PageFooter,
-        CardConnections
+        CardConnections,
+        CardLayers
     }
 }
 </script>
