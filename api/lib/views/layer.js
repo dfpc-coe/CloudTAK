833eb2c1d876a64cd73ab221add004dd636aaf5f
ingalls <nick@ingalls.ca>
2022-12-12 17:55:49 -0800

    Setup Download Layers

---
+++ api/lib/views/layer.js
@@ -0,0 +1,39 @@
+import Err from '@openaddresses/batch-error';
+import Generic, { Params } from '@openaddresses/batch-generic';
+import { sql } from 'slonik';
+
+export default class Layer extends Generic {
+    static _view = 'view_layers';
+
+    static async list(pool, query={}) {
+        query.limit = Params.integer(query.limit, { default: 100 });
+        query.page = Params.integer(query.page, { default: 0 });
+        query.sort = Params.string(query.sort, { default: 'created' });
+        query.order = Params.order(query.order);
+        query.filter = Params.string(query.filter, { default: '' });
+        query.connection = Params.integer(query.connection);
+
+        try {
+            const pgres = await pool.query(sql`
+                SELECT
+                    count(*) OVER() AS count,
+                    *
+                FROM
+                    ${sql.identifier([this._view])}
+                WHERE
+                    name ~* ${query.filter}
+                    AND (${query.connection}::BIGINT IS NULL OR ${query.connection}::BIGINT = connection)
+                ORDER BY
+                    id DESC
+                LIMIT
+                    ${query.limit}
+                OFFSET
+                    ${query.limit * query.page}
+            `);
+
+            return this.deserialize_list(pgres, 'layers');
+        } catch (err) {
+            throw new Err(500, err, 'Failed to list layers');
+        }
+    }
+}
