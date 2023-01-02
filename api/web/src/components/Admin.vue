6cba12bc372f5ac79604a3b43bea5640f4dd67e1
ingalls <nick@ingalls.ca>
2023-01-02 11:30:45 -0400

    Setup Admin Vue Component

---
+++ api/web/src/components/Admin.vue
@@ -0,0 +1,60 @@
+<template>
+<div>
+    <div class='page-wrapper'>
+        <div class="page-header d-print-none">
+            <div class="container-xl">
+                <div class="row g-2 align-items-center">
+                    <div class="col d-flex">
+                        <ol class="breadcrumb" aria-label="breadcrumbs">
+                            <li class="breadcrumb-item" aria-current="page"><a @click='$router.push("/")' class='cursor-pointer'>Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page"><a href="#">Admin</a></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class='page-body'>
+        <div class='container-xl'>
+            <div class='row row-deck row-cards'>
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                        </div>
+                        <div class="card-body">
+                        </div>
+                        <div class="card-footer">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <PageFooter/>
+</div>
+</template>
+
+<script>
+import PageFooter from './PageFooter.vue';
+
+export default {
+    name: 'Admin',
+    data: function() {
+        return {
+            server: null
+        }
+    },
+    mounted: async function() {
+        await this.fetch();
+    },
+    methods: {
+        fetch: async function() {
+            this.server = await window.std(`/api/server`);
+        }
+    },
+    components: {
+        PageFooter
+    }
+}
+</script>
