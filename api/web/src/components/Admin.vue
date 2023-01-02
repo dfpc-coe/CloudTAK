d07fc01e40d89b83b47a544c73556e8f1d536385
ingalls <nick@ingalls.ca>
2023-01-02 11:54:56 -0400

    Setup Admin Server TAK Defails

--- api/web/src/components/Admin.vue
+++ api/web/src/components/Admin.vue
@@ -20,12 +20,32 @@
             <div class='row row-deck row-cards'>
                 <div class="col-lg-12">
                     <div class="card">
-                        <div class="card-header">
-                        </div>
-                        <div class="card-body">
-                        </div>
-                        <div class="card-footer">
-                        </div>
+                        <TablerLoading v-if='loading'/>
+                        <template v-else-if='!edit'>
+                            <div class="card-header">
+                                <h3 class='card-title'>TAK Server Configuration</h3>
+                                <div class='ms-auto'>
+                                    <div class='btn-list'>
+                                        <SettingsIcon class='cursor-pointer' @click='edit = true'/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <template v-if='edit'>
+                                    <TablerInput v-model='server.url' placeholder='ssl://'/>
+
+                                    <div class='btn'>
+                                        Save Server
+                                    </div>
+                                </template>
+                                <template v-else>
+
+                                </template>
+                            </div>
+                            <div v-if='server.updated' class="card-footer">
+                                <span v-text='`Last Updated: ${server.updated}`'/>
+                            </div>
+                        </template>
                     </div>
                 </div>
             </div>
@@ -37,24 +57,47 @@
 
 <script>
 import PageFooter from './PageFooter.vue';
+import {
+    TablerLoading,
+    TablerInput
+} from '@tak-ps/vue-tabler';
+import {
+    SettingsIcon
+} from 'vue-tabler-icons';
+import timeDiff from '../timediff.js';
 
 export default {
     name: 'Admin',
     data: function() {
         return {
-            server: null
+            edit: false,
+            loading: true,
+            server: {
+                id: null,
+                created: null,
+                updated: null,
+                url: ''
+            }
         }
     },
     mounted: async function() {
         await this.fetch();
     },
     methods: {
+        timeDiff: function(updated) {
+            return timeDiff(updated);
+        },
         fetch: async function() {
+            this.loading = true;
             this.server = await window.std(`/api/server`);
+            this.loading = false;
         }
     },
     components: {
-        PageFooter
+        PageFooter,
+        SettingsIcon,
+        TablerLoading,
+        TablerInput
     }
 }
 </script>
--- api/web/src/components/Layer.vue
+++ api/web/src/components/Layer.vue
@@ -76,6 +76,7 @@ import PageFooter from './PageFooter.vue';
 import cronstrue from 'cronstrue';
 import LayerData from './Layer/LayerData.vue';
 import Styles from './Layer/Styles.vue';
+import timeDiff from '../timediff.js';
 import {
     TablerLoading
 } from '@tak-ps/vue-tabler'
@@ -99,24 +100,12 @@ export default {
         await this.fetch();
     },
     methods: {
+        timeDiff(update) {
+            return timeDiff(update);
+        },
         cronstr: function(cron) {
             if (!cron) return;
             return cronstrue.toString(cron);
-        },
-        timeDiff: function(updated) {
-            const msPerMinute = 60 * 1000;
-            const msPerHour = msPerMinute * 60;
-            const msPerDay = msPerHour * 24;
-            const msPerMonth = msPerDay * 30;
-            const msPerYear = msPerDay * 365;
-            const elapsed = +(new Date()) - updated;
-
-            if (elapsed < msPerMinute) return Math.round(elapsed/1000) + ' seconds ago';
-            if (elapsed < msPerHour) return Math.round(elapsed/msPerMinute) + ' minutes ago';
-            if (elapsed < msPerDay ) return Math.round(elapsed/msPerHour ) + ' hours ago';
-            if (elapsed < msPerMonth) return '~' + Math.round(elapsed/msPerDay) + ' days ago';
-            if (elapsed < msPerYear) return '~' + Math.round(elapsed/msPerMonth) + ' months ago';
-            return '~' + Math.round(elapsed/msPerYear ) + ' years ago';
         },
         fetch: async function() {
             this.loading.layer = true;
