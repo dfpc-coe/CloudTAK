a4e1f6960760a7a3622d2513d92bf960bb1b625c
ingalls <nick@ingalls.ca>
2023-01-03 11:53:06 -0400

    Add Asset Bucket

--- cloudformation/lib/api.js
+++ cloudformation/lib/api.js
@@ -189,6 +189,7 @@ export default {
                         },
                         { Name: 'SigningSecret', Value: cf.sub('{{resolve:secretsmanager:${AWS::StackName}/api/secret:SecretString::AWSCURRENT}}') },
                         { Name: 'StackName', Value: cf.stackName },
+                        { Name: 'ASSET_BUCKET', Value: cf.ref('AssetBucket') },
                         { Name: 'TAK_USERNAME', Value: cf.ref('Username') },
                         { Name: 'TAK_PASSWORD', Value: cf.ref('Password') },
                         { Name: 'AWS_DEFAULT_REGION', Value: cf.region }
