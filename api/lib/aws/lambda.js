e6de9e1cde386ff4b5163d4a21c8fe2ca8bdc467
ingalls <nick@ingalls.ca>
2023-01-04 15:59:10 -0400

    Add Lambda Builder

---
+++ api/lib/aws/lambda.js
@@ -0,0 +1,63 @@
+import cf from '@mapbox/cloudfriend';
+
+/**
+ * @class
+ */
+class Lambda {
+    static cloudformation(config, task, cron) {
+        const name = `${config.StackName}-${task}`;
+
+        return {
+            Resources: {
+                ETLFunctionLogs: {
+                    Type: 'AWS::Logs::LogGroup',
+                    Properties: {
+                        LogGroupName: `/aws/lambda/${name}`,
+                        RetentionInDays: 7
+                    }
+                },
+                ETLEvents: {
+                    Type: 'AWS::Events::Rule',
+                    Properties: {
+                        Description: name,
+                        State: 'ENABLED',
+                        ScheduleExpression: cron,
+                        Targets: [{
+                            Id: 'TagWatcherScheduler',
+                            Arn: cf.getAtt('ETLFunction', 'Arn')
+                        }]
+                    }
+                },
+                ETLFunctionInvoke: {
+                    Type: 'AWS::Lambda::Permission',
+                    Properties: {
+                        FunctionName: cf.getAtt('ETLFunction', 'Arn'),
+                        Action: 'lambda:InvokeFunction',
+                        Principal: 'events.amazonaws.com',
+                        SourceArn: cf.getAtt('ETLEvents', 'Arn')
+                    }
+                },
+                ETLFunction: {
+                    Type: 'AWS::Lambda::Function',
+                    Properties: {
+                        FunctionName: name,
+                        MemorySize: 128,
+                        Timeout: 60,
+                        Description: name,
+                        PackageType: 'Image',
+                        Environment: {
+                            Variables: {
+                                TAK_API: 'me',
+                                TAK_TOKEN: 'me'
+                            }
+                        },
+                        Role: cf.importValue(config.StackName + '-etl-role'),
+                        Code: {
+                            ImageUri: cf.join([cf.accountId, '.dkr.ecr.', cf.region, `.amazonaws.com/coe-ecr-etl-tasks:${name}`])
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
