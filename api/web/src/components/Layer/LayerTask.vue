be55b1da2f543799efbbd0a72cc788bf674ba7cf
ingalls <nick@ingalls.ca>
2023-01-07 10:37:21 -0500

    Surface Task Status

---
+++ api/web/src/components/Layer/LayerTask.vue
@@ -0,0 +1,44 @@
+<template>
+<div class='card'>
+    <div class='card-header'>
+        <h3 class='card-title'>Task Status</h3>
+    </div>
+
+    <div class='card-body'>
+        <template v-if='loading'>
+            <TablerLoading/>
+        </template>
+        <template v-else-if='status === "destroyed"'>
+            <div class="d-flex justify-content-center mb-4">
+                Stack Hasn't Deployed
+            </div>
+        </template>
+    </div>
+</div>
+</template>
+
+<script>
+import {
+    TablerLoading
+} from '@tak-ps/vue-tabler';
+
+export default {
+    name: 'LayerData',
+    data: function() {
+        return {
+            status: {}
+        };
+    },
+    mounted: async function() {
+        await this.fetch();
+    },
+    methods: {
+        fetch: async function() {
+            this.status = await window.std(`/api/layer/${this.$route.params.layerid}/task`);
+        }
+    },
+    components: {
+        TablerLoading
+    }
+}
+</script>
