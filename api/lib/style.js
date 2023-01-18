22b50d5051cceb7b8ab30f35749753621c14f6c8
ingalls <nick@ingalls.ca>
2023-01-18 12:56:52 -0700

    Document & Lint Module

--- api/lib/style.js
+++ api/lib/style.js
@@ -1,29 +1,43 @@
+import jsonata from 'jsonata';
+
+/**
+ * Apply layer styling to CoT Messages
+ * @class
+ *
+ * @prop {Layer} layer  Layer object
+ */
 export class Style {
     constructor(layer) {
-        this.layer
+        this.layer = layer;
     }
 
-    feat(feat) {
+    /**
+     * Apply styling to a GeoJSON Feature in-place
+     *
+     * @param {Object} feature     GeoJSON Feature
+     * @returns {Object} GeoJSON Feature
+     */
+    async feat(feature) {
         if (this.layer.data.stale) {
             feature.properties.stale = this.layer.data.stale;
         }
 
-        if (!this.layer.enabled_styles) return feat;
+        if (!this.layer.enabled_styles) return feature;
 
         if (this.layer.styles.queries) {
             for (const q of this.layer.styles.queries) {
                 const expression = jsonata(q.query);
 
                 if (await expression.evaluate(feature) === true) {
-                    this.by_geom(feature);
+                    this.#by_geom(feature);
                 }
             }
         } else {
-            this.by_geom(this.layer.styles, feature);
+            this.#by_geom(this.layer.styles, feature);
         }
     }
 
-    by_geom(style, feature) {
+    #by_geom(style, feature) {
         if (feature.geometry.type === 'Point' && style.point) {
             Object.assign(feature.properties, style.point);
         } else if (feature.geometry.type === 'LineString' && style.line) {
