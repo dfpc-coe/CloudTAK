488436baf21be7fffc3718254c4db9af10688cc5
ingalls <nick@ingalls.ca>
2023-01-21 12:47:40 -0700

    Add Layer Status Icon

---
+++ api/web/src/components/Layer/Status.vue
@@ -0,0 +1,20 @@
+<template>
+<span class="status-indicator status-indicator-animated" :class='{
+    "status-green": connection.status === "healthy",
+    "status-red": connection.status === "alarm",
+    "status-dark": connection.status === "unknown",
+}'>
+      <span class="status-indicator-circle"></span>
+      <span class="status-indicator-circle"></span>
+      <span class="status-indicator-circle"></span>
+</span>
+</template>
+
+<script>
+export default {
+    name: 'LayerStatus',
+    props: {
+        layer: Object
+    }
+}
+</script>
