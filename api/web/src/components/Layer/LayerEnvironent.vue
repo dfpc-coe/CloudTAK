b3c7edc8146d02dabd7cf470bbdaff0614ff4b81
ingalls <nick@ingalls.ca>
2023-02-08 08:35:01 -0700

    Manual JSON Schema

--- api/web/src/components/Layer/LayerEnvironent.vue
+++ api/web/src/components/Layer/LayerEnvironent.vue
@@ -7,7 +7,8 @@
         </div>
     </div>
 
-    <div class="table-responsive">
+    <TablerLoading v-if='loading.schema' desc='Loading Environment'/>
+    <div v-else-if='mode === "list"' class="table-responsive">
         <table class="table table-vcenter card-table">
             <thead>
                 <tr>
@@ -40,12 +41,42 @@
             </div>
         </template>
     </div>
+    <div v-else-if='mode === "schema"' class="col">
+        <template v-if='schema.type !== "object"'>
+            <div class="d-flex justify-content-center my-4">
+                Only Object Schemas are Supported.
+            </div>
+        </template>
+        <template v-else>
+            <div :key='key' v-for='key in Object.keys(schema.properties)'>
+                <template v-if='schema.properties[key].type === "string" && schema.properties[key].enum'>
+                    <div class='row'>
+                        SELECT
+                    </div>
+                </template>
+                <template v-if='schema.properties[key].type === "string"'>
+                    <div class='row'>
+                        <TablerInput :label='key' v-model='environment[key]'/>
+                    </div>
+                </template>
+                <template v-if='schema.properties[key].type === "boolean"'>
+                    <div class='row'>
+                        <span class='px-2' v-text='key'></span>
+                        <label class="form-check form-switch">
+                            <input v-model='environment[key]' class="form-check-input" type="checkbox">
+                        </label>
+                    </div>
+                </template>
+            </div>
+        </template>
+    </div>
 </div>
 </template>
 
 <script>
 import {
-    TablerInput
+    TablerInput,
+    TablerLoading
 } from '@tak-ps/vue-tabler';
 import {
     PlusIcon,
@@ -58,10 +89,16 @@ export default {
             type: Object,
             required: true
         },
+        disabled: {
+            type: Boolean,
+            default: false
+        }
     },
     data: function() {
         return {
             environment: [],
+            mode: 'list',
+            schema: null,
             loading: {
                 schema: false
             },
@@ -75,20 +112,34 @@ export default {
                 for (const kv of this.environment) {
                     env[kv.key] = kv.value;
                 }
-                this.layerdata.environment = env;
+
+                this.$emit('update:modelValue', env);
             }
         },
     },
-    mounted: function() {
+    mounted: async function() {
+        await this.fetchSchema()
+
+        if (this.schema) {
+            this.mode = 'schema';
+
+        }
+
         this.environment = Object.keys(this.modelValue).map((key) => {
-            return { key: key, value: this.layerdata.environment[key] };
+            return { key: key, value: this.modelValue[key] };
         });
     },
     methods: {
+        fetchSchema: async function() {
+            this.loading.schema = true;
+            this.schema = (await window.std(`/api/layer/${this.$route.params.layerid}/task/schema`)).schema;
+            this.loading.schema = false;
+        }
     },
     components: {
         PlusIcon,
-        TablerInput
+        TablerInput,
+        TablerLoading,
     }
 }
 </script>
