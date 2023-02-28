602c8d562cb76cf4c02a1835e50accc018f8c771
ingalls <nick@ingalls.ca>
2023-02-28 13:43:35 -0800

    Setup schedule validity

--- api/lib/schedule.ts
+++ api/lib/schedule.ts
@@ -1,3 +1,5 @@
+import Err from '@openaddresses/batch-error';
+
 export interface ScheduleRate {
     unit: string;
     freq: number;
@@ -13,7 +15,7 @@ export default class Schedule {
     }
 
     static parse_rate(schedule: string): ScheduleRate {
-        if (!this.is_rate(schedule)) throw new Error('Schedule is not a rate');
+        if (!this.is_rate(schedule)) throw new Err(400, null, 'Schedule is not a rate');
 
         const schedules: string[] = schedule
             .trim()
@@ -22,16 +24,26 @@ export default class Schedule {
             .trim()
             .split(' ');
 
-        if (schedules.length !== 2) throw new Error('Rate expression is improperly formatted');
+        if (schedules.length !== 2) throw new Err(400, null, 'Rate expression is improperly formatted');
 
         const freq = parseInt(schedules[0]);
-        if (isNaN(freq)) throw new Error('Rate expression must start with frequency integer');
+        if (isNaN(freq)) throw new Err(400, null, 'Rate expression must start with frequency integer');
 
         const unit = schedules[1].replace(/s$/, '');
 
-        if (!['second', 'minute', 'hour', 'day'].includes(unit)) throw new Error('Unknown Unit');
+        if (!['second', 'minute', 'hour', 'day'].includes(unit)) throw new Err(400, null, 'Unknown Unit in Rate');
 
         return { unit, freq };
+    }
+
+    static is_valid(schedule: string): boolean {
+        if (this.is_cron(schedule)) {
+            return true;
+        } else if (this.is_rate(schedule)) {
+            this.parse_rate(schedule);
+        } else {
+            throw new Err(400, null, 'Unknown Schedule Type');
+        }
     }
 
     static is_aws(schedule: string): boolean {
