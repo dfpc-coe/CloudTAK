3bd84dcb40a647e139937e1a5719f9113a4e6356
ingalls <nick@ingalls.ca>
2023-03-24 17:25:28 -0600

    Tsify S3

---
+++ api/lib/aws/s3.ts
@@ -0,0 +1,140 @@
+import * as S3AWS from '@aws-sdk/client-s3';
+import Err from '@openaddresses/batch-error';
+import { Response } from 'express';
+import {
+    Stream
+} from 'node:stream';
+
+/**
+ * @class
+ */
+export default class S3 {
+    static async head(key: string) {
+        try {
+            if (!process.env.ASSET_BUCKET) throw new Err(400, null, 'ASSET_BUCKET not set');
+
+            const s3 = new S3AWS.S3Client({ region: process.env.AWS_DEFAULT_REGION });
+            const head = await s3.send(new S3AWS.HeadObjectCommand({
+                Bucket: process.env.ASSET_BUCKET,
+                Key: key
+            }));
+
+            return head;
+        } catch (err) {
+            throw new Err(500, new Error(err), 'Failed to head file');
+        }
+    }
+
+    static async put(key: string, stream: Stream) {
+        try {
+            if (!process.env.ASSET_BUCKET) throw new Err(400, null, 'ASSET_BUCKET not set');
+
+            const s3 = new S3AWS.S3Client({ region: process.env.AWS_DEFAULT_REGION });
+            await s3.send(new S3AWS.PutObjectCommand({
+                Bucket: process.env.ASSET_BUCKET,
+                Key: key,
+                Body: stream
+            }));
+        } catch (err) {
+            throw new Err(500, new Error(err), 'Failed to upload file');
+        }
+    }
+
+    static async exists(key: string) {
+        try {
+            if (!process.env.ASSET_BUCKET) throw new Err(400, null, 'ASSET_BUCKET not set');
+
+            const s3 = new S3AWS.S3Client({ region: process.env.AWS_DEFAULT_REGION });
+            await s3.send(new S3AWS.HeadObjectCommand({
+                Bucket: process.env.ASSET_BUCKET,
+                Key: key
+            }));
+            return true;
+        } catch (err) {
+            if (err.code === 'NotFound') return false;
+
+            throw new Err(500, new Error(err), 'Failed to determine existance');
+        }
+    }
+
+    /**
+     * List a key or prefix
+     *
+     * @param {string}  fragment             Key or Prefix to delete
+     */
+    static async list(fragment: string) {
+        try {
+            if (!process.env.ASSET_BUCKET) throw new Err(400, null, 'ASSET_BUCKET not set');
+
+            const s3 = new S3AWS.S3Client({ region: process.env.AWS_DEFAULT_REGION });
+            const list = await s3.send(new S3AWS.ListObjectsV2Command({
+                Bucket: process.env.ASSET_BUCKET,
+                Prefix: fragment
+            }));
+
+            return list.Contents;
+        } catch (err) {
+            throw new Err(500, new Error(err), 'Failed to list files');
+        }
+    }
+
+    /**
+     * Delete a key or prefix
+     *
+     * @param {string}  key             Key or Prefix to delete
+     * @param {object}  opts            Options
+     * @param {boolean} [opts.recurse]      Recursive Delete on key
+     */
+    static async del(key: string, opts = {
+        recurse: Boolean
+    }) {
+        if (!process.env.ASSET_BUCKET) return;
+        const s3 = new S3AWS.S3Client({ region: process.env.AWS_DEFAULT_REGION });
+
+        if (!opts.recurse) {
+            try {
+                await s3.send(new S3AWS.DeleteObjectCommand({
+                    Bucket: process.env.ASSET_BUCKET,
+                    Key: key
+                }));
+            } catch (err) {
+                throw new Err(500, new Error(err), 'Failed to delete file');
+            }
+        } else {
+            try {
+                const list = await this.list(key);
+
+                await s3.send(new S3AWS.DeleteObjectsCommand({
+                    Bucket: process.env.ASSET_BUCKET,
+                    Delete: {
+                        Objects: list.map((l) => {
+                            return {
+                                Key: l.Key
+                            };
+                        })
+                    }
+                }));
+            } catch (err) {
+                throw new Err(500, new Error(err), 'Failed to delete files');
+            }
+        }
+    }
+
+    static async stream(res: Response, name: string) {
+        const s3 = new S3AWS.S3Client({ region: process.env.AWS_DEFAULT_REGION });
+        const s3request = await s3.send(new S3AWS.GetObjectCommand(this.params));
+        const s3stream = s3request.Body;
+
+        s3request.on('httpHeaders', (statusCode: number, headers: object) => {
+            headers['Content-disposition'] = `inline; filename="${name}"`;
+            res.writeHead(statusCode, headers);
+        });
+
+        s3stream.on('error', (err: Error) => {
+            // Could not find object, ignore
+            console.error(err);
+        });
+
+        s3stream.pipe(res);
+    }
+}
