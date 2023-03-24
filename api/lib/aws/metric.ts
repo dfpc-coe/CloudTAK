535635e258cf6abe40ba21ee128fa8eed2879fb5
ingalls <nick@ingalls.ca>
2023-03-24 17:34:49 -0600

    Sketch out Connection Metrics

---
+++ api/lib/aws/metric.ts
@@ -0,0 +1,37 @@
+import Err from '@openaddresses/batch-error';
+import CloudWatch from '@aws-sdk/client-cloudwatch';
+
+/**
+ * @class
+ */
+export default class Metric {
+    stack: string;
+
+    constructor(stack: string) {
+        this.stack = stack;
+    }
+
+    async post(connid: number) {
+        const cw = new CloudWatch.CloudWatchClient({ region: process.env.AWS_DEFAULT_REGION });
+
+        try {
+            await cw.send(new CloudWatch.PutMetricDataCommand({
+                MetricData: [{
+                    MetricName: `ConnectionHealth`,
+                    Unit: 'Count'
+                    Value: 1,
+                    TimeStamp: new Date(),
+                    Dimensions: [{
+                        Name: 'ConnectionId',
+                        Value: String(connid)
+                    },{
+                        Name: 'StackName',
+                        Value: this.stack
+                    }]
+                }]
+            }));
+        } catch (err) {
+            throw new Err(500, new Error(err), 'Failed to push metric data');
+        }
+    }
+}
