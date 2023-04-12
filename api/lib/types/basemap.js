1c94ef188c570f7ba188ea70bbf401bf3d57354c
ingalls <nick@ingalls.ca>
2023-04-11 21:21:17 -0600

    Setup BaseMap Routes

---
+++ api/lib/types/basemap.js
@@ -0,0 +1,40 @@
+import Generic, { Params } from '@openaddresses/batch-generic';
+import { sql } from 'slonik';
+
+/**
+ * @class
+ */
+export default class BaseMap extends Generic {
+    static _table = 'connections';
+
+    static async list(pool, query={}) {
+        query.limit = Params.integer(query.limit, { default: 100 });
+        query.page = Params.integer(query.page, { default: 0 });
+        query.sort = Params.string(query.sort, { default: 'created' });
+        query.order = Params.order(query.order);
+        query.filter = Params.string(query.filter, { default: '' });
+
+        try {
+            const pgres = await pool.query(sql`
+                SELECT
+                    count(*) OVER() AS count,
+                    *
+                FROM
+                    ${sql.identifier([this._table])}
+                WHERE
+                    name ~* ${query.filter}
+                ORDER BY
+                    id DESC
+                LIMIT
+                    ${query.limit}
+                OFFSET
+                    ${query.limit * query.page}
+            `);
+
+            return this.deserialize_list(pgres);
+        } catch (err) {
+            throw new Err(500, err, 'Failed to list basemaps');
+        }
+    }
+
+}
