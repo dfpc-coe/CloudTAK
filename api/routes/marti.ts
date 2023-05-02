ea277ba2a6827465f7ed0e05a148ba9bd1c630b9
ingalls <nick@ingalls.ca>
2023-05-02 15:17:27 -0700

    Make marti call

---
+++ api/routes/marti.ts
@@ -0,0 +1,23 @@
+import Err from '@openaddresses/batch-error';
+import Auth from '../lib/auth.js';
+import Config from '../lib/config.js';
+import { Request, Response } from 'express';
+
+export default async function router(schema: any, config: Config) {
+    await schema.get('/marti/signClient', {
+        name: 'Sign Client',
+        group: 'Marti',
+        auth: 'user',
+        description: 'Helper API for obtaining a signed Certificate pair given LDAP Credentials',
+        body: 'req.body.MartiSignClient.json'
+        res: 'res.MartiSignClient.json'
+    }, async (req: Request, res: Response) => {
+        try {
+            await Auth.is_auth(req);
+
+            return res.json({});
+        } catch (err) {
+            return Err.respond(err, res);
+        }
+    });
+}
