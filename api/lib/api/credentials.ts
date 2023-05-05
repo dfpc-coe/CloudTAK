39a59d113c253f8e9179bc3c80391cecac5d25c6
ingalls <nick@ingalls.ca>
2023-05-05 14:49:18 -0700

    Optimize Showdown deps

--- api/lib/api/credentials.ts
+++ api/lib/api/credentials.ts
@@ -1,5 +1,6 @@
 import TAKAPI from '../tak-api.js';
 import pem from 'pem';
+import xml2js from 'xml2js';
 
 export default class {
     api: TAKAPI;
@@ -19,20 +20,18 @@ export default class {
         const url = new URL(`/Marti/api/tls/signClient/v2`, this.api.url);
         url.searchParams.append('clientUid', this.api.auth.username + ' (ETL)');
 
-        const config = await this.config();
+        const config = xml2js.parseStringPromise(await this.config());
 
         console.error(config);
 
         //@ts-ignore The type defs don't have promisified
         const keys = await pem.promisified.createCertificate({ days: 356, selfSigned: true });
 
-        console.error(keys);
-
         const res = await this.api.fetch(url, {
             method: 'POST',
             body: keys.csr
         });
 
-        console.error(res);
+        //console.error(res);
     }
 }
