8846cec2bec9e03bac50020699a6c9b05a668021
ingalls <nick@ingalls.ca>
2023-05-14 20:19:17 -0600

    Add PUT endpoints for TileJSON & XML Upload

--- api/routes/basemap.ts
+++ api/routes/basemap.ts
@@ -1,13 +1,75 @@
 import Err from '@openaddresses/batch-error';
 // @ts-ignore
 import BaseMap from '../lib/types/basemap.js';
+// @ts-ignore
+import Data from '../lib/types/data.js';
 import Auth from '../lib/auth.js';
+import busboy from 'busboy';
 import { sql } from 'slonik';
 import Config from '../lib/config.js';
 import { Request, Response } from 'express';
 import xml2js from 'xml2js';
 
 export default async function router(schema: any, config: Config) {
+    await schema.put('/basemap', {
+        name: 'Import BaseMaps',
+        group: 'BaseMap',
+        auth: 'user',
+        description: `
+            If the Content-Type if text/plain, then assume the body contains a TileJSON URL
+            Alternatively, if the Content-Type is a MultiPart upload, assume the input is a TAK XML document
+
+            Both return as many BaseMap fields as possible to use in the creation of a new BaseMap
+        `,
+        res: 'res.ImportBaseMap.json'
+    }, async (req: Request, res: Response) => {
+        try {
+            await Auth.is_auth(req);
+
+            const imported = {};
+
+            console.error(req.header);
+
+            let bb;
+            let data: Data;
+            try {
+                await Auth.is_auth(req);
+
+                data = await Data.from(config.pool, req.params.dataid);
+
+                if (!req.headers['content-type']) throw new Err(400, null, 'Missing Content-Type Header');
+
+                bb = busboy({
+                    headers: req.headers,
+                    limits: {
+                        files: 1
+                    }
+                });
+            } catch (err) {
+                return Err.respond(err, res);
+            }
+
+            const assets: Promise<void>[] = [];
+            bb.on('file', async (fieldname, file, blob) => {
+                try {
+                } catch (err) {
+                    return Err.respond(err, res);
+                }
+            }).on('finish', async () => {
+                try {
+                } catch (err) {
+                    Err.respond(err, res);
+                }
+            });
+
+            return req.pipe(bb);
+
+            return res.json(imported);
+        } catch (err) {
+            return Err.respond(err, res);
+        }
+    });
+
     await schema.get('/basemap', {
         name: 'List BaseMaps',
         group: 'BaseMap',
