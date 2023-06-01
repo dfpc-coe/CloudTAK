41003b970cfd946a2502a8bf636479a3bb376f19
ingalls <nick@ingalls.ca>
2023-06-01 10:41:46 -0600

    Add Profile Tokens Page

---
+++ api/routes/token.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from 'express';
+import Err from '@openaddresses/batch-error';
+import Auth from '../lib/auth.js';
+import Token from '../lib/token.js';
+
+export default async function router(schema: any, config: Config) {
+    const token = new Token(config.pool);
+
+    await schema.get('/token', {
+        name: 'List Tokens',
+        group: 'Token',
+        auth: 'user',
+        description: 'List all tokens associated with the requester\'s account',
+        res: 'res.ListTokens.json'
+    }, async (req: Request, res: Response) => {
+        try {
+            await Auth.is_auth(req);
+
+            return res.json(await token.list(req.auth));
+        } catch (err) {
+            return Err.respond(err, res);
+        }
+    });
+
+    await schema.post('/token', {
+        name: 'Create Tokens',
+        group: 'Token',
+        auth: 'user',
+        description: 'Create a new API token for programatic access',
+        body: 'req.body.CreateToken.json',
+        res: 'res.CreateToken.json'
+    }, async (req: Request, res: Response) => {
+        try {
+            await Auth.is_auth(req);
+
+            return res.json(await token.generate(req.auth, req.body.name));
+        } catch (err) {
+            return Err.respond(err, res);
+        }
+    });
+
+    await schema.delete('/token/:id', {
+        name: 'Delete Tokens',
+        group: 'Token',
+        auth: 'user',
+        description: 'Delete a user\'s API Token',
+        ':id': 'integer',
+        res: 'res.Standard.json'
+    }, async (req: Request, res: Response) => {
+        try {
+            await Auth.is_auth(req);
+
+            return res.json(await token.delete(req.auth, req.params.id));
+        } catch (err) {
+            return Err.respond(err, res);
+        }
+    });
+}
--- api/web/src/components/Profile.vue
+++ api/web/src/components/Profile.vue
@@ -3,7 +3,8 @@
     <div class='page-body'>
         <div class='container-xl'>
             <div class='row row-deck row-cards'>
-                <div class='col-sm-6 col-lg-3'>
+                <div class='col-12'>
+                    <ProfileTokens/>
                 </div>
             </div>
         </div>
@@ -15,11 +16,13 @@
 
 <script>
 import PageFooter from './PageFooter.vue';
+import ProfileTokens from './profile/Tokens.vue';
 
 export default {
     name: 'Profile',
     components: {
         PageFooter,
+        ProfileTokens
     }
 }
 </script>
