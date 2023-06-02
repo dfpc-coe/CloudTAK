29d5a0fb4e80b81806a04e9341829cf6cfd01098
ingalls <nick@ingalls.ca>
2023-06-02 13:19:04 -0600

    Add Groups

---
+++ api/web/src/components/profile/Groups.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="card">
+    <div class="card-body">
+        <div class="d-flex">
+            <h3 class="card-title">User Groups</h3>
+        </div>
+    </div>
+
+</div>
+</template>
+
+<script>
+import {
+    TablerLoading
+} from '@tak-ps/vue-tabler';
+import None from '../cards/None.vue';
+
+export default {
+    name: 'ProfileGroups',
+    data: function() {
+        return {
+            loading: true,
+            groups: []
+        }
+    },
+    mounted: async function() {
+        await this.fetch();
+    },
+    methods: {
+        fetch: async function() {
+            this.loading = true;
+            this.list = await window.std('/api/token');
+            this.loading = false;
+        },
+    },
+    components: {
+        TablerLoading
+    }
+}
+</script>
