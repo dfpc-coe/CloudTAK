89d396d6540f614119c75bd66cb105d3011fa5d0
ingalls <nick@ingalls.ca>
2023-06-20 21:42:06 -0600

    Migrate to single inline error component

--- api/web/src/components/util/Alert.vue
+++ api/web/src/components/util/Alert.vue
@@ -1,18 +1,17 @@
 <template>
-<div class='card'>
-    <div class='card-body'>
-        <div class='d-flex justify-content-center' :class='{
-            "mt-4 mb-2": !compact
-        }'>
-            <AlertTriangleIcon v-if='compact' width='32' height='32' />
-            <AlertTriangleIcon v-else width='48' height='48' />
-        </div>
+<div class='card-body'>
+    <div class='d-flex justify-content-center' :class='{
+        "mt-4 mb-2": !compact
+    }'>
+        <AlertTriangleIcon v-if='compact' width='32' height='32' />
+        <AlertTriangleIcon v-else width='48' height='48' />
+    </div>
 
-        <div class='text-center' :class='{
-            "mb-4 mt-2": !compact
-        }'>
-            <div><span v-text='label'/></div>
-        </div>
+    <h3 class='pt-3 text-center' v-text='title'></h3>
+    <div class='text-center' :class='{
+        "mb-4 mt-2": !compact
+    }'>
+        <div><span v-text='err'/></div>
     </div>
 </div>
 </template>
@@ -25,7 +24,11 @@ import {
 export default {
     name: 'Alert',
     props: {
-        label: {
+        title: {
+            type: String,
+            default: 'Generic Error'
+        },
+        err: {
             type: String,
             default: 'Something is amiss'
         },
