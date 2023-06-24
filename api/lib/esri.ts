278378af00929f2d506b0f99186e627229a8be59
ingalls <nick@ingalls.ca>
2023-06-24 14:11:38 -0700

    Setup ESRI List Endpoints

--- api/lib/esri.ts
+++ api/lib/esri.ts
@@ -1,20 +1,71 @@
+import Err from '@openaddresses/batch-error';
+
 export default class EsriProxy {
     token: string;
+    base: URL;
+    expires: number;
+    referer: string;
 
-    constructor(token: string) {
+    constructor(token: string, expires: number, base: URL, referer: string) {
         this.token = token;
+        this.base = base;
+        this.expires = expires;
+        this.referer = referer;
+    }
+
+    static parser(url: URL): URL {
+        const base = new URL(url.origin);
+        const path = url.pathname.replace(/\/rest.*/, '') + '/rest';
+        base.pathname = path;
+        return base;
+    }
+
+    async getList() {
+        const res = await fetch(this.base + `?f=json`, {
+            method: 'GET',
+            headers: {
+                'Referer': this.referer,
+                'X-Esri-Authorization': `Bearer ${this.token}`
+            },
+        });
+
+        const json = await res.json()
+
+        if (json.error) throw new Err(400, null, 'ESRI Server Error: ' + json.error.message);
+
+        return json;
+    }
+
+    async getServers() {
+        const res = await fetch(this.base + `/portals/self/servers?f=json`, {
+            method: 'GET',
+            headers: {
+                'Referer': this.referer,
+                'X-Esri-Authorization': `Bearer ${this.token}`
+            },
+        });
+
+        const json = await res.json()
+
+        if (json.error) throw new Err(400, null, 'ESRI Server Error: ' + json.error.message);
+
+        return json;
     }
 
     static async generateToken(
         url: URL,
+        referer: string,
         username: string,
         password: string
     ): Promise<EsriProxy> {
         const body = new URLSearchParams();
         body.append('username', username);
         body.append('password', password);
+        body.append('referer', referer);
 
-        const res = await fetch(url, {
+        const base = this.parser(url);
+
+        const res = await fetch(base + '/generateToken?f=json', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
@@ -22,8 +73,15 @@ export default class EsriProxy {
             body: String(body)
         });
 
-        const token: string = '123';
+        const json = await res.json()
 
-        return new EsriProxy(token);
+        if (json.error) throw new Err(400, null, 'ESRI Server Error: ' + json.error.message);
+
+        return new EsriProxy(
+            json.token,
+            json.expires,
+            this.parser(url),
+            referer
+        );
     }
 }
--- api/web/src/components/util/EsriView.vue
+++ api/web/src/components/util/EsriView.vue
@@ -1,5 +1,7 @@
 <template>
-<div class='card-body'>
+<div class='border py-2'>
+    <h1 class='subheader px-3'>ESRI Server Explorer</h1>
+
     <template v-if='err'>
         <Alert title='ESRI Connection Error' :err='err.message' :compact='true'/>
         <div class="col-md-12 mt-3">
@@ -12,6 +14,18 @@
     </template>
     <template v-else-if='loading'>
         <TablerLoading desc='Connecting to ESRI Server'/>
+    </template>
+    <template v-else-if='!server'>
+        <div class='table-responsive'>
+            <table class="table table-hover card-table table-vcenter cursor-pointer">
+                <thead><tr><th>ID</th><th>Name</th><th>Url</th></tr></thead>
+                <tbody><tr @click='server = serv' :key='serv.id' v-for='serv in servers'>
+                    <td v-text='serv.id'></td>
+                    <td v-text='serv.name'></td>
+                    <td v-text='serv.url'></td>
+                </tr></tbody>
+            </table>
+        </div>
     </template>
 </div>
 </template>
@@ -41,16 +55,39 @@ export default {
         return {
             loading: true,
             err: null,
-            token: null
+            token: null,
+            server: null,
+            list: {},
+            servers: []
+        }
+    },
+    watch: {
+        server: async function() {
+            await this.getList()
         }
     },
     mounted: async function() {
         await this.generateToken();
     },
     methods: {
+        getList: async function() {
+            try {
+                const url = window.stdurl('/api/sink/esri');
+                url.searchParams.append('token', this.token);
+                url.searchParams.append('url', this.server.url + '/rest');
+                const res = await window.std(url, {
+                    method: 'GET',
+                });
+
+                this.list = res;
+            } catch (err) {
+                this.err = err;
+            }
+            this.loading = false;
+        },
         generateToken: async function() {
             try {
-                await window.std('/api/sink/esri', {
+                const res = await window.std('/api/sink/esri', {
                     method: 'POST',
                     body: {
                         username: this.username,
@@ -58,10 +95,12 @@ export default {
                         url: this.url
                     }
                 });
+
+                this.token = res.token;
+                this.servers = res.servers;
             } catch (err) {
                 this.err = err;
             }
-
             this.loading = false;
         }
     },
