41b5620fe74cfcc36ff73c8cfdd8b1f50c66f652
ingalls <nick@ingalls.ca>
2023-07-18 06:58:29 -0600

    Write Sink Metric Data

--- tasks/hooks/src/index.ts
+++ tasks/hooks/src/index.ts
@@ -2,28 +2,58 @@ import Lambda from "aws-lambda";
 import ArcGIS from './adaptors/arcgis.js';
 import CW from '@aws-sdk/client-cloudwatch';
 
+type Meta = {
+    Timestamp: Date;
+    Error?: Error;
+}
+
 export async function handler(
     event: Lambda.SQSEvent
 ): Promise<boolean> {
+    const meta: Map<string, Meta> = new Map();
+
     for (const record of event.Records) {
         try {
             const req = JSON.parse(record.body);
 
             if (req.type === 'ArcGIS') {
+                meta.set(record.messageId, {
+                    Timestamp: new Date()
+                });
                 await ArcGIS(req);
             } else {
                 throw new Error('Unknown Event Type');
             }
+
         } catch (err) {
-            cw
+            const m = meta.get(record.messageId);
+            if (m) m.Error = new Error(String(err));
         }
     }
 
     if (process.env.StackName) {
-        const cw = new CW.CloudwatchClient();
-        cw.send(new CW.PutMetricDataCommand({
+        const cw = new CW.CloudWatchClient({});
+        await cw.send(new CW.PutMetricDataCommand({
             Namespace: 'TAKETL',
+            MetricData: event.Records.map((record: Lambda.SQSRecord) => {
+                const m = meta.get(record.messageId);
+                if (!m) throw new Error('Indeterminant Meta');
 
+                const MetricName = m.Error ? 'ConnectionSinkFailure' : 'ConnectionSinkSuccess';
+
+                return {
+                    MetricName,
+                    Value: 1,
+                    Timestamp: m.Timestamp,
+                    Dimensions: [{
+                        Name: 'StackName',
+                        Value: process.env.StackName
+                    },{
+                        Name: 'ConnectionSinkId',
+                        Value: JSON.parse(record.body).id
+                    }],
+                }
+            })
         }));
     }
 
