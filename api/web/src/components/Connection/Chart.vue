8f332e3c5e12ed79228547b7ed2f9ea8d38ada7e
ingalls <nick@ingalls.ca>
2023-07-24 16:10:24 -0600

    Add Refresh Button to Connection Throughput

--- api/web/src/components/Connection/Chart.vue
+++ api/web/src/components/Connection/Chart.vue
@@ -2,6 +2,10 @@
 <div class='card'>
     <div class='card-header'>
         <h2 class='card-title'>Connection Throughput</h2>
+
+        <div class='ms-auto btn-list'>
+            <RefreshIcon @click='fetchData' class='cursor-pointer'/>
+        </div>
     </div>
     <div class='card-body'>
         <TablerLoading v-if='loading'/>
@@ -12,6 +16,9 @@
 
 <script>
 import ApexCharts from 'apexcharts';
+import {
+    RefreshIcon
+} from 'vue-tabler-icons'
 import {
     TablerLoading
 } from '@tak-ps/vue-tabler';
@@ -27,60 +34,61 @@ export default {
     },
     mounted: async function() {
         await this.fetchData();
-
-        this.$nextTick(() => {
-            new ApexCharts(document.getElementById('chart'), {
-                chart: {
-                    type: "line",
-                    fontFamily: 'inherit',
-                    height: 240,
-                    parentHeightOffset: 0,
-                    toolbar: {
-                        show: false,
-                    },
-                    animations: {
-                        enabled: false
-                    },
-                },
-                fill: {
-                    opacity: 1,
-                },
-                stroke: {
-                    width: 2,
-                    lineCap: "round",
-                    curve: "smooth",
-                },
-                series: [{
-                    name: "Success",
-                    data: this.success
-                }],
-                tooltip: { theme: 'dark' },
-                colors: [
-                    '#008000',
-                    '#FF0000'
-                ],
-                legend: {
-                    show: false
-                },
-                xaxis: {
-                    labels: {
-                        padding: 0,
-                    },
-                    tooltip: {
-                        enabled: false
-                    },
-                    type: 'datetime',
-                },
-                yaxis: {
-                    labels: {
-                        padding: 4
-                    },
-                },
-                labels: this.labels
-            }).render();
-        });
     },
     methods: {
+        mountChart: function() {
+            this.$nextTick(() => {
+                new ApexCharts(document.getElementById('chart'), {
+                    chart: {
+                        type: "line",
+                        fontFamily: 'inherit',
+                        height: 240,
+                        parentHeightOffset: 0,
+                        toolbar: {
+                            show: false,
+                        },
+                        animations: {
+                            enabled: false
+                        },
+                    },
+                    fill: {
+                        opacity: 1,
+                    },
+                    stroke: {
+                        width: 2,
+                        lineCap: "round",
+                        curve: "smooth",
+                    },
+                    series: [{
+                        name: "Success",
+                        data: this.success
+                    }],
+                    tooltip: { theme: 'dark' },
+                    colors: [
+                        '#008000',
+                        '#FF0000'
+                    ],
+                    legend: {
+                        show: false
+                    },
+                    xaxis: {
+                        labels: {
+                            padding: 0,
+                        },
+                        tooltip: {
+                            enabled: false
+                        },
+                        type: 'datetime',
+                    },
+                    yaxis: {
+                        labels: {
+                            padding: 4
+                        },
+                    },
+                    labels: this.labels
+                }).render();
+            });
+        },
         fetchData: async function() {
             this.loading = true;
             const list = await window.std(`/api/connection/${this.$route.params.connectionid}/stats`);
@@ -89,9 +97,11 @@ export default {
             this.success = list.stats.map(s => s.success);
 
             this.loading = false;
+            this.mountChart();
         }
     },
     components: {
+        RefreshIcon,
         TablerLoading
     }
 }
