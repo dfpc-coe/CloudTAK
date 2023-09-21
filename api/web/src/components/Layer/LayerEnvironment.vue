8b3d7db94cc522ab9796c0aa5d80436d65e6afde
ingalls <nick@ingalls.ca>
2023-09-21 06:08:55 -0600

    Environment Save

--- api/web/src/components/Layer/LayerEnvironment.vue
+++ api/web/src/components/Layer/LayerEnvironment.vue
@@ -8,6 +8,7 @@
     </div>
 
     <TablerLoading v-if='loading.schema' desc='Loading Environment'/>
+    <TablerLoading v-else-if='loading.save' desc='Saving Environment'/>
     <div v-else class="col">
         <template v-if='schema.type !== "object"'>
             <div class="d-flex justify-content-center my-4">
@@ -17,6 +18,12 @@
         <template v-else>
             <Schema :schema='schema' :disabled='disabled' v-model='environment'/>
         </template>
+        <div v-if='!disabled' class="col-12 px-2 py-2 d-flex">
+            <button @click='reload' class='btn'>Cancel</button>
+            <div class='ms-auto'>
+                <button @click='saveLayer' class='btn btn-primary'>Save</button>
+            </div>
+        </div>
     </div>
 </div>
 </template>
@@ -47,16 +54,20 @@ export default {
             environment: {},
             schema: {},
             loading: {
-                schema: false
+                schema: false,
+                save: false
             },
         };
     },
     mounted: async function() {
+        this.reload();
         await this.fetchSchema()
-
-        this.environment = JSON.parse(JSON.stringify(this.layer.environment));
     },
     methods: {
+        reload: function() {
+            this.environment = JSON.parse(JSON.stringify(this.layer.environment));
+            this.disabled = true;
+        },
         fetchSchema: async function() {
             this.alert = false;
 
@@ -68,7 +79,22 @@ export default {
             }
 
             this.loading.schema = false;
-        }
+        },
+        saveLayer: async function() {
+            this.loading.save = true;
+
+            const layer = await window.std(`/api/layer/${this.$route.params.layerid}`, {
+                method: 'PATCH',
+                body: {
+                    environment: this.environment
+                }
+            });
+
+            this.disabled = true;
+            this.loading.save = false;
+
+            this.$emit('layer', layer);
+        },
     },
     components: {
         Schema,
