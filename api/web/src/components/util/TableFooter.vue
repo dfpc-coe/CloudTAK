b2216ab913953d1da8e435bdb3068fc3888da960
ingalls <nick@ingalls.ca>
2023-09-27 17:14:27 -0600

    Fix TableFooter

--- api/web/src/components/util/TableFooter.vue
+++ api/web/src/components/util/TableFooter.vue
@@ -1,17 +1,21 @@
 <template>
-<div class="card-footer row">
-    <div class='col-sm-12 col-md-6'>
-        <p v-if='total === 0' class='m-0 text-muted'>Showing 0 of 0 entries</p>
-        <p v-else class="m-0 text-muted">Showing <span v-text='limit * page + 1'/> to <span v-text='total < limit ? total : (page * limit + limit > total ? total : page * limit + limit)'/> of <span v-text='total'/> entries</p>
-    </div>
-    <div v-if='total > limit' class='col-sm-12 col-md-6 d-flex'>
-        <Pager @page='page = $event' :total='total' :limit='limit'/>
+<div class="card-footer">
+    <div class='row'>
+        <div class='col-sm-12 col-md-6'>
+            <p v-if='total === 0' class='m-0 text-muted'>Showing 0 of 0 entries</p>
+            <p v-else class="m-0 text-muted">Showing <span v-text='limit * page + 1'/> to <span v-text='total < limit ? total : (page * limit + limit > total ? total : page * limit + limit)'/> of <span v-text='total'/> entries</p>
+        </div>
+        <div v-if='total > limit' class='col-sm-12 col-md-6 d-flex'>
+            <TablerPager @page='page = $event' :total='total' :limit='limit'/>
+        </div>
     </div>
 </div>
 </template>
 
 <script>
-import Pager from './Pager.vue'
+import {
+    TablerPager
+} from '@tak-ps/vue-tabler';
 
 export default {
     name: 'TableFooter',
@@ -36,7 +40,7 @@ export default {
         }
     },
     components: {
-        Pager
+        TablerPager
     }
 }
 </script>
