12d8317bd4d773cd6564c616b47e3bb467af477c
ingalls <nick@ingalls.ca>
2023-10-10 15:23:41 -0600

    Setup S3 Task

--- tasks/events/src/index.ts
+++ tasks/events/src/index.ts
@@ -1,16 +1,60 @@
+import fs from 'node:fs/promises';
 import Lambda from "aws-lambda";
-import jwt from 'jsonwebtoken';
+import path from 'node:path';
+//import jwt from 'jsonwebtoken';
 import S3 from "@aws-sdk/client-s3";
-import zlib from "zlib";
-// @ts-ignore
-import { NodeHttpHandler } from "@aws-sdk/node-http-handler";
-
+import StreamZip from 'node-stream-zip'
+import { pipeline } from 'node:stream/promises';
+import asyncPool from 'tiny-async-pool';
 
 export const handler = async (
     event: Lambda.S3NotificationEvent,
-    context: Lambda.Context,
 ): Promise<Lambda.APIGatewayProxyResult> => {
     for (const record of event.Records) {
-        console.error('Record', record);
+        const md = {
+            Bucket: record.s3.bucket.name,
+            Key: decodeURIComponent(record.s3.object.key.replace(/\+/g, ' '))
+        }
+        console.log(`ok - New file detected in ${md.Bucket}: ${md.Key}`);
+
+        if (!md.Key.startsWith('import/')) {
+            console.log(`ok - Not an import - skipping`);
+            return;
+        }
+
+        const s3 = new S3.S3Client({ region: process.env.AWS_DEFAULT_REGION || 'us-east-1' });
+
+        md.Ext = path.parse(md.Key).ext;
+        md.Local = path.resolve(os.tmpdir(), `input${md.Ext}`);
+
+        await pipeline(
+            (await s3.send(new S3.GetObjectCommand({
+                Bucket: md.Bucket,
+                Key: md.Key
+            }))).Body,
+            fs.createWriteStream(md.Local)
+        );
+
+        if (md.Ext === '.zip') {
+            const zip = new StreamZip.async({
+                file: path.resolve(os.tmpdir(), md.Local),
+                skipEntryNameValidation: true
+            });
+
+            const preentries = await zip.entries();
+            const entries = []
+            for (const entrykey in preentries) {
+                const entry = preentries[entrykey];
+                entries.push(entry);
+            }
+
+            for await (const ms of asyncPool(1000, entries, async (entry) => {
+                console.error(entry)
+                const data = await zip.entryData(entry);
+                await fs.writeFile(path.resolve(os.tmpdir(), entry.name), data);
+            })) {
+                console.log(ms);
+            }
+        }
     }
 };
