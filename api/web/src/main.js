106baeb0a6196979f1c4a835ecd14350a1da5e8d
ingalls <nick@ingalls.ca>
2023-10-11 21:41:13 -0600

    Setup Iconset Explorer Page

--- api/web/src/main.js
+++ api/web/src/main.js
@@ -84,6 +84,7 @@ const router = new VueRouter.createRouter({
 
         { path: '/iconset', name: 'icons', component: () => import('./components/Icons.vue') },
         { path: '/iconset/new', name: 'icon-new', component: () => import('./components/IconEdit.vue') },
+        { path: '/iconset/:icon', name: 'icon', component: () => import('./components/Icon.vue') },
         { path: '/iconset/:icon/edit', name: 'icon-edit', component: () => import('./components/IconEdit.vue') },
 
         { path: '/login', name: 'login', component: () => import('./components/Login.vue') },
