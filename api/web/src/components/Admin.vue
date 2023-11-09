9942c9e67bc0a8112f458a0e3b8026b52eea9b37
ingalls <nick@ingalls.ca>
2023-11-09 08:36:53 -0700

    Setup Certificate Upload

--- api/web/src/components/Admin.vue
+++ api/web/src/components/Admin.vue
@@ -28,74 +28,78 @@
                                 </div>
                             </div>
                             <div class="card-body">
-                                <div class='row'>
-                                    <div class='col-lg-12 py-2'>
+                                <div class='col-lg-12 py-2'>
+                                    <TablerInput
+                                        v-model='server.name'
+                                        :disabled='!edit'
+                                        label='TAK Server Name'
+                                        placeholder='ssl://'
+                                        :error='errors.name'
+                                    />
+                                </div>
+
+                                <div class='col-lg-12 py-2'>
+                                    <TablerInput
+                                        v-model='server.url'
+                                        :disabled='!edit'
+                                        label='TAK Server Streaming CoT'
+                                        placeholder='ssl://'
+                                        :error='errors.url'
+                                    />
+                                </div>
+                                <div class='col-lg-12 py-2'>
+                                    <TablerInput
+                                        v-model='server.api'
+                                        :disabled='!edit'
+                                        label='TAK Server API'
+                                        placeholder='https://'
+                                        :error='errors.api'
+                                    />
+                                </div>
+                            </div>
+                            <div class='card-header'>
+                                <h3 class='card-title'>Admin Certificate</h3>
+                                <div v-if='regen && edit' class='ms-auto btn-list'>
+                                    <PlusIcon @click='modal.upload = true' class='cursor-pointer'/>
+                                </div>
+                            </div>
+                            <div class='card-body row'>
+                                <template v-if='regen && edit'>
+                                    <div class="col-md-6">
                                         <TablerInput
-                                            v-model='server.name'
-                                            :disabled='!edit'
-                                            label='TAK Server Name'
-                                            placeholder='ssl://'
-                                            :error='errors.name'
+                                            label='Connection Cert'
+                                            v-model='auth.cert'
+                                            :error='errors.cert'
+                                            :rows='6'
                                         />
                                     </div>
-
-                                    <div class='col-lg-12 py-2'>
+                                    <div class="col-md-6">
                                         <TablerInput
-                                            v-model='server.url'
-                                            :disabled='!edit'
-                                            label='TAK Server Streaming CoT'
-                                            placeholder='ssl://'
-                                            :error='errors.url'
+                                            label='Connection Key'
+                                            v-model='auth.key'
+                                            :error='errors.key'
+                                            :rows='6'
                                         />
                                     </div>
-                                    <div class='col-lg-12 py-2'>
-                                        <TablerInput
-                                            v-model='server.api'
-                                            :disabled='!edit'
-                                            label='TAK Server API'
-                                            placeholder='https://'
-                                            :error='errors.api'
-                                        />
+                                </template>
+                                <template v-else>
+                                    <div class='px-3 py-3'>
+                                        <div class='d-flex justify-content-center'>
+                                            <LockIcon width='50' height='50'/>
+                                        </div>
+                                        <div class='d-flex justify-content-center my-3'>
+                                            Once Certificates are uploaded they cannot be viewed
+                                        </div>
+                                        <div v-if='edit' class='d-flex justify-content-center'>
+                                            <button @click='regen=true' class='btn btn-secondary'>Replace Certificate</button>
+                                        </div>
                                     </div>
+                                </template>
 
-                                    <template v-if='regen'>
-                                        <div class="col-md-6">
-                                            <TablerInput
-                                                label='Connection Cert'
-                                                v-model='auth.cert'
-                                                :error='errors.cert'
-                                                :rows='6'
-                                            />
-                                        </div>
-                                        <div class="col-md-6">
-                                            <TablerInput
-                                                label='Connection Key'
-                                                v-model='auth.key'
-                                                :error='errors.key'
-                                                :rows='6'
-                                            />
-                                        </div>
-                                    </template>
-                                    <template v-else>
-                                        <label class='subheader mt-3'>Admin Certificate</label>
-                                        <div class='px-3 py-3'>
-                                            <div class='d-flex justify-content-center'>
-                                                <LockIcon width='50' height='50'/>
-                                            </div>
-                                            <div class='d-flex justify-content-center my-3'>
-                                                Once Certificates are uploaded they cannot be viewed
-                                            </div>
-                                            <div v-if='edit' class='d-flex justify-content-center'>
-                                                <button @click='regen=true' class='btn btn-secondary'>Replace Certificate</button>
-                                            </div>
-                                        </div>
-                                    </template>
-
-                                    <div v-if='edit' class='col-lg-12 d-flex py-2'>
-                                        <div class='ms-auto'>
-                                            <div @click='postServer' class='btn btn-primary'>
-                                                Save Server
-                                            </div>
+                                <div v-if='edit' class='col-lg-12 d-flex py-2'>
+                                    <div class='ms-auto'>
+                                        <div @click='postServer' class='btn btn-primary'>
+                                            Save Server
                                         </div>
                                     </div>
                                 </div>
@@ -130,6 +134,7 @@ import {
     TablerInput
 } from '@tak-ps/vue-tabler';
 import {
+    PlusIcon,
     LockIcon,
     SettingsIcon
 } from 'vue-tabler-icons';
@@ -178,6 +183,7 @@ export default {
         fetch: async function() {
             this.loading = true;
             this.server = await window.std(`/api/server`);
+            if (!this.server.auth) this.regen = true;
             this.loading = false;
         },
         postServer: async function() {
@@ -228,6 +234,7 @@ export default {
         TablerLoading,
         TablerInput,
         LockIcon,
+        PlusIcon,
         Upload
     }
 }
