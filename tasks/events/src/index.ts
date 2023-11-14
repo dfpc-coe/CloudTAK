cbcbbc965d87921ae65bb1e60dc65396bd4a4519
ingalls <nick@ingalls.ca>
2023-11-14 07:43:45 -0700

    Wire up Stack SQS to Events Lambda

--- tasks/events/src/index.ts
+++ tasks/events/src/index.ts
@@ -19,75 +19,87 @@ interface Event {
 }
 
 export const handler = async (
-    event: Lambda.S3Event,
+    event: Lambda.S3Event | Lambda.SQSEvent,
 ): Promise<void> => {
 
     for (const record of event.Records) {
-        const md = {
-            Import: path.parse(decodeURIComponent(record.s3.object.key.replace(/\+/g, ' '))).name,
-            Token: jwt.sign({ access: 'event' }, String(process.env.SigningSecret)),
-            Bucket: record.s3.bucket.name,
-            Key: decodeURIComponent(record.s3.object.key.replace(/\+/g, ' ')),
-            Ext: path.parse(decodeURIComponent(record.s3.object.key.replace(/\+/g, ' '))).ext,
-            Local: path.resolve(os.tmpdir(), `input${path.parse(decodeURIComponent(record.s3.object.key.replace(/\+/g, ' '))).ext}`),
-        }
-
-        try {
-            console.log(`ok - New file detected in s3://${md.Bucket}/${md.Key}`);
-
-            if (!md.Key.startsWith('import/')) {
-                console.log(`ok - Not an import - skipping`);
-                return;
-            }
-
-            await updateImport(md, { status: 'Running' });
-
-            const s3 = new S3.S3Client({ region: process.env.AWS_DEFAULT_REGION || 'us-east-1' });
-
-            await pipeline(
-                // @ts-ignore
-                (await s3.send(new S3.GetObjectCommand({
-                    Bucket: md.Bucket,
-                    Key: md.Key
-                }))).Body,
-                fs.createWriteStream(md.Local)
-            );
-
-            if (md.Ext === '.zip') {
-                const zip = new StreamZip.async({
-                    file: path.resolve(os.tmpdir(), md.Local),
-                    skipEntryNameValidation: true
-                });
-
-                const preentries = await zip.entries();
-
-                const indexes = [];
-                for (const entrykey in preentries) {
-                    const entry = preentries[entrykey];
-
-                    if (path.parse(entry.name).ext === '.xml') {
-                        indexes.push(entry);
-                    }
-                }
-
-                for (const index of indexes) {
-                    await processIndex(md, String(await zip.entryData(index)), zip);
-                }
-            } else if (md.Ext === '.xml') {
-                await processIndex(md, String(await fsp.readFile(md.Local)));
-            } else {
-                throw new Error('Unable to parse Index');
-            }
-        } catch (err) {
-            console.error(err);
-
-            await updateImport(md, {
-                status: 'Fail',
-                error: err instanceof Error ? err.message : String(err)
-            });
+        if (record.s3) {
+            await s3Event(record as Lambda.S3EventRecord)
+        } else if (record.body) {
+            await sqsEvent(record as Lambda.SQSRecord);
         }
     }
 };
+
+async function sqsEvent(record: Lambda.SQSRecord) {
+    console.error(record);
+}
+
+async function s3Event(record: Lambda.S3EventRecord) {
+    const md = {
+        Import: path.parse(decodeURIComponent(record.s3.object.key.replace(/\+/g, ' '))).name,
+        Token: jwt.sign({ access: 'event' }, String(process.env.SigningSecret)),
+        Bucket: record.s3.bucket.name,
+        Key: decodeURIComponent(record.s3.object.key.replace(/\+/g, ' ')),
+        Ext: path.parse(decodeURIComponent(record.s3.object.key.replace(/\+/g, ' '))).ext,
+        Local: path.resolve(os.tmpdir(), `input${path.parse(decodeURIComponent(record.s3.object.key.replace(/\+/g, ' '))).ext}`),
+    }
+
+    try {
+        console.log(`ok - New file detected in s3://${md.Bucket}/${md.Key}`);
+
+        if (!md.Key.startsWith('import/')) {
+            console.log(`ok - Not an import - skipping`);
+            return;
+        }
+
+        await updateImport(md, { status: 'Running' });
+
+        const s3 = new S3.S3Client({ region: process.env.AWS_DEFAULT_REGION || 'us-east-1' });
+
+        await pipeline(
+            // @ts-ignore
+            (await s3.send(new S3.GetObjectCommand({
+                Bucket: md.Bucket,
+                Key: md.Key
+            }))).Body,
+            fs.createWriteStream(md.Local)
+        );
+
+        if (md.Ext === '.zip') {
+            const zip = new StreamZip.async({
+                file: path.resolve(os.tmpdir(), md.Local),
+                skipEntryNameValidation: true
+            });
+
+            const preentries = await zip.entries();
+
+            const indexes = [];
+            for (const entrykey in preentries) {
+                const entry = preentries[entrykey];
+
+                if (path.parse(entry.name).ext === '.xml') {
+                    indexes.push(entry);
+                }
+            }
+
+            for (const index of indexes) {
+                await processIndex(md, String(await zip.entryData(index)), zip);
+            }
+        } else if (md.Ext === '.xml') {
+            await processIndex(md, String(await fsp.readFile(md.Local)));
+        } else {
+            throw new Error('Unable to parse Index');
+        }
+    } catch (err) {
+        console.error(err);
+
+        await updateImport(md, {
+            status: 'Fail',
+            error: err instanceof Error ? err.message : String(err)
+        });
+    }
+}
 
 async function updateImport(event: Event, body: object) {
     const res = await fetch(new URL(`/api/import/${event.Import}`, process.env.TAK_ETL_API), {
@@ -166,7 +178,7 @@ async function processIndex(event: Event, xmlstr: string, zip?: StreamZipAsync) 
                 })
             });
 
-            if (!iconset_req.ok) console.error(await iconset_req.text());
+            if (!icon_req.ok) console.error(await icon_req.text());
         }
 
         await updateImport(event, {
