afe9eb6b724bd777600592354214617dffcd4b84
ingalls <nick@ingalls.ca>
2023-11-14 07:56:05 -0700

    CF Lints

--- cloudformation/lib/stacks.js
+++ cloudformation/lib/stacks.js
@@ -3,12 +3,12 @@ import cf from '@openaddresses/cloudfriend';
 export default {
     Resources: {
         StackHook: {
-            Type : "AWS::SNS::Topic",
+            Type : 'AWS::SNS::Topic',
             Properties : {
                 DisplayName: cf.join([cf.stackName, ' Layer CloudFormation Events']),
                 TopicName: cf.join([cf.stackName, '-stack-events']),
                 Subscription: [{
-                    Endpoint: cf.getAtt('StackHookQueue', 'Arn')
+                    Endpoint: cf.getAtt('StackHookQueue', 'Arn'),
                     Protocol: 'sqs'
                 }]
             }
@@ -16,7 +16,7 @@ export default {
         StackHookQueue: {
             Type: 'AWS::SQS::Queue',
             Properties: {
-                QueueName: cf.join([cf.stackName, '-stack-events']),
+                QueueName: cf.join([cf.stackName, '-stack-events'])
             }
         },
         StackHookLambdaSource: {
@@ -26,6 +26,6 @@ export default {
                 EventSourceArn:  cf.getAtt('StackHookQueue', 'Arn'),
                 FunctionName: cf.ref('EventLambdaRole')
             }
-        },
+        }
     }
 };
