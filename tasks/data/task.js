d0aa60ca0d45dbbc32b33ee3e0f7dc15be7113c5
ingalls <nick@ingalls.ca>
2023-11-22 13:04:32 -0700

    Log output

--- tasks/data/task.js
+++ tasks/data/task.js
@@ -117,9 +117,11 @@ export default class Task {
             await pmuploader.done();
         } else {
             console.log(`ok - converting ${asset}`);
-            cp.spawnSync(`pmtiles convert ${asset} ${path.resolve(os.tmpdir(), path.parse(this.etl.task.asset).name + '.pmtiles')}`);
+            const pmout = cp.spawnSync(`pmtiles convert ${asset} ${path.resolve(os.tmpdir(), path.parse(this.etl.task.asset).name + '.pmtiles')}`);
+            console.log(String(pmout.stdout.toString()))
+            console.log(String(pmout.stderr.toString()))
 
-            console.error(`ok - converted: ${path.resolve(os.tmpdir(), path.parse(this.etl.task.asset).name + '.pmtiles')}`);
+            console.log(`ok - converted: ${path.resolve(os.tmpdir(), path.parse(this.etl.task.asset).name + '.pmtiles')}`);
 
             const pmuploader = new Upload({
                 client: s3,
