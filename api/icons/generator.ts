099074139bfb197df6b08a7e447bbaad269c5a19
ingalls <nick@ingalls.ca>
2023-11-24 10:32:44 -0700

    Get Default Icons working

--- api/icons/generator.ts
+++ api/icons/generator.ts
@@ -2,6 +2,7 @@ import xmljs from 'xml-js';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 import Sprites from '../lib/sprites.js';
+import Sharp from 'sharp';
 
 const iconset = new Array();
 for (const icon of xmljs.xml2js(String(await fs.readFile(new URL('../icons/icons.xml', import.meta.url))), {
@@ -10,12 +11,17 @@ for (const icon of xmljs.xml2js(String(await fs.readFile(new URL('../icons/icons
 }).icons.icon) {
     if (!icon.filePath || !icon.filePath._text) continue;
 
+    const img = await Sharp(Buffer.from(await fs.readFile(new URL(`../icons/${path.parse(icon.filePath._text).base}`, import.meta.url))))
+        .resize(32)
+        .png()
+        .toBuffer();
+
     const item = {
         id: icon.id._text,
         name: icon.displayName._text,
         file: path.parse(icon.filePath._text).base,
         parent: icon.parentID._text,
-        data: Buffer.from(await fs.readFile(new URL(`../icons/${path.parse(icon.filePath._text).base}`, import.meta.url))).toString('base64'),
+        data: img.toString('base64'),
         children: (icon.childrenIDs && Array.isArray(icon.childrenIDs.id)) ? icon.childrenIDs.id.map((id) => {
             return id._text;
         }) : []
@@ -28,7 +34,7 @@ const defaultSprites = await Sprites({
     icons: iconset.map((icon) => {
         return {
             name: icon.name,
-            type2525b: icon.id,
+            type2525b: 'a-' + icon.id,
             data: icon.data
         }
     })
