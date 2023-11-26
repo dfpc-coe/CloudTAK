7f010025e4a0b5b19c73e021eada9a6d85069cd3
ingalls <nick@ingalls.ca>
2023-11-26 10:04:51 -0700

    Add View Item

--- api/web/src/components/CloudTAK/RadialMenu/RadialMenu.vue
+++ api/web/src/components/CloudTAK/RadialMenu/RadialMenu.vue
@@ -11,6 +11,7 @@
 
      <svg id="icons" class='d-none'>
          <symbol id="radial-trash" viewBox="0 0 24 24" stroke-width="2" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></symbol>
+         <symbol id="radial-view" viewBox="0 0 24 24" stroke-width="2" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M13 5h8" /><path d="M13 9h5" /><path d="M13 15h8" /><path d="M13 19h5" /><path d="M3 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M3 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /></symbol>
      </svg>
 </template>
 
@@ -50,7 +51,8 @@ export default {
                 size: this.size,
                 closeOnClick: true,
                 menuItems: [{
-                    id: 'cot', title: 'View',
+                    id: 'cot',
+                    icon: '#radial-view'
                 },{
                     id: 'delete',
                     icon: '#radial-trash'
