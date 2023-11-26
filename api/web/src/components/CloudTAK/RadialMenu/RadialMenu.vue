29b0a349d5d84b153a907b336ab3e91bfbe8d3b6
ingalls <nick@ingalls.ca>
2023-11-26 09:11:48 -0700

    Allow drag proximal to Radial

--- api/web/src/components/CloudTAK/RadialMenu/RadialMenu.vue
+++ api/web/src/components/CloudTAK/RadialMenu/RadialMenu.vue
@@ -2,16 +2,24 @@
     <div
         ref='radialMenu'
         class='position-absolute'
+        style='pointer-events: none;'
         :style='{
-            top: `${y - (size / 2) - 15}px`,
-            left: `${x - (size / 2) - 15}px`,
+            top: `${y - (size / 2) - 10}px`,
+            left: `${x - (size / 2) - 10}px`,
         }'
     ></div>
+
+     <svg id="icons" class='d-none'>
+         <symbol id="radial-trash" viewBox="0 0 24 24" stroke-width="2" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></symbol>
+     </svg>
 </template>
 
 <script>
 import RadialMenu from './RadialMenu.js';
 import './RadialMenu.css';
+import {
+    TrashIcon
+} from 'vue-tabler-icons';
 
 export default {
     name: 'RadialMenu',
@@ -41,9 +49,10 @@ export default {
                 size: this.size,
                 closeOnClick: true,
                 menuItems: [{
-                    id: 'item1', title: 'Item 1'
+                    id: 'cot', title: 'View',
                 },{
-                    id: 'item2', title: 'Item 2'
+                    id: 'delete',
+                    icon: '#radial-trash'
                 }],
                 onClick: (item) => {
                     console.log('You have clicked:', item);
@@ -54,6 +63,9 @@ export default {
             });
             this.menu.open();
         })
+    },
+    components: {
+        TrashIcon
     }
 }
 </script>
