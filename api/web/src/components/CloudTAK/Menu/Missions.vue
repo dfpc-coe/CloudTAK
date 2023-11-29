2db6601bd022835c95baae896ea5c220b6808899
ingalls <nick@ingalls.ca>
2023-11-28 19:06:14 -0700

    Add Mission Modal

--- api/web/src/components/CloudTAK/Menu/Missions.vue
+++ api/web/src/components/CloudTAK/Menu/Missions.vue
@@ -1,13 +1,49 @@
 <template>
-<MissionList :modal='false'/>
+    <MissionList
+        :modal='false'
+        @create='mode = "create"'
+        @mission='mission = $event'
+    />
+    <TablerModal size='xl' v-if='mode !== "list"' class='text-black'>
+    <div class="modal-status bg-yellow"></div>
+        <button type="button" class="btn-close" @click='mode = "list"' aria-label="Close"></button>
+        <MissionCreate
+            v-if='mode === "create"'
+        />
+        <MissionEdit
+            v-else-if='mode === "edit"'
+            :initial='mission'
+        />
+    </TablerModal>
 </template>
 
 <script>
 import MissionList from '../../Mission/MissionList.vue';
+import MissionEdit from '../../Mission/MissionEdit.vue';
+import MissionCreate from '../../Mission/MissionCreate.vue';
+import MissionModal from '../../Mission/Modal.vue';
+import {
+    TablerModal
+} from '@tak-ps/vue-tabler';
 
 export default {
     name: 'Missions',
+    data: function() {
+        return {
+            mode: 'list',
+            mission: false
+        };
+    },
+    watch: {
+        mission: function() {
+            if (this.mission) this.mode = 'edit';
+            else this.mode = 'list';
+        }
+    },
     components: {
+        TablerModal,
+        MissionCreate,
+        MissionEdit,
         MissionList,
     }
 }
