6db64173436072bd2eea3d593f54c3bf4d2675f8
ingalls <nick@ingalls.ca>
2023-12-21 20:34:17 -0700

    Full CoT

--- api/web/src/stores/profile.js
+++ api/web/src/stores/profile.js
@@ -3,18 +3,31 @@ import { defineStore } from 'pinia'
 export const useProfileStore = defineStore('profile', {
     state: () => {
         return {
+            id: `ANDROID-${(Math.random() + 1).toString(36).substring(2)}`,
             profile: null,
         }
     },
     actions: {
         CoT: function() {
             return {
+                id: this.id,
                 type: 'Feature',
                 properties: {
                     type: 'a-f-G-E-V-C',
                     how: 'm-g',
                     callsign: this.profile.tak_callsign,
-                    droid: `ANDROID-${this.profile.tak_callsign}`
+                    droid: this.profile.tak_callsign,
+                    group: {
+                        name: this.profile.tak_group,
+                        role: this.profile.tak_role
+                    },
+                    takv: {
+                        device: navigator.userAgent,
+                        platform: 'CloudTAK',
+                        os: navigator.oscpu,
+                        //TODO Use versions
+                        version: '1.0.0'
+                    }
                 },
                 geometry: this.profile.tak_loc
             }
