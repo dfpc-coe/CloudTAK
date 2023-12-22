c8c25b0c6c5a08f8418a9839c0102066fed0c776
ingalls <nick@ingalls.ca>
2023-12-22 15:49:41 -0700

    Slightly easier colour selection

--- api/web/src/components/CloudTAK/util/CoTStyle.vue
+++ api/web/src/components/CloudTAK/util/CoTStyle.vue
@@ -1,37 +1,47 @@
 <template>
-<div class='card-body'>
-    <div class='row g-2'>
-        <template v-if='feat.geometry.type === "Point"'>
-            <div class='col-md-12 darken round'>
-                <IconSelect label='Point Icon' v-model='feat.properties.icon'/>
-            </div>
-            <div class='col-md-12 darken round'>
-                <TablerColour label='Point Color' v-model='feat.properties.color'/>
-            </div>
-        </template>
-        <template v-else-if='feat.geometry.type !== "Point"'>
-            <div class='col-md-12 darken round'>
-                <TablerColour label='Line Color' v-model='feat.properties.stroke'/>
-            </div>
+<div class='col-12'>
+    <label class='subheader'>COT Style</label>
+    <div class='mx-3 py-3'>
+        <div class='row g-2 rounded px-2 bg-gray-500'>
+            <template v-if='feat.geometry.type === "Point"'>
+                <div class='col-12'>
+                    <IconSelect label='Point Icon' v-model='feat.properties.icon'/>
+                </div>
+                <div class='col-12'>
+                    <label class='subheader'>Point Colour</label>
+                    <TablerInput type='color' v-model='feat.properties.color'/>
+                </div>
+            </template>
+            <template v-else-if='feat.geometry.type !== "Point"'>
+                <div class='col-12'>
+                    <label class='subheader'>Line Colour</label>
+                    <TablerInput type='color' v-model='feat.properties.stroke'/>
+                </div>
 
-            <div class='col-md-12 darken round'>
-                <TablerEnum label='Line Style' v-model='feat.properties["stroke-style"]' :options='["Solid", "Dashed", "Dotted", "Outlined"]'/>
-            </div>
-            <div class='col-md-12 darken round'>
-                <TablerRange label='Line Thickness' v-model='feat.properties["stroke-width"]' :min="1" :max="6" :step="1"/>
-            </div>
-            <div class='col-md-12 darken round'>
-                <TablerRange label='Line Opacity' v-model='feat.properties["stroke-opacity"]' :min='0' :max='1' :step='0.1'/>
-            </div>
-        </template>
-        <template v-if='feat.geometry.type === "Polygon"'>
-            <div class='col-md-12 darken round'>
-                <TablerColour label='Fill Color' v-model='feat.properties.fill'/>
-            </div>
-            <div class='col-md-12 darken round'>
-                <TablerRange label='Fill Opacity' v-model='feat.properties["fill-opacity"]' :min='0' :max='1' :step='0.1'/>
-            </div>
-        </template>
+                <div class='col-12'>
+                    <label class='subheader'>Line Style</label>
+                    <TablerEnum v-model='feat.properties["stroke-style"]' :options='["Solid", "Dashed", "Dotted", "Outlined"]'/>
+                </div>
+                <div class='col-12'>
+                    <label class='subheader'>Line Thickness</label>
+                    <TablerRange v-model='feat.properties["stroke-width"]' :min="1" :max="6" :step="1"/>
+                </div>
+                <div class='col-12'>
+                    <label class='subheader'>Line Opacity</label>
+                    <TablerRange v-model='feat.properties["stroke-opacity"]' :min='0' :max='1' :step='0.1'/>
+                </div>
+            </template>
+            <template v-if='feat.geometry.type === "Polygon"'>
+                <div class='col-12'>
+                    <label class='subheader'>Fill Colour</label>
+                    <TablerInput type='color' v-model='feat.properties.fill'/>
+                </div>
+                <div class='col-12 round'>
+                    <label class='subheader'>Fill Opacity</label>
+                    <TablerRange v-model='feat.properties["fill-opacity"]' :min='0' :max='1' :step='0.1'/>
+                </div>
+            </template>
+        </div>
     </div>
 </div>
 </template>
@@ -40,7 +50,7 @@
 import IconSelect from '../../util/IconSelect.vue';
 import {
     TablerRange,
-    TablerColour,
+    TablerInput,
     TablerToggle,
     TablerEnum
 } from '@tak-ps/vue-tabler';
@@ -67,7 +77,7 @@ export default {
         TablerToggle,
         TablerRange,
         TablerEnum,
-        TablerColour,
+        TablerInput,
         IconSelect,
     }
 }
