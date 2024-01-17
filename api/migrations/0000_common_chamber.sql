c66893e20b1a3dd4a641db3d8165664f0f697672
ingalls <nick@ingalls.ca>
2024-01-16 20:43:21 -0700

    Fix migration

--- api/migrations/0000_common_chamber.sql
+++ api/migrations/0000_common_chamber.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS "knex_migrations"
-DROP TABLE IF EXISTS "knex_migrations_lock"
+DROP TABLE IF EXISTS "knex_migrations";
+DROP TABLE IF EXISTS "knex_migrations_lock";
 
 CREATE TABLE IF NOT EXISTS "basemaps" (
 	"id" serial PRIMARY KEY NOT NULL,
