d14cb3301dbba47121cdf6ce0be047d0125c23e9
ingalls <nick@ingalls.ca>
2024-01-21 11:00:21 -0700

    Update SSL Mode

--- cloudformation/lib/api.js
+++ cloudformation/lib/api.js
@@ -320,7 +320,7 @@ export default {
                                 cf.sub('{{resolve:secretsmanager:${AWS::StackName}/rds/secret:SecretString:password:AWSCURRENT}}'),
                                 '@',
                                 cf.getAtt('DBInstance', 'Endpoint.Address'),
-                                ':5432/tak_ps_etl'
+                                ':5432/tak_ps_etl?sslmode=require'
                             ])
                         },
                         { Name: 'HookURL', Value: cf.ref('HookQueue') },
