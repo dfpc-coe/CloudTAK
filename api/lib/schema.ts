7c1f59d91c149d0e05cf645f2def29169fdd8909
ingalls <nick@ingalls.ca>
2024-02-02 16:31:02 -0700

    Add Connection

--- api/lib/schema.ts
+++ api/lib/schema.ts
@@ -197,7 +197,7 @@ export const Token = pgTable('tokens', {
 
 export const ConnectionToken = pgTable('connection_tokens', {
     id: serial('id').notNull(),
-    connection: text('connection').notNull().references(() => Connection.id),
+    connection: integer('connection').notNull().references(() => Connection.id),
     name: text('name').notNull(),
     token: text('token').primaryKey(),
     created: timestamp('created', { withTimezone: true }).notNull().default(sql`Now()`),
