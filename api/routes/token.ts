3733d491511b99b051fc37f276503d6c973a7afe
ingalls <nick@ingalls.ca>
2024-02-02 16:44:02 -0700

    Return Token Body

--- api/routes/token.ts
+++ api/routes/token.ts
@@ -57,7 +57,7 @@ export default async function router(schema: any, config: Config) {
                 email: req.auth.email
             });
 
-            return res.json(token[0]);
+            return res.json(token);
         } catch (err) {
             return Err.respond(err, res);
         }
