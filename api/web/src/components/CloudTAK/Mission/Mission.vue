4e0c42c69d983100dee9914d7ace3d9c015ff3dd
ingalls <nick@ingalls.ca>
2024-02-16 09:46:52 -0700

    Data Sync Styling

--- api/web/src/components/CloudTAK/Mission/Mission.vue
+++ api/web/src/components/CloudTAK/Mission/Mission.vue
@@ -76,21 +76,25 @@
                 </div>
                 <div class="mx-2 my-2" style='width: calc(100% - 40px);'>
                     <template v-if='mode === "info"'>
-                        <div class="datagrid-item pb-2">
-                            <div class="datagrid-title">Created</div>
-                            <div class="datagrid-content" v-text='mission.createTime'></div>
+                        <div class='datagrid'>
+                            <div class="datagrid-item pb-2">
+                                <div class="datagrid-title">Created</div>
+                                <div class="datagrid-content" v-text='mission.createTime'></div>
+                            </div>
+                            <div class="datagrid-item pb-2">
+                                <div class="datagrid-title">Subscribers</div>
+                                <div class="datagrid-content" v-text='subscriptions.length'></div>
+                            </div>
+                            <div class="datagrid-item pb-2">
+                                <div class="datagrid-title">Groups (Channels)</div>
+                                <div class="datagrid-content" v-text='mission.groups.join(", ")'></div>
+                            </div>
                         </div>
-                        <div class="datagrid-item pb-2">
-                            <div class="datagrid-title">Updated</div>
-                            <div class="datagrid-content"></div>
-                        </div>
-                        <div class="datagrid-item pb-2">
-                            <div class="datagrid-title">Groups (Channels)</div>
-                            <div class="datagrid-content" v-text='mission.groups.join(", ")'></div>
-                        </div>
-                        <div class="datagrid-item pb-2">
-                            <div class="datagrid-title">Description</div>
-                            <div class="datagrid-content" v-text='mission.description || "No Feed Description"'></div>
+                        <div class='datagrid'>
+                            <div class="datagrid-item pb-2">
+                                <div class="datagrid-title">Description</div>
+                                <div class="datagrid-content" v-text='mission.description || "No Feed Description"'></div>
+                            </div>
                         </div>
                     </template>
                     <template v-else-if='mode === "users"'>
