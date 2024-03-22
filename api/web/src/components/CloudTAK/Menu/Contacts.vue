b77040a9e5a01894a9258d8d354b52d6a9342f8f
ingalls <nick@ingalls.ca>
2024-03-21 19:14:37 -0600

    Allow Searching for Contacts

--- api/web/src/components/CloudTAK/Menu/Contacts.vue
+++ api/web/src/components/CloudTAK/Menu/Contacts.vue
@@ -5,12 +5,17 @@
             <IconCircleArrowLeft @click='$router.back()' size='32' class='cursor-pointer'/>
             <div class='modal-title'>Contacts</div>
             <div class='btn-list'>
+                <IconSearch v-if='contacts.length' @click='search.shown = !search.shown' v-tooltip='"Search"' size='32' class='cursor-pointer'/>
                 <IconRefresh v-if='!loading' @click='fetchList' size='32' class='cursor-pointer' v-tooltip='"Refresh"'/>
             </div>
         </div>
     </div>
+    <div v-if='search.shown' class='col-12 px-3'>
+        <TablerInput v-model='search.filter' placeholder='Filter'/>
+    </div>
+
     <TablerLoading v-if='loading'/>
-    <TablerNone v-else-if='!contacts.length' :create='false'/>
+    <TablerNone v-else-if='!visibleContacts.length' :create='false'/>
     <template v-else>
         <div :key='a.id' v-for='a of visibleContacts' class="col-lg-12">
             <Contact @chat='$router.push(`/menu/chats/${$event}`)' :contact='a'/>
@@ -23,11 +28,13 @@
 import { std, stdurl } from '/src/std.ts';
 import {
     TablerNone,
+    TablerInput,
     TablerLoading
 } from '@tak-ps/vue-tabler';
 import Contact from '../partial/Contact.vue';
 import {
     IconRefresh,
+    IconSearch,
     IconCircleFilled,
     IconCircleArrowLeft
 } from '@tabler/icons-vue';
@@ -38,17 +45,29 @@ export default {
         return {
             err: false,
             loading: true,
-            contacts: []
+            contacts: [],
+            search: {
+                shown: false,
+                filter: ''
+            }
         }
     },
     mounted: async function() {
         await this.fetchList();
     },
+    watch: {
+        'search.shown': function() {
+            if (!this.search.shown) this.search.filter = '';
+        }
+    },
     computed: {
         visibleContacts: function() {
             return this.contacts.filter((contact) => {
                 return contact.callsign;
-            });
+            }).filter((contact) => {
+                if (this.search.shown) return contact.callsign.includes(this.search.filter);
+                return true;
+            })
         }
     },
     methods: {
@@ -62,8 +81,10 @@ export default {
     components: {
         Contact,
         TablerNone,
+        TablerInput,
         TablerLoading,
         IconRefresh,
+        IconSearch,
         IconCircleFilled,
         IconCircleArrowLeft,
     }
