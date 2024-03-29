20af0d5172ccb09bc1c549121c294a9ae150f92c
ingalls <nick@ingalls.ca>
2024-03-29 15:49:42 -0600

    Smarter Schema Save

--- api/web/src/components/Layer/LayerSchema.vue
+++ api/web/src/components/Layer/LayerSchema.vue
@@ -12,7 +12,7 @@
 
     <TablerLoading v-if='loading.schema' desc='Retrieving Schema'/>
     <TablerLoading v-else-if='loading.save' desc='Saving Schema'/>
-    <TablerNone v-else-if='!schema.length' label='Schema' :create='false'/>
+    <TablerNone v-else-if='!schema.length && disabled' label='Schema' :create='false'/>
     <div v-else class='table-responsive'>
         <table class="table table-hover card-table table-vcenter" :class='{
             "cursor-pointer": !disabled
@@ -168,6 +168,7 @@ export default {
             });
 
             this.loading.save = false;
+            this.disabled = true;
 
             this.$emit('layer', layer);
         },
