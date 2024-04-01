c950776e25da7422d6b7b1d550fd75fd16cc5cee
ingalls <nick@ingalls.ca>
2024-04-01 09:28:33 -0600

    Setup Default Values

--- api/lib/enums.ts
+++ api/lib/enums.ts
@@ -3,3 +3,28 @@ export enum Layer_Priority {
     LOW = 'low',
     OFF = 'off'
 }
+
+export enum Profile_Stale {
+    Immediate = 'Immediate',
+    TenMinutes = '10 Minutes',
+    ThirtyMinutes = '30 Minutes',
+    OneHour = '1 Hour',
+    Never = 'Never'
+};
+
+export enum Profile_Speed {
+    MS = 'm/s',
+    KMH = 'km/h',
+    MPH = 'mp/h'
+};
+
+export enum Profile_Distance {
+    METER = 'meter',
+    KILOMETER = 'kilometer',
+    MILE = 'mile'
+};
+
+export enum Profile_Elevation {
+    METER = 'meter',
+    FEET = 'feet'
+};
