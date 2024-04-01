7ee51779d7c3cd6b47279f7fc934e2462d8db1e1
ingalls <nick@ingalls.ca>
2024-04-01 09:33:40 -0600

    Fix lints

--- api/lib/enums.ts
+++ api/lib/enums.ts
@@ -10,21 +10,21 @@ export enum Profile_Stale {
     ThirtyMinutes = '30 Minutes',
     OneHour = '1 Hour',
     Never = 'Never'
-};
+}
 
 export enum Profile_Speed {
     MS = 'm/s',
     KMH = 'km/h',
     MPH = 'mp/h'
-};
+}
 
 export enum Profile_Distance {
     METER = 'meter',
     KILOMETER = 'kilometer',
     MILE = 'mile'
-};
+}
 
 export enum Profile_Elevation {
     METER = 'meter',
     FEET = 'feet'
-};
+}
