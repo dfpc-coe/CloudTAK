5ff08ca6ddabd476708c6567b8675d54a73ef79a
ingalls <nick@ingalls.ca>
2024-04-05 07:42:39 -0600

    Migrate Imports

--- api/web/src/components/CloudTAK/Menu/Imports.vue
+++ api/web/src/components/CloudTAK/Menu/Imports.vue
@@ -1,31 +1,27 @@
 <template>
-<div>
-    <div class='col-12 border-bottom border-light'>
-        <div class='modal-header px-0 mx-2'>
-            <IconCircleArrowLeft @click='$router.back()' size='32' class='cursor-pointer'/>
-            <div class='modal-title'>Data Imports</div>
-            <div class='btn-list'>
-                <IconRefresh v-if='!loading' @click='fetchList' size='32' class='cursor-pointer' v-tooltip='"Refresh"'/>
-            </div>
-        </div>
-    </div>
-    <TablerLoading v-if='loading'/>
-    <TablerNone v-else-if='!list.items.length' label='Imports' :create='false'/>
-    <template v-else>
-        <div @click='$router.push(`/menu/imports/${imported.id}`)' :key='imported.id' v-for='imported in list.items'>
-            <div class='cursor-pointer col-12 py-2 px-3 d-flex align-items-center hover-dark'>
-                <div class='col-auto'>
-                    <Status :status='imported.status'/>
-                </div>
-                <div class='mx-2 col-auto row'>
-                    <div style='width: 300px;' class='text-truncate' v-text='imported.name'></div>
-                    <div class='col-12 subheader' v-text='timeDiff(imported.created)'></div>
-                </div>
-            </div>
-        </div>
+<MenuTemplate name='Data Imports'>
+    <template #buttons>
+        <IconRefresh v-if='!loading' @click='fetchList' size='32' class='cursor-pointer' v-tooltip='"Refresh"'/>
     </template>
-    <TablerPager v-if='list.total > paging.limit' @page='paging.page = $event' :page='paging.page'  :total='list.total' :limit='paging.limit'/>
-</div>
+    <template #default>
+        <TablerLoading v-if='loading'/>
+        <TablerNone v-else-if='!list.items.length' label='Imports' :create='false'/>
+        <template v-else>
+            <div @click='$router.push(`/menu/imports/${imported.id}`)' :key='imported.id' v-for='imported in list.items'>
+                <div class='cursor-pointer col-12 py-2 px-3 d-flex align-items-center hover-dark'>
+                    <div class='col-auto'>
+                        <Status :status='imported.status'/>
+                    </div>
+                    <div class='mx-2 col-auto row' style='width: 300px;'>
+                        <div class='text-truncate' v-text='imported.name'></div>
+                        <div class='subheader' v-text='timeDiff(imported.created)'></div>
+                    </div>
+                </div>
+            </div>
+        </template>
+        <TablerPager v-if='list.total > paging.limit' @page='paging.page = $event' :page='paging.page'  :total='list.total' :limit='paging.limit'/>
+    </template>
+</MenuTemplate>
 </template>
 
 <script>
@@ -37,8 +33,8 @@ import {
 } from '@tak-ps/vue-tabler';
 import {
     IconRefresh,
-    IconCircleArrowLeft
 } from '@tabler/icons-vue';
+import MenuTemplate from '../util/MenuTemplate.vue';
 import Status from '../../util/Status.vue';
 import timeDiff from '../../../timediff.js';
 
@@ -87,7 +83,7 @@ export default {
         TablerPager,
         TablerLoading,
         IconRefresh,
-        IconCircleArrowLeft,
+        MenuTemplate,
     }
 }
 </script>
