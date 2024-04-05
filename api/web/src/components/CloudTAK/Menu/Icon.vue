10fe8aecdcb5182d1e15762bcaa8d39c72b880ea
ingalls <nick@ingalls.ca>
2024-04-05 07:38:39 -0600

    Migrate Icon Menus

--- api/web/src/components/CloudTAK/Menu/Icon.vue
+++ api/web/src/components/CloudTAK/Menu/Icon.vue
@@ -1,42 +1,38 @@
 <template>
-<div>
-    <div class='col-12 border-bottom border-light'>
-        <div class='modal-header px-0 mx-2'>
-            <IconCircleArrowLeft @click='$router.back()' size='32' class='cursor-pointer'/>
-            <div class='modal-title' v-text='icon.name'></div>
-            <div class='btn-list'>
-                <TablerDelete v-if='iconset.username || profile.system_admin' displaytype='icon' @delete='deleteIcon'/>
-                <IconSettings v-if='iconset.username || profile.system_admin' @click='editModal = icon' size='32' class='cursor-pointer'/>
-            </div>
+<MenuTemplate :name='icon.name'>
+    <template #buttons>
+        <TablerDelete v-if='iconset.username || profile.system_admin' displaytype='icon' @delete='deleteIcon'/>
+        <IconSettings v-if='iconset.username || profile.system_admin' @click='editModal = icon' size='32' class='cursor-pointer'/>
+    </template>
+    <template #default>
+        <div class='mx-4'>
+            <TablerLoading v-if='loading'/>
+            <template v-else>
+                <div class='pb-4'>
+                    <div class='d-flex justify-content-center mt-3'>
+                        <img :src='iconurl(icon)' width='64'>
+                    </div>
+                </div>
+                <div class="datagrid">
+                    <div class='datagrid-item'>
+                        <div class='datagrid-title'>Iconset</div>
+                        <div class='datagrid-content' v-text='icon.iconset'></div>
+                    </div>
+                    <div class='datagrid-item'>
+                        <div class='datagrid-title'>Name</div>
+                        <div class='datagrid-content' v-text='icon.name'></div>
+                    </div>
+                    <div class='datagrid-item'>
+                        <div class='datagrid-title'>Type 2525b</div>
+                        <div class='datagrid-content' v-text='icon.type2525b || "None"'></div>
+                    </div>
+                </div>
+            </template>
         </div>
-    </div>
-    <div class='mx-4'>
-        <TablerLoading v-if='loading'/>
-        <template v-else>
-            <div class='pb-4'>
-                <div class='d-flex justify-content-center mt-3'>
-                    <img :src='iconurl(icon)' width='64'>
-                </div>
-            </div>
-            <div class="datagrid">
-                <div class='datagrid-item'>
-                    <div class='datagrid-title'>Iconset</div>
-                    <div class='datagrid-content' v-text='icon.iconset'></div>
-                </div>
-                <div class='datagrid-item'>
-                    <div class='datagrid-title'>Name</div>
-                    <div class='datagrid-content' v-text='icon.name'></div>
-                </div>
-                <div class='datagrid-item'>
-                    <div class='datagrid-title'>Type 2525b</div>
-                    <div class='datagrid-content' v-text='icon.type2525b || "None"'></div>
-                </div>
-            </div>
-        </template>
-    </div>
+    </template>
+</MenuTemplate>
 
-    <IconEditModal v-if='editModal' :icon='editModal' @close='refresh'/>
-</div>
+<IconEditModal v-if='editModal' :icon='editModal' @close='refresh'/>
 </template>
 
 <script>
@@ -46,9 +42,9 @@ import {
     TablerLoading
 } from '@tak-ps/vue-tabler';
 import {
-    IconCircleArrowLeft,
     IconSettings,
 } from '@tabler/icons-vue';
+import MenuTemplate from '../util/MenuTemplate.vue';
 import IconEditModal from './Icon/EditModal.vue';
 import { mapState } from 'pinia';
 import { useProfileStore } from '/src/stores/profile.js';
@@ -102,7 +98,7 @@ export default {
         }
     },
     components: {
-        IconCircleArrowLeft,
+        MenuTemplate,
         IconEditModal,
         IconSettings,
         TablerDelete,
