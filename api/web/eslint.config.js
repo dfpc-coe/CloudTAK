4add81d1f8332f28e48876f6f6a775368a529701
ingalls <nick@ingalls.ca>
2024-04-15 10:24:47 -0600

    Add quote rules

--- api/web/eslint.config.js
+++ api/web/eslint.config.js
@@ -1,11 +1,11 @@
+import js from '@eslint/js';
 import pluginVue from 'eslint-plugin-vue'
 export default [
-  // add more generic rulesets here, such as:
-  // js.configs.recommended,
+  js.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
-  // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
   {
-    "rules": {
+    rules: {
+        "vue/html-quotes": ["error", "single", { "avoidEscape": false } ],
         "vue/multi-word-component-names": 1,
         "vue/no-multiple-template-root": 0,
         "vue/no-v-model-argument": 0,
