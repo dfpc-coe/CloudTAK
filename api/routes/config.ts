4a1c2f7a613456c493a424c6ad2515b2bb7fa221
ingalls <nick@ingalls.ca>
2024-04-17 13:35:22 -0600

    Update Config Lints

--- api/routes/config.ts
+++ api/routes/config.ts
@@ -2,9 +2,7 @@ import { Type } from '@sinclair/typebox'
 import Schema from '@openaddresses/batch-schema';
 import Err from '@openaddresses/batch-error';
 import Auth from '../lib/auth.js';
-import { sql } from 'drizzle-orm';
 import Config from '../lib/config.js';
-import { ServerResponse } from '../lib/types.js';
 
 export default async function router(schema: Schema, config: Config) {
     await schema.get('/config', {
