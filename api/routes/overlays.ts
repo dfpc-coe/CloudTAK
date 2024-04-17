50044b45fc78b860d57b6f4bb889f577e5ecd11c
ingalls <nick@ingalls.ca>
2024-04-17 15:37:50 -0600

    Fix Overlay Lints

--- api/routes/overlays.ts
+++ api/routes/overlays.ts
@@ -1,17 +1,11 @@
 import { Type } from '@sinclair/typebox'
 import { GenericListOrder } from '@openaddresses/batch-generic';
-import { validate } from '@maplibre/maplibre-gl-style-spec';
-import path from 'node:path';
 import Config from '../lib/config.js';
 import Schema from '@openaddresses/batch-schema';
-import S3 from '../lib/aws/s3.js';
 import Err from '@openaddresses/batch-error';
 import Auth from '../lib/auth.js';
-import { StandardResponse, OverlayResponse } from '../lib/types.js'
+import { OverlayResponse } from '../lib/types.js'
 import { sql } from 'drizzle-orm';
-import TAKAPI, {
-    APIAuthCertificate,
-} from '../lib/tak-api.js';
 
 export default async function router(schema: Schema, config: Config) {
     await schema.get('/overlay', {
@@ -31,7 +25,7 @@ export default async function router(schema: Schema, config: Config) {
 
     }, async (req, res) => {
         try {
-            const user = await Auth.as_user(config, req);
+            await Auth.is_auth(config, req);
 
             const overlays = await config.models.Overlay.list({
                 limit: req.query.limit,
