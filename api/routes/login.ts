2798f7b92c3bc71dfad771dcbdbbacf863b1d192
ingalls <nick@ingalls.ca>
2024-04-22 15:04:43 -0600

    Get from profile

--- api/routes/login.ts
+++ api/routes/login.ts
@@ -17,7 +17,6 @@ export default async function router(schema: Schema, config: Config) {
             password: Type.String()
         }),
         res: Type.Object({
-            id: Type.Union([Type.Integer(), Type.Null()]),
             token: Type.String(),
             access: Type.String(),
             email: Type.String()
@@ -45,10 +44,9 @@ export default async function router(schema: Schema, config: Config) {
 
             const profile = await config.models.Profile.from(email);
 
-            const id = profile.id;
             const access = profile.system_admin ? 'admin' : 'user';
 
-            return res.json({ id, access, email, token: jwt.sign({ access, email }, config.SigningSecret) })
+            return res.json({ access, email, token: jwt.sign({ access, email }, config.SigningSecret) })
         } catch (err) {
             Err.respond(err, res);
         }
@@ -58,7 +56,6 @@ export default async function router(schema: Schema, config: Config) {
         name: 'Get Login',
         group: 'Login',
         res: Type.Object({
-            id: Type.Union([Type.Integer(), Type.Null()]),
             email: Type.String(),
             access: Type.String()
         })
@@ -67,7 +64,6 @@ export default async function router(schema: Schema, config: Config) {
 
         try {
             return res.json({
-                id: user.id,
                 email: user.email,
                 access: user.access
             });
