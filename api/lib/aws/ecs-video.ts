31ff81ef71333f2bed623eaf4e90fa37f2c0e196
ingalls <nick@ingalls.ca>
2024-04-30 12:45:48 -0600

    Add Tasks List

--- api/lib/aws/ecs-video.ts
+++ api/lib/aws/ecs-video.ts
@@ -1,4 +1,8 @@
-import AWSECS, { ListTaskDefinitionsCommandInput } from '@aws-sdk/client-ecs';
+import AWSECS, {
+    ListTaskDefinitionsCommandInput,
+    ListTasksResponse,
+    ListTasksCommandInput
+} from '@aws-sdk/client-ecs';
 import Config from '../config.js';
 import Err from '@openaddresses/batch-error';
 import process from 'node:process';
@@ -34,8 +38,36 @@ export default class ECSVideo {
                 taskDefinitionArns.push(...res.taskDefinitionArns);
             } while (res.nextToken)
 
-            return res.taskDefinitionArns.map((def) => {
+            return taskDefinitionArns.map((def) => {
                 return Number(def.replace(/.*\/coe-media-.*:/, ''));
+            });
+        } catch (err) {
+            throw new Err(500, new Error(err instanceof Error ? err.message : String(err)), 'Failed to list ECR Tasks');
+        }
+    }
+
+    /**
+     * Return a list of Media Server Tasks
+     */
+    async tasks(): Promise<Array<string>> {
+        try {
+            const ecs = new AWSECS.ECSClient({ region: process.env.AWS_DEFAULT_REGION });
+            const taskArns: string[] = [];
+
+            let res;
+            do {
+                const req: ListTasksCommandInput = {
+                    cluster: `coe-ecs-${this.config.StackName.replace(/^coe-etl-/, '')}`,
+                    family: `coe-media-${this.config.StackName.replace(/^coe-etl-/, '')}`
+                };
+
+                if (res && res.nextToken) req.nextToken = res.nextToken;
+                res = await ecs.send(new AWSECS.ListTasksCommand(req));
+                taskArns.push(...res.taskArns);
+            } while (res.nextToken)
+
+            return taskArns.map((def) => {
+                return def;
             });
         } catch (err) {
             throw new Err(500, new Error(err instanceof Error ? err.message : String(err)), 'Failed to list ECR Tasks');
