9b7ca66abccd2cfa86951dea26158f99621dd42d
ingalls <nick@ingalls.ca>
2024-05-05 15:41:02 -0600

    Fix Lints

--- api/routes/profile-features.ts
+++ api/routes/profile-features.ts
@@ -1,18 +1,12 @@
 import { Type } from '@sinclair/typebox'
 import { Geometry } from 'geojson';
 import { GenericListOrder, GenerateUpsert } from '@openaddresses/batch-generic';
-import { validate } from '@maplibre/maplibre-gl-style-spec';
-import path from 'node:path';
 import Config from '../lib/config.js';
 import Schema from '@openaddresses/batch-schema';
-import S3 from '../lib/aws/s3.js';
 import Err from '@openaddresses/batch-error';
 import Auth from '../lib/auth.js';
 import { StandardResponse, ProfileFeatureResponse } from '../lib/types.js'
 import { sql } from 'drizzle-orm';
-import TAKAPI, {
-    APIAuthCertificate,
-} from '../lib/tak-api.js';
 
 export default async function router(schema: Schema, config: Config) {
     await schema.get('/profile/feature', {
