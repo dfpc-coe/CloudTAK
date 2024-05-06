eb59690a862c0f4f7f38807fc403b61cb365d072
ingalls <nick@ingalls.ca>
2024-05-06 13:11:19 -0600

    Use fixed filenames

--- api/web/vite.config.js
+++ api/web/vite.config.js
@@ -10,6 +10,15 @@ export default defineConfig((configEnv) => ({
     },
     server: {
         port: 8080,
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`
+            }
+        }
     }
 }))
 
