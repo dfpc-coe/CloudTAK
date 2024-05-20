c880fce1f827b1e39b940b6d9d2655474fdde99c
ingalls <nick@ingalls.ca>
2024-05-20 10:32:46 -0600

    Remove driver

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -3,7 +3,6 @@ import type { Config } from "drizzle-kit";
 export default {
     dialect: 'postgresql',
     schema: './lib/schema.ts',
-    driver: 'pg',
     dbCredentials: {
         url: process.env.POSTGRES || 'postgres://postgres@localhost:5432/tak_ps_etl',
     },
