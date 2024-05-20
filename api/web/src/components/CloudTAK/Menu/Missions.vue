a267bdb78bf605072ba4ecb14687254b4512e388
ingalls <nick@ingalls.ca>
2024-05-20 09:01:32 -0600

    add Mission Menu

--- api/web/src/components/CloudTAK/Menu/Missions.vue
+++ api/web/src/components/CloudTAK/Menu/Missions.vue
@@ -22,18 +22,12 @@
             @chat='$emit("chat", $event)'
             @close='mode = "list"'
         />
-        <Mission
-            v-else-if='mode === "edit"'
-            :initial='mission'
-            @close='mode = "list"'
-        />
     </TablerModal>
 </template>
 
 <script>
-import MissionList from '../Mission/MissionList.vue';
-import Mission from '../Mission/Mission.vue';
-import MissionCreate from '../Mission/MissionCreate.vue';
+import MissionList from './Mission/MissionList.vue';
+import MissionCreate from './Mission/MissionCreate.vue';
 import {
     TablerModal
 } from '@tak-ps/vue-tabler';
@@ -43,27 +37,16 @@ export default {
     components: {
         TablerModal,
         MissionCreate,
-        Mission,
         MissionList,
     },
     data: function() {
         return {
-            mode: 'list',
-            mission: false
+            mode: 'list'
         };
     },
     watch: {
-        mode: function() {
-            if (this.mode === 'list') {
-                this.mission = false;
-            }
-        },
         mission: function() {
-            if (this.mission) {
-                this.mode = 'edit';
-            } else {
-                this.mode = 'list';
-            }
+            this.mode = 'list';
         }
     }
 }
