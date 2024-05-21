d384a07292fd8e24b57d80f435d8b494e7336a35
ingalls <nick@ingalls.ca>
2024-05-21 10:59:26 -0600

    Get MissionInfo working

--- api/web/src/components/CloudTAK/Menu/Mission/MissionInfo.vue
+++ api/web/src/components/CloudTAK/Menu/Mission/MissionInfo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class='datagrid'>
-        <div class='datagrid-item pb-2'>
+        <div class='datagrid-item'>
             <div class='datagrid-title'>
                 Created
             </div>
@@ -9,7 +9,7 @@
                 v-text='mission.createTime'
             />
         </div>
-        <div class='datagrid-item pb-2'>
+        <div class='datagrid-item'>
             <div class='datagrid-title'>
                 Subscribers
             </div>
@@ -18,38 +18,6 @@
                 v-text='subscriptions.length'
             />
         </div>
-        <div class='datagrid-item pb-2'>
-            <div class='datagrid-title'>
-                Groups (Channels)
-            </div>
-            <div
-                class='datagrid-content'
-                v-text='mission.groups.join(", ")'
-            />
-        </div>
-    </div>
-    <div class='datagrid'>
-        <div class='datagrid-item pb-2'>
-            <div class='datagrid-title'>
-                Description
-            </div>
-            <div
-                class='datagrid-content'
-                v-text='mission.description || "No Feed Description"'
-            />
-        </div>
-    </div>
-    <div class='datagrid d-flex'>
-        <div class='datagrid-item'>
-            <div class='datagrid-title'>
-                Created
-            </div>
-            <div
-                class='datagrid-content'
-                v-text='mission.createTime ? mission.createTime.replace(/T.*/, "") : "Unknown"'
-            />
-        </div>
-
         <div class='datagrid-item'>
             <div class='datagrid-title'>
                 Contents
@@ -59,6 +27,24 @@
                 v-text='Array.isArray(mission.content) ? mission.contents.length : 0 + " Items"'
             />
         </div>
+        <div class='datagrid-item'>
+            <div class='datagrid-title'>
+                Groups (Channels)
+            </div>
+            <div
+                class='datagrid-content'
+                v-text='mission.groups.join(", ")'
+            />
+        </div>
+        <div class='datagrid-item'>
+            <div class='datagrid-title'>
+                Description
+            </div>
+            <div
+                class='datagrid-content'
+                v-text='mission.description || "No Feed Description"'
+            />
+        </div>
     </div>
 </template>
 
@@ -66,7 +52,8 @@
 export default {
     name: 'MissionInfo',
     props: {
-        mission: Object
+        mission: Object,
+        subscriptions: Array
     },
 }
 </script>
