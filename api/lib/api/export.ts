943ade31dc6e97ddb986a2d07c1ca5af8fb36c15
ingalls <nick@ingalls.ca>
2024-05-24 16:28:09 -0600

    Add export page

---
+++ api/lib/api/export.ts
@@ -0,0 +1,38 @@
+import TAKAPI from '../tak-api.js';
+import { Type, Static } from '@sinclair/typebox';
+
+export const ExportInput = Type.Object({
+    startTime: Type.String(),
+    endTime: Type.String(),
+    groups: Type.Array(Type.String()),
+    format: Type.String({ enum: ['kmz', 'kml'] }),
+    interval: Type.Optional(Type.Number()),
+    multiTrackThreshold: Type.Optional(Type.String()),
+    extendedData: Type.Optional(Type.Boolean()),
+    optimizeExport: Type.Optional(Type.Boolean()),
+});
+
+/**
+ * @class
+ */
+export default class {
+    api: TAKAPI;
+
+    constructor(api: TAKAPI) {
+        this.api = api;
+    }
+
+    async export(query: Static<typeof ExportInput>): Promise<Buffer> {
+        const url = new URL(`/Marti/ExportMissionKML`, this.api.url);
+
+        const params = new URLSearchParams();
+        for (const q in query) params.append(q, String(query[q]));
+
+        const res = await this.api.fetch(url, {
+            method: 'POST',
+            body: params
+        });
+
+        return await res.body();
+    }
+}
---
+++ api/web/src/components/Admin/AdminExport.vue
@@ -0,0 +1,73 @@
+<template>
+<div>
+    <div class='card-header'>
+        <h3 class='card-title'>
+            Data Export
+        </h3>
+    </div>
+    <TablerLoading v-if='loading'/>
+    <div class='card-body row g-2'>
+        <div class='col-6'>
+            <TablerInput label='Start Time' v-model='data.startTime' type='datetime-local'/>
+        </div>
+        <div class='col-6'>
+            <TablerInput label='End Time' v-model='data.endTime' type='datetime-local'/>
+        </div>
+        <div class='col-12'>
+            <TablerEnum label='Export Format' v-model='data.format' :options='["kmz", "kml"]'/>
+        </div>
+        <div class='col-12'>
+            <TablerToggle label='Extended Data (Disabled)' disabled v-model='data.extendedData'/>
+        </div>
+        <div class='col-12'>
+            <TablerToggle label='Optimize Export' v-model='data.optimizeExport'/>
+        </div>
+        <div class='col-12 d-flex py-2'>
+            <div class='ms-auto'>
+                <button @click='postExport' class='btn btn-primary'>Export</button>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import { std, stdurl } from '/src/std.ts';
+import {
+    TablerLoading,
+    TablerToggle,
+    TablerEnum,
+    TablerInput
+} from '@tak-ps/vue-tabler';
+
+export default {
+    name: 'AdminExport',
+    components: {
+        TablerLoading,
+        TablerToggle,
+        TablerEnum,
+        TablerInput,
+    },
+    data: function() {
+        return {
+            loading: false,
+            data: {
+                startTime: '',
+                endTime: '',
+                format: 'kmz',
+                optimizeExport: true
+            }
+        }
+    },
+    methods: {
+        postexport: async function() {
+            this.loading = true;
+            await std(`/api/marti/export`, {
+                method: 'POST',
+                body: this.data
+            });
+            this.loading = false;
+        }
+    }
+}
+</script>
