bc9b15ec3d291df02f37f3c61bfed1b4a95b0f2e
ingalls <nick@ingalls.ca>
2024-06-03 12:44:07 -0600

    add Token col

--- api/routes/profile-overlays.ts
+++ api/routes/profile-overlays.ts
@@ -157,7 +157,13 @@ export default async function router(schema: Schema, config: Config) {
                 const api = await TAKAPI.init(new URL(String(config.server.api)), new APIAuthCertificate(profile.auth.cert, profile.auth.key));
 
                 const mission = await api.Mission.getGuid(overlay.mode_id, {});
-                await api.Mission.subscribe(mission.name, { uid: user.email });
+                const sub = await api.Mission.subscribe(mission.name, { uid: user.email });
+
+                if (sub.data.length) {
+                    await config.models.ProfileOverlay.commit({
+
+                    })
+                }
             }
 
             return res.json(overlay);
