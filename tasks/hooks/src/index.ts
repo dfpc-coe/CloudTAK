9a89ccd3b0ca31865115c68666d27d0119381bb2
ingalls <nick@ingalls.ca>
2024-06-17 18:32:52 -0600

    Switch to allSettled

--- tasks/hooks/src/index.ts
+++ tasks/hooks/src/index.ts
@@ -16,7 +16,7 @@ export async function handler(
 
     for (const record of event.Records) {
         pool.push(
-            (async () => {
+            (async (record: Lambda.SQSRecord) => {
                 try {
                     const req = JSON.parse(record.body);
 
@@ -34,11 +34,11 @@ export async function handler(
                     const m = meta.get(record.messageId);
                     if (m) m.Error = new Error(String(err));
                 }
-            })()
+            })(record)
         )
     }
 
-    await Promise.all(pool);
+    await Promise.allSettled(pool);
 
     if (process.env.StackName) {
         const MetricData = event.Records.filter((record: Lambda.SQSRecord) => {
