f71ecf5e9f3dcdac3ae9518d7b51c15c027d3761
ingalls <nick@ingalls.ca>
2024-06-21 08:55:10 -0600

    Diff on MissionLayer

--- api/lib/api/mission-layer.ts
+++ api/lib/api/mission-layer.ts
@@ -2,6 +2,8 @@ import TAKAPI from '../tak-api.js';
 import { TAKList } from './types.js';
 import { Type, Static } from '@sinclair/typebox';
 import { MissionOptions } from './mission.js';
+import Err from '@openaddresses/batch-error';
+import { Feature } from '@tak-ps/node-cot';
 
 export enum MissionLayerType {
     GROUP = 'GROUP',
@@ -18,10 +20,21 @@ export const MissionLayer = Type.Object({
     uid: Type.String(),
     mission_layers: Type.Array(Type.Any()),
     uids: Type.Array(Type.Object({
-        data: Type.String(),
+        data: Type.String({
+            description: 'The UID of the COT'
+        }),
         timestamp: Type.String(),
         creatorUid: Type.String(),
-        keywords: Type.Array(Type.String())
+        keywords: Type.Optional(Type.Array(Type.String())),
+        detail: Type.Optional(Type.Object({
+            type: Type.String(),
+            callsign: Type.String(),
+            color: Type.String(),
+            location: Type.Object({
+                lat: Type.Number(),
+                lon: Type.Number()
+            })
+        }))
     })),
     contents: Type.Array(Type.Any()),
     maplayers: Type.Array(Type.Any())
@@ -77,7 +90,7 @@ export default class {
             method: 'GET',
             headers: this.#headers(opts),
         });
-    
+
         res.data.map((l) => {
             if (l.type === MissionLayerType.UID && !l.uids) {
                 l.uids = [];
@@ -86,6 +99,43 @@ export default class {
         })
 
         return res;
+    }
+
+    /**
+     * Stopgap function until the main latestFeats function can accept a path
+     * parameter
+     */
+    async latestFeats(
+        name: string,
+        layerUid: string, // Layer UID
+        opts?: Static<typeof MissionOptions>
+    ): Promise<Static<typeof Feature>[]> {
+        const layer = await this.get(name, layerUid, opts);
+        const feats = await this.api.Mission.latestFeats(name, opts);
+
+        const layerUids = new Set(layer.uids.map((u) => {
+            return u.data
+        }));
+
+        return feats.filter((f) => {
+            return layerUids.has(f.id)
+        });
+    }
+
+    async get(
+        name: string,
+        layerUid: string, // Layer UID
+        opts?: Static<typeof MissionOptions>
+    ): Promise<Static<typeof MissionLayer>> {
+        const layers = await this.list(name, opts);
+
+        for (const layer of layers.data) {
+            if (layer.uid === layerUid) {
+                return layer;
+            }
+        }
+
+        throw new Err(404, null, `Layer ${layerUid} not found`);
     }
 
     async create(
