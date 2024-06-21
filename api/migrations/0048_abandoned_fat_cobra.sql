191ddcd9ad9bf4905e95fc7bc08228b9969ccae4
ingalls <nick@ingalls.ca>
2024-06-21 14:21:48 -0600

    DB Migration

---
+++ api/migrations/0048_abandoned_fat_cobra.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "tasks" ADD COLUMN "created" timestamp with time zone DEFAULT Now() NOT NULL;--> statement-breakpoint
+ALTER TABLE "tasks" ADD COLUMN "updated" timestamp with time zone DEFAULT Now() NOT NULL;
