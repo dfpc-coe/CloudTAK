ebfbef172caf1329ce2c5c2b7274d54b7f81e256
ingalls <nick@ingalls.ca>
2024-07-16 09:40:51 -0600

    Add Forward Geocode

--- api/lib/geocode.ts
+++ api/lib/geocode.ts
@@ -12,15 +12,50 @@ const ReverseContainer = Type.Object({
     address: FetchReverse
 });
 
-export const FetchForward = Type.Object({
+
+export const FetchSuggest = Type.Object({
+    text: Type.String(),
+    magicKey: Type.String(),
+    isCollection: Type.Boolean()
 });
+
+export const SuggestContainer = Type.Object({
+    suggestions: Type.Array(FetchSuggest)
+})
+
+export const FetchForward = Type.Object({
+    address: Type.String(),
+    location: Type.Object({
+        x: Type.Number(),
+        y: Type.Number(),
+    }),
+    score: Type.Integer(),
+    attributes: Type.Object({
+        LongLabel: Type.Optional(Type.String()),
+        ShortLabel: Type.Optional(Type.String()),
+    }),
+    extent: Type.Object({
+        xmin: Type.Number(),
+        ymin: Type.Number(),
+        xmax: Type.Number(),
+        ymax: Type.Number()
+    })
+});
+
+export const ForwardContainer = Type.Object({
+    candidates: Type.Array(FetchForward)
+})
 
 export default class Geocode {
     reverseApi: string;
+    suggestApi: string;
+    forwardApi: string;
     token?: string;
 
     constructor(token?: string) {
         this.reverseApi = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode';
+        this.suggestApi = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest';
+        this.forwardApi = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates'
         this.token = token;
     }
 
@@ -36,7 +71,29 @@ export default class Geocode {
         return body.address;
     }
 
-    async forward(): Promise<Static<typeof FetchForward>> {
-        throw new Err(400, null, 'Unimplemented');
+    async forward(query: string, magicKey: string): Promise<Array<Static<typeof FetchForward>>> {
+        const url = new URL(this.forwardApi)
+        url.searchParams.append('magicKey', magicKey);
+        url.searchParams.append('singleLine', query);
+        url.searchParams.append('token', this.token);
+        url.searchParams.append('f', 'json');
+
+        const res = await fetch(url);
+
+        const body = await res.typed(ForwardContainer)
+
+        return body.candidates;
+    }
+
+    async suggest(query: string): Promise<Array<Static<typeof FetchSuggest>>> {
+        const url = new URL(this.suggestApi)
+        url.searchParams.append('text', query);
+        url.searchParams.append('f', 'json');
+
+        const res = await fetch(url);
+
+        const body = await res.typed(SuggestContainer)
+
+        return body.suggestions;
     }
 }
--- api/routes/search.ts
+++ api/routes/search.ts
@@ -3,7 +3,7 @@ import Schema from '@openaddresses/batch-schema';
 import Err from '@openaddresses/batch-error';
 import Auth from '../lib/auth.js';
 import Weather, { FetchHourly } from '../lib/weather.js';
-import Geocode, { FetchReverse } from '../lib/geocode.js';
+import Geocode, { FetchReverse, FetchSuggest, FetchForward } from '../lib/geocode.js';
 import Config from '../lib/config.js';
 
 export default async function router(schema: Schema, config: Config) {
@@ -11,12 +11,20 @@ export default async function router(schema: Schema, config: Config) {
 
     const geocode = new Geocode('');
     if (await config.models.Setting.typed('agol::enabled', false)) {
-        geocode.token = await config.models.Setting.typed('agol::token', '');
+        geocode.token = (await config.models.Setting.typed('agol::token', '')).value;
     }
 
     const ReverseResponse = Type.Object({
         weather: Type.Union([FetchHourly, Type.Null()]),
         reverse: Type.Union([FetchReverse, Type.Null()])
+    });
+
+    const SuggestResponse = Type.Object({
+        items: Type.Array(FetchSuggest)
+    });
+
+    const ForwardResponse = Type.Object({
+        items: Type.Array(FetchForward)
     });
 
     await schema.get('/search/reverse/:longitude/:latitude', {
@@ -54,6 +62,59 @@ export default async function router(schema: Schema, config: Config) {
                     }
                 })()
             ])
+
+            return res.json(response);
+        } catch (err) {
+            return Err.respond(err, res);
+        }
+    });
+
+    await schema.get('/search/forward', {
+        name: 'Forward',
+        group: 'Search',
+        description: 'Get information about a given string',
+        query: Type.Object({
+            query: Type.String(),
+            magicKey: Type.String(),
+        }),
+        res: ForwardResponse
+    }, async (req, res) => {
+        try {
+            await Auth.as_user(config, req);
+
+            const response: Static<typeof ForwardResponse> = {
+                items: [],
+            };
+
+            if (geocode.token && req.query.query.trim().length) {
+                response.items = await geocode.forward(req.query.query, req.query.magicKey);
+            }
+
+            return res.json(response);
+        } catch (err) {
+            return Err.respond(err, res);
+        }
+    });
+
+    await schema.get('/search/suggest', {
+        name: 'Suggest',
+        group: 'Search',
+        description: 'Get information about a given string',
+        query: Type.Object({
+            query: Type.String(),
+        }),
+        res: SuggestResponse
+    }, async (req, res) => {
+        try {
+            await Auth.as_user(config, req);
+
+            const response: Static<typeof SuggestResponse> = {
+                items: [],
+            };
+
+            if (geocode.token && req.query.query.trim().length) {
+                response.items = await geocode.suggest(req.query.query);
+            }
 
             return res.json(response);
         } catch (err) {
