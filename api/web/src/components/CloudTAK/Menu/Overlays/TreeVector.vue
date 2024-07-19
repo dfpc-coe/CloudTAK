73b46f5b7b05d1025849d9f8a84a2672364cf103
ingalls <nick@ingalls.ca>
2024-07-19 17:45:45 -0600

    Setup Layer Tree

--- api/web/src/components/CloudTAK/Menu/Overlays/TreeVector.vue
+++ api/web/src/components/CloudTAK/Menu/Overlays/TreeVector.vue
@@ -1,54 +1,65 @@
 <template>
-    <div>
-        <OverlayLayer
-            v-if='layer'
-            :layer='layer'
-            @close='layer = false'
-        />
+<div class='ms-3'>
+    <div
+        :key='l.id'
+        v-for='l of overlay._layers'
+    >
+        <template v-if='["fill", "line", "circle"].includes(l.type)'>
+            <div
+                class='cursor-pointer align-items-center px-3 py-2 me-2 hover-button'
+                @click='treeState[l.id] = true'
+            >
+                <IconChevronRight
+                    v-if='!treeState[l.id]'
+                    :size='20'
+                    :stroke='1'
+                    class='cursor-pointer'
+                    @click='treeState[l.id] = true'
+                />
+                <IconChevronDown
+                    v-else-if='treeState[l.id]'
+                    :size='20'
+                    :stroke='1'
+                    class='cursor-pointer'
+                    @click='treeState[l.id] = false'
+                />
+                <IconPaint
+                    v-if='l.type === "fill"'
+                    :size='24'
+                    :stroke='1'
+                />
+                <IconLine
+                    v-else-if='l.type === "line"'
+                    :size='24'
+                    :stroke='1'
+                />
+                <IconCircle
+                    v-else-if='l.type === "circle"'
+                    :size='24'
+                    :stroke='1'
+                />
 
-        <template v-else>
-            <div class='ms-3'>
-                <div
-                    :key='l.id'
-                    v-for='l of overlay._layers'
-                >
-                    <template v-if='["fill", "line", "circle"].includes(l.type)'>
-                        <div
-                            class='cursor-pointer align-items-center px-3 py-2 me-2 hover-button'
-                            @click='layer = l'
-                        >
-                            <IconPaint
-                                v-if='l.type === "fill"'
-                                :size='24'
-                                :stroke='1'
-                            />
-                            <IconLine
-                                v-else-if='l.type === "line"'
-                                :size='24'
-                                :stroke='1'
-                            />
-                            <IconCircle
-                                v-else-if='l.type === "circle"'
-                                :size='24'
-                                :stroke='1'
-                            />
+                <span
+                    class='user-select-none mx-2'
+                    v-text='l.id || l.name'
+                />
 
-                            <span
-                                class='user-select-none mx-2'
-                                v-text='l.id || l.name'
-                            />
-                        </div>
-                    </template>
-                </div>
+                <OverlayLayer
+                    v-if='layer'
+                    :layer='l'
+                />
             </div>
         </template>
     </div>
+</div>
 </template>
 
 <script>
 import MenuTemplate from '../../util/MenuTemplate.vue';
 import OverlayLayer from './Layer.vue';
 import {
+    IconChevronDown,
+    IconChevronRight,
     IconPaint,
     IconLine,
     IconCircle,
@@ -67,15 +78,18 @@ export default {
     data: function() {
         return {
             err: false,
-            loading: false,
-            layer: false
+            treeState: {},
         }
     },
-    computed: {
-        ...mapState(useMapStore, ['layers'])
+    mounted: function() {
+        for (const layer of this.overlay._layers) {
+            this.treeState[layer.id] = false;
+        }
     },
     components: {
         OverlayLayer,
+        IconChevronDown,
+        IconChevronRight,
         IconPaint,
         IconLine,
         IconCircle,
