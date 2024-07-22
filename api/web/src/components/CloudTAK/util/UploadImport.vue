a179960f968b029194452c87429c0ddfe8fc7000
ingalls <nick@ingalls.ca>
2024-07-22 13:20:02 -0600

    Open in imports

--- api/web/src/components/CloudTAK/util/UploadImport.vue
+++ api/web/src/components/CloudTAK/util/UploadImport.vue
@@ -7,35 +7,34 @@
             <div class='col-12 d-flex justify-content-center mb-3 subheader'>
                 Select a file to import
             </div>
-            <div 
+            <div
                 ref='dragger'
-                :ondragenter='dragEnter'
-                :ondragleave='dragLeave'
-                :drop='dragDrop'
+                @dragleave.prevent='dragLeave'
+                @dragenter.prevent='dragEnter'
+                @drop.prevent='dragDrop()'
                 class='custom-drop row g-0 d-flex col-12 justify-content-center py-2'
             >
-                <div class='col-12 my-3 d-flex justify-content-center'>
-                    <div>Drag &amp; Drop Files Here</div>
-                </div>
-                <div class='col-12 d-flex justify-content-center'>
-                    <div>or</div>
-                </div>
-                <div class='col-12 d-flex justify-content-center'>
-                    <div class='btn-list my-3'>
-                        <button
-                            v-if='cancelButton'
-                            class='btn btn-secondary'
-                            @click='$emit("cancel")'
-                        >
-                            Cancel
-                        </button>
-                        <button
-                            class='btn btn-primary'
-                            @click='$refs.fileInput.click()'
-                        >
-                            Select Files
-                        </button>
+                <div style='pointer-events: none;'>
+                    <div class='col-12 my-3 d-flex justify-content-center'>
+                        <div>Drag &amp; Drop Files Here</div>
                     </div>
+                </div>
+            </div>
+            <div class='col-12 d-flex justify-content-center'>
+                <div class='btn-list my-3'>
+                    <button
+                        v-if='cancelButton'
+                        class='btn btn-secondary'
+                        @click='$emit("cancel")'
+                    >
+                        Cancel
+                    </button>
+                    <button
+                        class='btn btn-primary'
+                        @click='$refs.fileInput.click()'
+                    >
+                        Manually Select Files
+                    </button>
                 </div>
             </div>
             <form>
@@ -45,7 +44,7 @@
                     class='d-none'
                     type='file'
                     name='file'
-                    @change='upload'
+                    @change='manualUpload'
                 >
             </form>
         </div>
@@ -114,20 +113,24 @@ export default {
             input.type = 'file';
         },
         dragEnter: function(event) {
-            event.stopPropagation();
-            event.preventDefault();
             this.$refs.dragger.classList.add('custom-drop-drag')
         },
         dragLeave: function(event) {
-            event.stopPropagation();
-            event.preventDefault();
             this.$refs.dragger.classList.remove('custom-drop-drag')
         },
-        dragDrop: function() {
-            
+        dragDrop: async function(event) {
+            console.error('HERE');
+
+            const dt = event.dataTransfer
+            const file = dt.files[0];
+
+            await this.upload(file);
         },
-        upload: async function(event) {
+        manualUpload: async function(event) {
             const file = event.target.files[0];
+            await this.upload(file);
+        },
+        upload: async function(file) {
             this.name = file.name;
 
             const imported = await std('/api/import', {
