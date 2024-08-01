c7e12af0a5e85bb7c341245b6c9220bd7304ea4b
ingalls <nick@ingalls.ca>
2024-08-01 15:00:31 -0600

    Migrate Files

--- api/web/src/components/CloudTAK/Menu/Files.vue
+++ api/web/src/components/CloudTAK/Menu/Files.vue
@@ -1,14 +1,6 @@
 <template>
     <MenuTemplate name='User Files'>
         <template #buttons>
-            <IconPlus
-                v-if='!loading && !upload'
-                v-tooltip='"New Import"'
-                :size='32'
-                :stroke='1'
-                class='cursor-pointer'
-                @click='upload = true'
-            />
             <IconRefresh
                 v-if='!loading'
                 v-tooltip='"Refresh"'
@@ -19,50 +11,65 @@
             />
         </template>
         <template #default>
-            <div
-                v-if='upload'
-                class='py-2 px-4'
-            >
-                <Upload
-                    :url='uploadURL()'
-                    :headers='uploadHeaders()'
-                    method='PUT'
-                    @cancel='upload = false'
-                    @done='uploadComplete($event)'
-                />
-            </div>
-
             <TablerLoading v-if='loading' />
             <TablerNone
-                v-else-if='!list.items.length'
+                v-else-if='!list.assets.length'
                 label='Imports'
                 :create='false'
             />
             <template v-else>
                 <div
-                    v-for='imported in list.items'
-                    :key='imported.id'
-                    @click='$router.push(`/menu/imports/${imported.id}`)'
+                    v-for='asset in list.assets'
+                    :key='asset.name'
                 >
-                    <div class='cursor-pointer col-12 py-2 px-3 d-flex align-items-center hover-dark'>
+                    <div class='col-12 py-2 px-3 d-flex align-items-center hover-dark'>
                         <div class='col-auto'>
-                            <Status
-                                :dark='true'
-                                :status='imported.status'
+                            <IconMap
+                                v-if='asset.visualized'
+                                v-tooltip='"Visualizable"'
+                                :size='32'
+                                :stroke='1'
+                            />
+                            <IconMapOff
+                                v-else
+                                v-tooltip='"Not Cloud Optimized"'
+                                :size='32'
+                                :stroke='1'
                             />
                         </div>
-                        <div
-                            class='mx-2 col-auto row'
-                            style='width: 300px;'
-                        >
-                            <div
-                                class='text-truncate'
-                                v-text='imported.name'
-                            />
-                            <div
-                                class='subheader'
-                                v-text='timeDiff(imported.created)'
-                            />
+                        <div class='col-auto'>
+                            <div class='col-12'>
+                                <span class='mx-2' v-text='asset.name'/>
+                            </div>
+                            <div class='col-12 subheader'>
+                                <span class='mx-2'>
+                                    <TablerBytes :bytes='asset.size' /> - <TablerEpoch :date='asset.updated' />
+                                </span>
+                            </div>
+                        </div>
+                        <div class='col-auto ms-auto'>
+                            <div class='d-flex btn-list'>
+                                <TablerDelete
+                                    v-tooltip='"Delete Asset"'
+                                    displaytype='icon'
+                                    @delete='deleteAsset(asset)'
+                                />
+                                <IconTransform
+                                    v-if='!asset.visualized'
+                                    v-tooltip='"Convert Asset"'
+                                    :size='32'
+                                    :stroke='1'
+                                    class='cursor-pointer'
+                                    @click='initTransform(asset)'
+                                />
+                                <IconDownload
+                                    v-tooltip='"Download Asset"'
+                                    :size='32'
+                                    :stroke='1'
+                                    class='cursor-pointer'
+                                    @click='downloadAsset(asset)'
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -84,12 +91,19 @@
 <script>
 import { std, stdurl } from '/src/std.ts';
 import {
+    TablerDelete,
     TablerNone,
     TablerPager,
-    TablerLoading
+    TablerLoading,
+    TablerBytes,
+    TablerEpoch
 } from '@tak-ps/vue-tabler';
 import {
     IconPlus,
+    IconMap,
+    IconMapOff,
+    IconTransform,
+    IconDownload,
     IconRefresh,
 } from '@tabler/icons-vue';
 import MenuTemplate from '../util/MenuTemplate.vue';
@@ -105,20 +119,28 @@ export default {
         TablerNone,
         TablerPager,
         TablerLoading,
+        TablerBytes,
+        TablerEpoch,
+        TablerDelete,
+        IconMap,
+        IconMapOff,
         IconPlus,
+        IconTransform,
+        IconDownload,
         IconRefresh,
         MenuTemplate,
     },
     data: function() {
         return {
-            upload: false,
             err: false,
             loading: true,
             paging: {
                 limit: 20,
                 page: 0
             },
-            list: []
+            list: {
+                assets: []
+            }
         }
     },
     watch: {
@@ -136,28 +158,39 @@ export default {
         timeDiff(update) {
             return timeDiff(update)
         },
-        uploadHeaders: function() {
-            return {
-                Authorization: `Bearer ${localStorage.token}`
-            };
-        },
-        uploadComplete: function(event) {
-            this.upload = false;
-            const imp = JSON.parse(event);
-            this.$router.push(`/menu/imports/${imp.imports[0].uid}`)
-        },
-        uploadURL: function() {
-            return stdurl(`/api/import`);
+        downloadAsset: async function(asset) {
+            const url = stdurl(`/api/profile/asset/${asset.name}`);
+            url.searchParams.append('token', localStorage.token);
+            window.open(url, "_blank")
         },
         fetchList: async function() {
+            this.upload = false;
+
+            try {
+                this.loading = true;
+                this.err = false;
+                this.list = await std(`/api/profile/asset`);
+                this.loading = false;
+            } catch (err) {
+                this.err = err;
+            }
+        },
+        deleteAsset: async function(asset) {
             this.loading = true;
-            const url = stdurl('/api/import');
-            url.searchParams.append('order', 'desc');
-            url.searchParams.append('page', this.paging.page);
-            url.searchParams.append('limit', this.paging.limit);
-            url.searchParams.append('sort', 'created');
-            this.list = await std(url);
-            this.loading = false;
+            await std(`/api/profile/asset/${asset.name}`, {
+                method: 'DELETE'
+            });
+
+            await this.fetchList();
+        },
+        initTransform: function(asset) {
+            if (!asset) {
+                this.transform.asset = {};
+                this.transform.shown = false;
+            } else {
+                this.transform.asset = asset;
+                this.transform.shown = true;
+            }
         },
     }
 }
--- api/web/src/components/Profile/ProfileFiles.vue
+++ api/web/src/components/Profile/ProfileFiles.vue
@@ -36,62 +36,6 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr
-                        v-for='asset in list.assets'
-                        :key='asset.name'
-                    >
-                        <td>
-                            <div class='d-flex align-items-center'>
-                                <div class='btn-list'>
-                                    <IconMap
-                                        v-if='asset.visualized'
-                                        v-tooltip='"Visualizable"'
-                                        :size='32'
-                                        :stroke='1'
-                                    />
-                                    <IconMapOff
-                                        v-else
-                                        v-tooltip='"Not Cloud Optimized"'
-                                        :size='32'
-                                        :stroke='1'
-                                    />
-                                </div>
-
-                                <span
-                                    class='mx-2'
-                                    v-text='asset.name'
-                                />
-                            </div>
-                        </td>
-                        <td>
-                            <TablerBytes :bytes='asset.size' />
-                        </td>
-                        <td class='d-flex align-items-center'>
-                            <TablerEpoch :date='asset.updated' />
-                            <div class='ms-auto btn-list'>
-                                <TablerDelete
-                                    v-tooltip='"Delete Asset"'
-                                    displaytype='icon'
-                                    @delete='deleteAsset(asset)'
-                                />
-                                <IconTransform
-                                    v-if='!asset.visualized'
-                                    v-tooltip='"Convert Asset"'
-                                    :size='32'
-                                    :stroke='1'
-                                    class='cursor-pointer'
-                                    @click='initTransform(asset)'
-                                />
-                                <IconDownload
-                                    v-tooltip='"Download Asset"'
-                                    :size='32'
-                                    :stroke='1'
-                                    class='cursor-pointer'
-                                    @click='downloadAsset(asset)'
-                                />
-                            </div>
-                        </td>
-                    </tr>
                 </tbody>
             </table>
         </div>
@@ -141,14 +85,6 @@ import {
 } from '@tabler/icons-vue'
 import TransformModal from './TransformModal.vue';
 import Upload from '../util/Upload.vue';
-import {
-    TablerAlert,
-    TablerNone,
-    TablerDelete,
-    TablerLoading,
-    TablerBytes,
-    TablerEpoch
-} from '@tak-ps/vue-tabler';
 
 export default {
     name: 'ProfileFiles',
@@ -200,41 +136,6 @@ export default {
         uploadURL: function() {
             return stdurl(`/api/profile/asset`);
         },
-        downloadAsset: async function(asset) {
-            const url = stdurl(`/api/profile/asset/${asset.name}`);
-            url.searchParams.append('token', localStorage.token);
-            window.open(url, "_blank")
-        },
-        initTransform: function(asset) {
-            if (!asset) {
-                this.transform.asset = {};
-                this.transform.shown = false;
-            } else {
-                this.transform.asset = asset;
-                this.transform.shown = true;
-            }
-        },
-        deleteAsset: async function(asset) {
-            this.loading.list = true;
-            await std(`/api/profile/asset/${asset.name}`, {
-                method: 'DELETE'
-            });
-
-            await this.fetchList();
-        },
-        fetchList: async function() {
-            this.upload = false;
-
-            try {
-                this.loading.list = true;
-                this.err = false;
-                this.list = await std(`/api/profile/asset`);
-                this.loading.list = false;
-                this.$emit('assets', this.list);
-            } catch (err) {
-                this.err = err;
-            }
-        }
     }
 }
 </script>
