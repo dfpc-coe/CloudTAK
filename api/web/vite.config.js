e3daf586ee895fc06f39f35d506d82d2285a06af
ingalls <nick@ingalls.ca>
2024-08-06 10:36:59 -0700

    Add PWA Basics

--- api/web/vite.config.js
+++ api/web/vite.config.js
@@ -1,9 +1,31 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig((configEnv) => ({
     plugins: [
         vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'CloudTAK',
+                short_name: 'CloudTAK',
+                description: 'Cloud powered in-browser TAK Client',
+                theme_color: '#ffffff',
+                icons: [{
+                    src: './oublic/logo.png',
+                    sizes: '1000x1062',
+                    type: 'image/png'
+                }]
+            },
+            devOptions: {
+                enabled: true
+            },
+            workbox: {
+                clientsClaim: true,
+                skipWaiting: true
+            }
+        })
     ],
     optimizeDeps: {
         include: ["showdown", "@tak-ps/vue-tabler"],
