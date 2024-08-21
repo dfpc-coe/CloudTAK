5f681683e96d6b425ef310974591b68c962b8a4b
ingalls <nick@ingalls.ca>
2024-08-21 14:20:12 -0600

    Move Lease into CloudTAK

--- api/web/src/components/Profile.vue
+++ api/web/src/components/Profile.vue
@@ -26,17 +26,6 @@
                                             <span
                                                 class='list-group-item list-group-item-action d-flex align-items-center'
                                                 :class='{
-                                                    "active": $route.name === "profile-videos",
-                                                    "cursor-pointer": $route.name !== "profile-videos"
-                                                }'
-                                                @click='$router.push(`/profile/videos`)'
-                                            ><IconVideo
-                                                :size='32'
-                                                :stroke='1'
-                                            /><span class='mx-3'>Video Leases</span></span>
-                                            <span
-                                                class='list-group-item list-group-item-action d-flex align-items-center'
-                                                :class='{
                                                     "active": $route.name === "profile-jobs",
                                                     "cursor-pointer": $route.name !== "profile-jobs"
                                                 }'
