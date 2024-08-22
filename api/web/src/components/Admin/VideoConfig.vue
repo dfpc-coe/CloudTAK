fc4bf63e62da92d0521f4d3a56bd88f7fef0641f
ingalls <nick@ingalls.ca>
2024-08-22 11:45:42 -0600

    Add Path API Creation

--- api/web/src/components/Admin/VideoConfig.vue
+++ api/web/src/components/Admin/VideoConfig.vue
@@ -1,58 +1,60 @@
 <template>
-    <div class='col-12 px-2 py-2 row g-2'>
-        <TablerToggle
-            v-model='config.api'
-            :disabled='disabled'
-            label='MediaMTX API Enabled'
-        />
+    <div class='col-12 py-2 g-2'>
+        <div class='row g-0'>
+            <TablerToggle
+                v-model='config.api'
+                :disabled='disabled'
+                label='Config API Enabled'
+            />
 
-        <TablerToggle
-            v-model='config.metrics'
-            :disabled='disabled'
-            label='MediaMTX Metrics Enabled'
-        />
+            <TablerToggle
+                v-model='config.metrics'
+                :disabled='disabled'
+                label='Metrics Enabled'
+            />
 
-        <TablerToggle
-            v-model='config.pprof'
-            :disabled='disabled'
-            label='Performance Metrics Enabled'
-        />
+            <TablerToggle
+                v-model='config.pprof'
+                :disabled='disabled'
+                label='Performance Metrics Enabled'
+            />
 
-        <TablerToggle
-            v-model='config.playback'
-            :disabled='disabled'
-            label='Playback API Enabled'
-        />
+            <TablerToggle
+                v-model='config.playback'
+                :disabled='disabled'
+                label='Playback API Enabled'
+            />
 
-        <TablerToggle
-            v-model='config.rtsp'
-            :disabled='disabled'
-            label='RTSP Endpoint Enabled'
-        />
+            <TablerToggle
+                v-model='config.rtsp'
+                :disabled='disabled'
+                label='RTSP Endpoint Enabled'
+            />
 
-        <TablerToggle
-            v-model='config.rtmp'
-            :disabled='disabled'
-            label='RTMP Endpoint Enabled'
-        />
+            <TablerToggle
+                v-model='config.rtmp'
+                :disabled='disabled'
+                label='RTMP Endpoint Enabled'
+            />
 
-        <TablerToggle
-            v-model='config.hls'
-            :disabled='disabled'
-            label='HLS Endpoint Enabled'
-        />
+            <TablerToggle
+                v-model='config.hls'
+                :disabled='disabled'
+                label='HLS Endpoint Enabled'
+            />
 
-        <TablerToggle
-            v-model='config.webrtc'
-            :disabled='disabled'
-            label='WebRTC Endpoint Enabled'
-        />
+            <TablerToggle
+                v-model='config.webrtc'
+                :disabled='disabled'
+                label='WebRTC Endpoint Enabled'
+            />
 
-        <TablerToggle
-            v-model='config.srt'
-            :disabled='disabled'
-            label='SRT Endpoint Enabled'
-        />
+            <TablerToggle
+                v-model='config.srt'
+                :disabled='disabled'
+                label='SRT Endpoint Enabled'
+            />
+        </div>
 
 
         <label class='subheader mx-2'>Server Paths</label>
@@ -63,8 +65,8 @@
             label='Server Paths'
         />
         <template v-else>
-            <div v-for='path in service.paths'>
-                <span v-text='path' />
+            <div v-for='path in service.paths' class='hover-light px-2 py-2'>
+                <span v-text='path.name' />
             </div>
         </template>
     </div>
