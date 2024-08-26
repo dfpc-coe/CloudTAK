c9e46a8074c0c7debd882093c6814f34ca8fbec0
ingalls <nick@ingalls.ca>
2024-08-26 10:46:27 -0600

    Update Course with Copy

---
+++ api/web/src/components/CloudTAK/util/CopyField.vue
@@ -0,0 +1,38 @@
+<template>
+    <div
+        class='bg-gray-500 rounded-top py-2 px-2 position-relative'
+    >
+        <span v-text='text' />
+        <CopyButton
+            :text='inMode'
+            class='position-absolute'
+            :size='24'
+            style='right: 8px'
+        />
+    </div>
+</template>
+
+<script>
+import CopyButton from './CopyButton.vue';
+
+export default {
+    name: 'CopyField',
+    components: {
+        CopyButton
+    },
+    props: {
+        text: {
+            type: [String, Number],
+            required: true
+        },
+        size: {
+            type: Number,
+            default: 32
+        },
+        stroke: {
+            type: Number,
+            default: 1
+        }
+    },
+}
+</script>
--- api/web/src/components/CloudTAK/util/Elevation.vue
+++ api/web/src/components/CloudTAK/util/Elevation.vue
@@ -2,17 +2,10 @@
     <div class='col-12'>
         <label class='subheader mx-2'>Elevation</label>
         <div class='mx-2'>
-            <div
-                class='bg-gray-500 rounded-top py-2 px-2 position-relative'
-            >
-                <span v-text='inMode' />
-                <CopyButton
-                    :text='inMode'
-                    class='position-absolute'
-                    :size='24'
-                    style='right: 8px'
-                />
-            </div>
+            <CopyField
+                :text='inMode'
+                :size='24'
+            />
             <span
                 v-tooltip='"Feet"'
                 class='my-1 px-2'
@@ -36,12 +29,12 @@
 </template>
 
 <script>
-import CopyButton from './CopyButton.vue';
+import CopyField from './CopyField.vue';
 
 export default {
     name: 'COTElevation',
     components: {
-        CopyButton
+        CopyField
     },
     props: {
         elevation: {
