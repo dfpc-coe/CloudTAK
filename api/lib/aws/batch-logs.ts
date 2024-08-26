49a0526ba61c642684acebc9e44b584e4933fbb3
ingalls <nick@ingalls.ca>
2024-08-25 18:28:41 -0600

    More permissive match

--- api/lib/aws/batch-logs.ts
+++ api/lib/aws/batch-logs.ts
@@ -31,7 +31,7 @@ export default class LogGroup {
                 })
             }
         } catch (err) {
-            if (String(err).startsWith('ResourceNotFoundException')) {
+            if (String(err).includes('ResourceNotFoundException')) {
                 return { logs: [] }
             } else {
                 throw new Error(err instanceof Error ? err.message : String(err));
