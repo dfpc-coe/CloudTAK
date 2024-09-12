90410f54d25aa1d7622cf91d99f3a8975d6ce898
ingalls <nick@ingalls.ca>
2024-09-12 13:23:24 -0600

    Store Fonts

--- api/web/src/components/CloudTAK/util/Video.vue
+++ api/web/src/components/CloudTAK/util/Video.vue
@@ -1,23 +1,30 @@
 <template>
     <div>
-        <video
-            id='cot-player'
-            class='video-js vjs-default-skin'
-            width='400'
-            height='300'
-            controls
-        >
-            <source
-                type='application/x-mpegURL'
-                :src='video'
+        <TablerLoading v-if='loading'/>
+        <template v-else>
+            <video
+                id='cot-player'
+                class='video-js vjs-default-skin'
+                width='400'
+                height='300'
+                controls
             >
-        </video>
+                <source
+                    type='application/x-mpegURL'
+                    :src='video'
+                >
+            </video>
+        </template>
     </div>
 </template>
 
-<script>
+<script lang='ts'>
+import { std } from '/src/std.ts';
 import videojs from 'video.js';
 import 'video.js/dist/video-js.css';
+import {
+    TablerLoading
+} from '@tak-ps/vue-tabler';
 
 export default {
     name: 'CoTVideo',
@@ -29,16 +36,37 @@ export default {
     },
     data: function() {
         return {
-            player: false
+            loading: true,
+            player: false,
+            lease: false,
         }
     },
     unmounted: function() {
         this.player.dispose();
     },
     mounted: async function() {
+        await this.requestLease();
+
+
+        this.loading = false;
         this.$nextTick(() => {
             this.player = videojs('cot-player');
         });
+    },
+    methods: {
+        requestLease: async function() {
+            this.lease = await std('/api/video/lease', {
+                method: 'POST',
+                body:  {
+                    name: 'Temporary Lease',
+                    duration: 1 * 60 * 60,
+                    proxy: this.video
+                }
+            })
+        }
+    },
+    components: {
+        TablerLoading
     }
 }
 </script>
