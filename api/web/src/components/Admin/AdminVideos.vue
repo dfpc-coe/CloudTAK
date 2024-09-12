629921da35fb8fb05b4161369d0f4d94e6cb196e
ingalls <nick@ingalls.ca>
2024-09-12 16:04:27 -0600

    Enable HLS

--- api/web/src/components/Admin/AdminVideos.vue
+++ api/web/src/components/Admin/AdminVideos.vue
@@ -7,6 +7,14 @@
             </h1>
 
             <div class='ms-auto btn-list'>
+                <IconSettings
+                    v-if='disabled'
+                    v-tooltip='"Edit"'
+                    :size='32'
+                    :stroke='1'
+                    class='cursor-pointer'
+                    @click='disabled = !disabled'
+                />
                 <IconRefresh
                     v-tooltip='"Refresh"'
                     :size='32'
@@ -25,84 +33,89 @@
             />
             <VideoConfig
                 v-else
+                @cancel='disabled = true'
                 :service='service'
+                :disabled='disabled'
             />
         </div>
-        <div class='card-header'>
-            <h1 class='card-title'>
-                Individual Video Servers
-            </h1>
 
-            <div class='ms-auto btn-list'>
-                <IconPlus
-                    v-if='list.versions.length'
-                    v-tooltip='"Create Server"'
-                    :size='32'
-                    :stroke='1'
-                    class='cursor-pointer'
-                    @click='createServer'
-                />
+        <template v-if='disabled'>
+            <div class='card-header'>
+                <h1 class='card-title'>
+                    Individual Video Servers
+                </h1>
 
-                <IconRefresh
-                    v-tooltip='"Refresh"'
-                    :size='32'
-                    :stroke='1'
-                    class='cursor-pointer'
-                    @click='fetchList'
+                <div class='ms-auto btn-list'>
+                    <IconPlus
+                        v-if='list.versions.length'
+                        v-tooltip='"Create Server"'
+                        :size='32'
+                        :stroke='1'
+                        class='cursor-pointer'
+                        @click='createServer'
+                    />
+
+                    <IconRefresh
+                        v-tooltip='"Refresh"'
+                        :size='32'
+                        :stroke='1'
+                        class='cursor-pointer'
+                        @click='fetchList'
+                    />
+                </div>
+            </div>
+            <div>
+                <TablerLoading v-if='loading.tasks' />
+                <TablerNone
+                    v-else-if='!list.items.length'
+                    label='Video Servers'
+                    :create='false'
                 />
+                <div
+                    v-else
+                    class='table-responsive'
+                >
+                    <table class='table card-table table-hover table-vcenter datatable'>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Version</th>
+                                <th>Created</th>
+                                <th>CPU</th>
+                                <th>Memory</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr
+                                v-for='server in list.items'
+                                :key='server.id'
+                                class='cursor-pointer'
+                                @click='$router.push(`/admin/video/${server.id}`)'
+                            >
+                                <td class='d-flex align-items-center'>
+                                    <Status
+                                        v-if='server.status === "RUNNING"'
+                                        status='Success'
+                                    />
+                                    <Status
+                                        v-else
+                                        :status='server.status'
+                                    />
+                                    <span
+                                        class='mx-2'
+                                        v-text='server.id'
+                                    />
+                                </td>
+                                <td v-text='server.version' />
+                                <td v-text='server.created' />
+                                <td v-text='server.cpu' />
+                                <td v-text='server.memory' />
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-        <div>
-            <TablerLoading v-if='loading.tasks' />
-            <TablerNone
-                v-else-if='!list.items.length'
-                label='Video Servers'
-                :create='false'
-            />
-            <div
-                v-else
-                class='table-responsive'
-            >
-                <table class='table card-table table-hover table-vcenter datatable'>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Version</th>
-                            <th>Created</th>
-                            <th>CPU</th>
-                            <th>Memory</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr
-                            v-for='server in list.items'
-                            :key='server.id'
-                            class='cursor-pointer'
-                            @click='$router.push(`/admin/video/${server.id}`)'
-                        >
-                            <td class='d-flex align-items-center'>
-                                <Status
-                                    v-if='server.status === "RUNNING"'
-                                    status='Success'
-                                />
-                                <Status
-                                    v-else
-                                    :status='server.status'
-                                />
-                                <span
-                                    class='mx-2'
-                                    v-text='server.id'
-                                />
-                            </td>
-                            <td v-text='server.version' />
-                            <td v-text='server.created' />
-                            <td v-text='server.cpu' />
-                            <td v-text='server.memory' />
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+        </template>
     </div>
 </template>
 
@@ -116,6 +129,7 @@ import {
 } from '@tak-ps/vue-tabler';
 import {
     IconRefresh,
+    IconSettings,
     IconPlus,
 } from '@tabler/icons-vue'
 
@@ -126,12 +140,14 @@ export default {
         TablerNone,
         Status,
         IconRefresh,
+        IconSettings,
         IconPlus,
         TablerLoading,
     },
     data: function() {
         return {
             err: false,
+            disabled: true,
             loading: {
                 main: true,
                 service: true,
