81efea285dfda2d6e8439b0323149e20d0330d05
ingalls <nick@ingalls.ca>
2024-09-16 14:18:30 -0600

    Add Play Radial icon for future use

--- api/web/src/components/CloudTAK/RadialMenu/RadialMenu.vue
+++ api/web/src/components/CloudTAK/RadialMenu/RadialMenu.vue
@@ -27,6 +27,15 @@
             fill='none'
         /><path d='M8 8a3.5 3 0 0 1 3.5 -3h1a3.5 3 0 0 1 3.5 3a3 3 0 0 1 -2 3a3 4 0 0 0 -2 4' /><path d='M12 19l0 .01' /></symbol>
         <symbol
+            id='radial-play'
+            viewBox='0 0 24 24'
+            stroke-width='2'
+            stroke='#fff'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+        ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 4v16l13 -8z" /></symbol>
+        <symbol
             id='radial-trash'
             viewBox='0 0 24 24'
             stroke-width='2'
