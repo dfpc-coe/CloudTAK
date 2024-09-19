36e827d68248832770cf6231c5f199dae96c585c
ingalls <nick@ingalls.ca>
2024-09-19 09:40:11 -0600

    Add empty text

--- api/migrations/0063_glamorous_captain_midlands.sql
+++ api/migrations/0063_glamorous_captain_midlands.sql
@@ -2,7 +2,7 @@ UPDATE profile_overlays
     SET styles = '[]'::JSON
     WHERE
         styles IS NULL
-        OR styles::TEXT = '""'::JSON::TEXT;
+        OR styles::TEXT = '"\"\""';
 
 ALTER TABLE "profile_overlays" ALTER COLUMN "styles" SET DEFAULT '[]'::json;--> statement-breakpoint
 ALTER TABLE "profile_overlays" ALTER COLUMN "styles" SET NOT NULL;
