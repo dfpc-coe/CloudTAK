449e86704bc7e15e3bc2cc54f299084807c9f4d4
ingalls <nick@ingalls.ca>
2024-09-30 09:02:14 -0600

    Consistent Name casing

--- api/test/basemap.srv.test.ts
+++ api/test/basemap.srv.test.ts
@@ -154,4 +154,125 @@ test('GET: api/basemap/1/tiles', async (t) => {
     t.end();
 });
 
+test('PATCH: api/basemap/1', async (t) => {
+    try {
+        const res = await flight.fetch('/api/basemap/1', {
+            method: 'PATCH',
+            auth: {
+                bearer: flight.token.admin
+            },
+            body: {
+                name: 'Test Basemap2',
+            }
+        }, true);
+
+        delete res.body.created;
+        delete res.body.updated
+
+        t.deepEqual(res.body, {
+            id: 1,
+            name: 'Test Basemap2',
+            url: 'https://test.com/test/{z}/{x}/{y}',
+            overlay: false,
+            username: 'test@example.com',
+            minzoom: 0,
+            maxzoom: 16,
+            format: 'png',
+            style: 'zxy',
+            styles: [],
+            type: 'raster'
+        })
+    } catch (err) {
+        t.error(err)
+    }
+
+    t.end();
+});
+
+test('PATCH: api/basemap/1 - Invalid URL', async (t) => {
+    try {
+        const res = await flight.fetch('/api/basemap/1', {
+            method: 'PATCH',
+            auth: {
+                bearer: flight.token.admin
+            },
+            body: {
+                name: 'Test Basemap',
+                url: 'test',
+            }
+        }, true);
+
+        t.fail()
+    } catch (err) {
+        t.equals(String(err), 'AssertionError [ERR_ASSERTION]: {"status":400,"message":"Invalid URL provided","messages":[]}');
+    }
+
+    t.end();
+});
+
+test('DELETE: api/basemap/1', async (t) => {
+    try {
+        const res = await flight.fetch('/api/basemap/1', {
+            method: 'DELETE',
+            auth: {
+                bearer: flight.token.admin
+            },
+        }, true);
+
+        t.deepEqual(res.body, {
+            status: 200,
+            message: 'Basemap Deleted'
+        })
+    } catch (err) {
+        t.error(err)
+    }
+
+    t.end();
+});
+
+test('DELETE: api/basemap/1 - Doesn\'t Exist', async (t) => {
+    try {
+        const res = await flight.fetch('/api/basemap/1', {
+            method: 'DELETE',
+            auth: {
+                bearer: flight.token.admin
+            },
+        }, false);
+
+        t.deepEqual(res.body, {
+            status: 404,
+            message:"Item Not Found",
+            messages:[]
+        })
+    } catch (err) {
+        t.error(err)
+    }
+
+    t.end();
+});
+
+test('PATCH: api/basemap/1 - Doesn\'t Exist', async (t) => {
+    try {
+        const res = await flight.fetch('/api/basemap/1', {
+            method: 'PATCH',
+            auth: {
+                bearer: flight.token.admin
+            },
+            body: {
+                name: 'Test Basemap2',
+            }
+        }, false);
+
+        t.deepEqual(res.body, {
+            status: 404,
+            message:"Item Not Found",
+            messages:[]
+        })
+    } catch (err) {
+        t.error(err)
+    }
+
+    t.end();
+});
+
 flight.landing();
