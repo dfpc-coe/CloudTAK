76df21e4c29ba0320b06592f2aeab233693d6145
ingalls <nick@ingalls.ca>
2024-10-09 10:21:12 -0600

    Add Video/Sensor Buttons

--- api/web/src/components/CloudTAK/CoTView.vue
+++ api/web/src/components/CloudTAK/CoTView.vue
@@ -95,17 +95,27 @@
                             <template #dropdown>
                                 <div class='px-1 py-1'>
                                     <div
-                                        v-if='!feat.properties.sensor'
+                                        v-if='feat.properties.attachments === undefined'
                                         role='button'
                                         class='hover-dark px-2 py-2 d-flex align-items-center'
                                         @click='feat.properties.attachments = []'
                                     >
                                         <IconPaperclip :stroke='1' :size='32'/><div class='mx-2'>Add Attachment</div>
                                     </div>
-                                    <div v-if='!feat.properties.video' role='button' class='hover-dark px-2 py-2 d-flex align-items-center'>
+                                    <div
+                                        v-if='feat.properties.video === undefined'
+                                        role='button'
+                                        class='hover-dark px-2 py-2 d-flex align-items-center'
+                                        @click='feat.properties.video = {}'
+                                    >
                                         <IconMovie :stroke='1' :size='32'/><div class='mx-2'>Add Video</div>
                                     </div>
-                                    <div v-if='!feat.properties.sensor' role='button' class='hover-dark px-2 py-2 d-flex align-items-center'>
+                                    <div
+                                        v-if='feat.properties.sensor === undefined'
+                                        role='button'
+                                        class='hover-dark px-2 py-2 d-flex align-items-center'
+                                        @click='feat.properties.sensor = {}'
+                                    >
                                         <IconCone :stroke='1' :size='32'/><div class='mx-2'>Add Sensor</div>
                                     </div>
                                 </div>
