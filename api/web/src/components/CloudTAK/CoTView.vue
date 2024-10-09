3ab3f1b1af9fbf890db567f053e4fd102c5bbffb
ingalls <nick@ingalls.ca>
2024-10-08 19:43:23 -0600

    Add Video/Sensor Section

--- api/web/src/components/CloudTAK/CoTView.vue
+++ api/web/src/components/CloudTAK/CoTView.vue
@@ -82,6 +82,27 @@
                             class='cursor-pointer'
                             @click='zoomTo'
                         />
+
+                        <TablerDropdown>
+                            <IconDotsVertical
+                                v-tooltip='"Add Properties"'
+                                :size='32'
+                                :stroke='1'
+                                class='cursor-pointer'
+                                @click='zoomTo'
+                            />
+
+                            <template #dropdown>
+                                <div class='px-1 py-1'>
+                                    <div role='button' class='hover-dark px-2 py-2 d-flex align-items-center'>
+                                        <IconMovie :stroke='1' :size='32'/><div class='mx-2'>Add Video</div>
+                                    </div>
+                                    <div role='button' class='hover-dark px-2 py-2 d-flex align-items-center'>
+                                        <IconCone :stroke='1' :size='32'/><div class='mx-2'>Add Sensor</div>
+                                    </div>
+                                </div>
+                            </template>
+                        </TablerDropdown>
                     </div>
                 </div>
             </div>
@@ -452,7 +473,8 @@ import {
     TablerInput,
     TablerToggle,
     TablerDelete,
-    TablerMarkdown
+    TablerMarkdown,
+    TablerDropdown
 } from '@tak-ps/vue-tabler';
 import Share from './util/Share.vue';
 import CoTStyle from './util/CoTStyle.vue';
@@ -464,6 +486,9 @@ import Speed from './util/Speed.vue';
 import Elevation from './util/Elevation.vue';
 import Attachments from './util/Attachments.vue';
 import {
+    IconMovie,
+    IconCone,
+    IconDotsVertical,
     IconAmbulance,
     IconPlayerPlay,
     IconShare2,
@@ -636,6 +661,9 @@ export default {
     },
     components: {
         IconCode,
+        IconMovie,
+        IconCone,
+        IconDotsVertical,
         IconAffiliate,
         IconShare2,
         IconInfoCircle,
@@ -659,6 +687,7 @@ export default {
         TablerInput,
         TablerMarkdown,
         TablerToggle,
+        TablerDropdown,
         TablerDelete,
         Subscriptions
     }
