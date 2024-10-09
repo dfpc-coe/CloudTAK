6a3c4dded816a135148635f93322c43ae84d9779
ingalls <nick@ingalls.ca>
2024-10-09 15:11:36 -0600

    Add Video/Sensor

--- api/web/src/components/CloudTAK/util/Sensor.vue
+++ api/web/src/components/CloudTAK/util/Sensor.vue
@@ -1,64 +1,69 @@
 <template>
-<div class='px-1 pb-2 col-12'>
-    <label class='mx-1 subheader'>Sensor FOV</label>
+    <div class='px-1 pb-2 col-12'>
+        <label class='mx-1 subheader'>Sensor FOV</label>
 
-    <div class='mx-2 py-3'>
-        <div class='row g-2 rounded px-2 bg-gray-500 pb-2'>
-            <TablerRange
-                v-if='sensor.range !== undefined'
-                label='Sensor Range Length'
-                :min='0'
-                :max='60000'
-                v-model='sensor.range'
-            >
-                <div class='d-flex align-items-center'>
-                    <TablerInput
-                        style='width: 82px'
-                        v-model='sensor.range'
-                    />
-                    <div class='ms-1'>m</div>
-                </div>
-            </TablerRange>
+        <div class='mx-2 py-3'>
+            <div class='row g-2 rounded px-2 bg-gray-500 pb-2'>
+                <TablerRange
+                    v-if='sensor.range !== undefined'
+                    v-model='sensor.range'
+                    label='Sensor Range Length'
+                    :min='0'
+                    :max='60000'
+                >
+                    <div class='d-flex align-items-center'>
+                        <TablerInput
+                            v-model='sensor.range'
+                            style='width: 82px'
+                        />
+                        <div class='ms-1'>
+                            m
+                        </div>
+                    </div>
+                </TablerRange>
 
-            <TablerRange
-                v-if='sensor.azimuth !== undefined'
-                label='Sensor Direction'
-                :min='0'
-                :max='360'
-                v-model='sensor.azimuth'
-            >
-                <div class='d-flex align-items-center'>
-                    <TablerInput
-                        style='width: 82px'
-                        v-model='sensor.azimuth'
-                    />
-                    <div class='ms-1'>deg</div>
-                </div>
-            </TablerRange>
+                <TablerRange
+                    v-if='sensor.azimuth !== undefined'
+                    v-model='sensor.azimuth'
+                    label='Sensor Direction'
+                    :min='0'
+                    :max='360'
+                >
+                    <div class='d-flex align-items-center'>
+                        <TablerInput
+                            v-model='sensor.azimuth'
+                            style='width: 82px'
+                        />
+                        <div class='ms-1'>
+                            deg
+                        </div>
+                    </div>
+                </TablerRange>
 
-            <TablerRange
-                v-if='sensor.fov !== undefined'
-                label='Sensor FOV'
-                :min='0'
-                :max='360'
-                v-model='sensor.fov'
-            >
-                <div class='d-flex align-items-center'>
-                    <TablerInput
-                        style='width: 82px'
-                        v-model='sensor.fov'
-                    />
-                    <div class='ms-1'>deg</div>
-                </div>
-            </TablerRange>
+                <TablerRange
+                    v-if='sensor.fov !== undefined'
+                    v-model='sensor.fov'
+                    label='Sensor FOV'
+                    :min='0'
+                    :max='360'
+                >
+                    <div class='d-flex align-items-center'>
+                        <TablerInput
+                            v-model='sensor.fov'
+                            style='width: 82px'
+                        />
+                        <div class='ms-1'>
+                            deg
+                        </div>
+                    </div>
+                </TablerRange>
+            </div>
         </div>
     </div>
-</div>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue'
-import { std } from '../../../../src/std.ts';
 import type { Feature } from '../../../types.ts';
 import {
     TablerRange,
@@ -80,14 +85,6 @@ export default defineComponent({
     emits: [
         'update:modelValue'
     ],
-    watch: {
-        sensor: {
-            deep: true,
-            handler: function() {
-                this.$emit('update:modelValue', this.sensor);
-            }
-        }
-    },
     data: function(): {
         sensor: Required<Feature["properties"]["sensor"]>
     } {
@@ -97,6 +94,14 @@ export default defineComponent({
         if (!sensor.range) sensor.range = 0;
 
         return { sensor }
+    },
+    watch: {
+        sensor: {
+            deep: true,
+            handler: function() {
+                this.$emit('update:modelValue', this.sensor);
+            }
+        }
     },
 })
 </script>
