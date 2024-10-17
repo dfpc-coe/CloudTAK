b60c9766abdb8397cb5d9efc1d3bc05cc2af15bb
ingalls <nick@ingalls.ca>
2024-10-17 12:59:23 -0600

    Fix Build

--- api/routes/video-lease.ts
+++ api/routes/video-lease.ts
@@ -161,23 +161,22 @@ export default async function router(schema: Schema, config: Config) {
         try {
             const user = await Auth.as_user(config, req);
 
-            if (user.access !== AuthUserAccess.ADMIN && req.body.duration > 60 * 60 * 16) {
+            if (user.access !== AuthUserAccess.ADMIN && req.body.duration && req.body.duration > 60 * 60 * 16) {
                 throw new Err(400, null, 'Only Administrators can request a lease > 16 hours')
             }
 
-            let lease;
+            let lease = await config.models.VideoLease.from(req.params.lease);
+
             if (user.access === AuthUserAccess.ADMIN) {
                 lease = await config.models.VideoLease.commit(req.params.lease, {
                     ...req.body,
-                    expiration: moment().add(req.body.duration, 'seconds').toISOString(),
+                    expiration: req.body.duration ? moment().add(req.body.duration, 'seconds').toISOString() : lease.expiration,
                 });
             } else {
-                lease = await config.models.VideoLease.from(req.params.lease);
-
                 if (lease.username === user.email) {
                     lease = await config.models.VideoLease.commit(req.params.lease, {
                         ...req.body,
-                        expiration: moment().add(req.body.duration, 'seconds').toISOString(),
+                        expiration: req.body.duration ? moment().add(req.body.duration, 'seconds').toISOString() : lease.expiration,
                     });
                 } else {
                     throw new Err(400, null, 'You can only update a lease you created');
