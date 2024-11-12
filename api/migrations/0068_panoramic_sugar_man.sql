6eddf3eb4836a2dd633fda05a2f34fe569e74f15
ingalls <nick@ingalls.ca>
2024-11-12 11:05:24 -0700

    Add WebTAK Field

--- api/migrations/0068_panoramic_sugar_man.sql
+++ api/migrations/0068_panoramic_sugar_man.sql
@@ -1,3 +1,1 @@
-ALTER TABLE "settings" ALTER COLUMN "value" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "icons" ADD COLUMN "data_alt" text;--> statement-breakpoint
 ALTER TABLE "server" ADD COLUMN "webtak" text DEFAULT '' NOT NULL;
