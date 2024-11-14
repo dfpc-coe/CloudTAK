7a9d7a3900b1c882c2c73590363493e8482c6c64
ingalls <nick@ingalls.ca>
2024-11-14 15:01:22 -0700

    Update EventPool init

--- api/lib/events-pool.ts
+++ api/lib/events-pool.ts
@@ -24,11 +24,9 @@ export default class EventsPool {
             jobs: [],
             errorHandler: (error, workerMetadata) => {
                 if (workerMetadata.threadId) {
-                    console.error(`ERROR: There was an error while running a worker ${workerMetadata.name} with thread ID: ${workerMetadata.threadId}`)
-                    console.error(error);
+                    console.error(`ERROR: There was an error while running a worker ${workerMetadata.name} with thread ID: ${workerMetadata.threadId}`, error)
                 } else {
-                    console.error(`ERROR: There was an error while running a worker ${workerMetadata.name}`)
-                    console.error(error);
+                    console.error(`ERROR: There was an error while running a worker ${workerMetadata.name}`, error)
                 }
             }
         })
@@ -41,59 +39,66 @@ export default class EventsPool {
      */
     async init(pool: PostgresJsDatabase<typeof pgtypes>): Promise<void> {
         const LayerModel = new Modeler(pool, Layer);
-        const layers: InferSelectModel<typeof Layer>[] = [];
+        const layers: Map<number, InferSelectModel<typeof Layer>> = new Map();
 
         const stream = await LayerModel.stream();
 
         await this.bree.start();
 
-        return new Promise((resolve) => {
+        await new Promise((resolve) => {
             stream.on('data', (layer: InferSelectModel<typeof Layer>) => {
-                if (layer.cron && Schedule.is_aws(layer.cron)) return;
+                if (
+                    (layer.cron && Schedule.is_aws(layer.cron))
+                    || !layer.enabled
+                ) return;
 
-                if (!layer.enabled) return;
-                layers.push(layer);
-            }).on('end', async () => {
-                for (const layer of layers) {
-                    try {
-                        if (layer.cron) this.add(layer.id, layer.cron);
-                    } catch (err) {
-                        console.error(err);
-                    }
-                }
-
-                return resolve();
-            });
+                layers.set(layer.id, layer);
+            }).on('end', resolve);
         });
+
+        for (const layer of layers.values()) {
+            try {
+                this.add(layer.id, layer.cron);
+            } catch (err) {
+                console.error(`CloudTAK Bree: Init Error on Layer ${layer.id}`, err);
+            }
+        }
     }
 
     async add(layerid: number, cron: string) {
         const name = `layer-${layerid}`;
         console.error(`ok - adding layer ${layerid} @ ${cron}`);
 
-        const parsed = Schedule.parse_rate(cron);
-        await this.delete(layerid);
-        await this.bree.add({
-            name,
-            path: (new URL('./jobs/lambda.js', import.meta.url)).pathname,
-            interval: `${parsed.freq} ${parsed.unit}`,
-            worker: {
-                workerData: {
-                    LayerID: layerid,
-                    StackName: this.stackName
+        try {
+            const parsed = Schedule.parse_rate(cron);
+            await this.delete(layerid);
+
+            await this.bree.add({
+                name,
+                path: (new URL('./jobs/lambda.js', import.meta.url)).pathname,
+                interval: `${parsed.freq} ${parsed.unit}`,
+                worker: {
+                    workerData: {
+                        LayerID: layerid,
+                        StackName: this.stackName
+                    }
                 }
-            }
-        });
-        await this.bree.start(name);
+            });
+
+            await this.bree.start(name);
+        } catch (err) {
+            console.error(`CloudTAK Bree: Add Error on Layer ${layerid}`, err);
+            throw err;
+        }
     }
 
     async delete(layerid: number) {
+        const name = `layer-${layerid}`;
+
         try {
-            const name = `layer-${layerid}`;
             await this.bree.remove(name);
         } catch (err) {
-            console.error(err);
-            // This usually only happens when a job is added that doesn't exist yet
+            console.log(`CloudTAK Bree: ${name} does not yet exist and cannot be removed`, err);
         }
     }
 }
