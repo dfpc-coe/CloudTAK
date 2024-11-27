6ab4de397fbb4eb39a28eaee47e4822a2b6939de
ingalls <nick@ingalls.ca>
2024-11-27 15:12:16 -0700

    Add Capacitor

---
+++ api/web/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'gov.cotak.map',
+  appName: 'CloudTAK',
+  webDir: 'dist'
+};
+
+export default config;
