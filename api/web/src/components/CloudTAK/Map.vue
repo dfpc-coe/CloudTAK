3f8d8a47e1f6d62bac914f74918ca467cfe93ff9
ingalls <nick@ingalls.ca>
2024-12-11 09:18:20 -0700

    Update Checks

--- api/web/src/components/CloudTAK/Map.vue
+++ api/web/src/components/CloudTAK/Map.vue
@@ -568,7 +568,7 @@ const drawMode = ref<string>('static') // Set the terra-draw mode to avoid getMo
 const drawModePoint = ref<string>('u-d-p');
 const pointInput = ref<boolean>(false);
 const feat = ref()        // Show the Feat Viewer sidebar
-const locked = ref([])         // Lock the map view to a given CoT - The last element is the currently locked value
+const locked = ref<Array<string>>([])         // Lock the map view to a given CoT - The last element is the currently locked value
                     //   this is an array so that things like the radial menu can temporarily lock state but remember the previous lock value when they are closed
 const live_loc_denied = ref(false)   // User denied live location services
 const live_loc = ref<Feature | undefined>();
@@ -615,7 +615,6 @@ watch(mapStore.radial, () => {
         mapStore.map.touchZoomRotate.disableRotation();
         mapStore.map.dragRotate.disable();
         mapStore.map.dragPan.disable();
-        // @ts-expect-error Fix Types
         locked.value.push(mapStore.radial.cot.properties ? mapStore.radial.cot.properties.id : mapStore.radial.cot.id);
     } else {
         mapStore.map.scrollZoom.enable();
