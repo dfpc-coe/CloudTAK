c395b5a2d8397a8482069ef2cf521955a14ca0e8
ingalls <nick@ingalls.ca>
2024-12-20 14:05:59 -0700

    Set Projection

--- api/web/src/components/CloudTAK/Map.vue
+++ api/web/src/components/CloudTAK/Map.vue
@@ -1003,7 +1003,9 @@ function mountMap(): Promise<void> {
         mapStore.init(mapRef.value);
 
         mapStore.map.once('idle', async () => {
-            mapStore.map.setProjection({ type: "globe" });
+            if (profileStore.profile && profileStore.profile.display_projection === 'globe') {
+                mapStore.map.setProjection({ type: "globe" });
+            }
 
             // Eventually make a sprite URL part of the overlay so KMLs can load a sprite package & add paging support
             const iconsets = await std('/api/iconset') as IconsetList;
