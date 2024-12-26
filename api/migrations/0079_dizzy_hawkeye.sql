7455155eb1d7a71696b866c73ba1264d117ed7da
ingalls <nick@ingalls.ca>
2024-12-26 11:20:34 -0700

    Custom DB Migration

---
+++ api/migrations/0079_dizzy_hawkeye.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "server" DROP COLUMN "provider_url";--> statement-breakpoint
+ALTER TABLE "server" DROP COLUMN "provider_secret";--> statement-breakpoint
+ALTER TABLE "server" DROP COLUMN "provider_client";
