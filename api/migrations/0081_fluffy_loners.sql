1958c84b592046b58a7448962ab3654176104ab8
ingalls <nick@ingalls.ca>
2024-12-31 07:52:56 -0700

    Fix SQL Migration

--- api/migrations/0081_fluffy_loners.sql
+++ api/migrations/0081_fluffy_loners.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "profile_interests" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
-	"bounds" "GEOMETRY(POLYGON, 4326)",
+	"bounds" GEOMETRY(POLYGON, 4326),
 	"created" timestamp with time zone DEFAULT Now() NOT NULL,
 	"updated" timestamp with time zone DEFAULT Now() NOT NULL
 );
