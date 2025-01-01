0652ed7825ede377cb25dc768d22f8e06997722c
ingalls <nick@ingalls.ca>
2025-01-01 09:34:48 -0700

    Add Icons in Edit Dialogue

--- api/web/src/components/CloudTAK/util/DrawOverlay.vue
+++ api/web/src/components/CloudTAK/util/DrawOverlay.vue
@@ -1,11 +1,11 @@
 <template>
     <div 
-        class='position-absolute bottom-0 text-white bg-dark px-2 py-2'
+        class='position-absolute bottom-0 text-white bg-dark px-2 py-2 rounded-top'
         style='
-    z-index: 1;
-    left: calc(50% - 120px);
-    width: 380px;
-'
+            z-index: 1;
+            left: calc(50% - 120px);
+            width: 300px;
+        '
     >
         <div v-if='mapStore.drawOptions.mode === "point"'>
             <CoordinateType
@@ -13,19 +13,19 @@
             />
         </div>
         <div v-else-if='mapStore.drawOptions.mode === "angled-rectangle"'>
-            Rectangle Editing
+            <IconVector :size='24' stroke='1'/><span class='mx-2'>Rectangle Editing</span>
         </div>
         <div v-else-if='mapStore.drawOptions.mode === "linestring"'>
-            Line Editing
+            <IconLine :size='24' stroke='1'/><span class='mx-2'>Line Editing</span>
         </div>
         <div v-else-if='mapStore.drawOptions.mode === "polygon"'>
-            Polygon Editing
+            <IconPolygon :size='24' stroke='1'/><span class='mx-2'>Polygon Editing</span>
         </div>
         <div v-else-if='mapStore.drawOptions.mode === "sector"'>
-            Sector Editing
+            <IconCone :size='24' stroke='1'/><span class='mx-2'>Sector Editing</span>
         </div>
         <div v-else-if='mapStore.drawOptions.mode === "freehand"'>
-            Freehand Editing
+            <IconLasso :size='24' stroke='1'/><span class='mx-2'>Lasso Select</span>
         </div>
         <div v-else>
             Unknown Editing
@@ -36,5 +36,13 @@
 <script setup lang='ts'>
 import CoordinateType from '../util/CoordinateType.vue';
 import { useMapStore } from '../../../stores/map.ts';
+import {
+    IconLine,
+    IconLasso,
+    IconCone,
+    IconVector,
+    IconPolygon,
+} from '@tabler/icons-vue';
+
 const mapStore = useMapStore();
 </script>
