f6252befdd0f16fb195d654e1573b609ad3260ed
ingalls <nick@ingalls.ca>
2025-01-05 18:34:03 -0700

    TabIndex Coordinate Type

--- api/web/src/components/CloudTAK/util/Coordinate.vue
+++ api/web/src/components/CloudTAK/util/Coordinate.vue
@@ -17,46 +17,56 @@
                 />
             </template>
             <template v-if='modes.length'>
-                <span
-                    v-if='modes.includes("dd")'
-                    v-tooltip='"Decimal Degrees"'
-                    class='my-1 px-2'
-                    :class='{
-                        "bg-gray-500 rounded-bottom": mode === "dd",
-                        "cursor-pointer": mode !== "dd",
-                    }'
-                    @click='mode = "dd"'
-                >DD</span>
-                <span
-                    v-if='modes.includes("dms")'
-                    v-tooltip='"Decimal Minutes Seconds"'
-                    class='my-1 px-2'
-                    :class='{
-                        "bg-gray-500 rounded-bottom": mode === "dms",
-                        "cursor-pointer": mode !== "dms",
-                    }'
-                    @click='mode = "dms"'
-                >DMS</span>
-                <span
-                    v-if='modes.includes("mgrs")'
-                    v-tooltip='"Military Grid Reference System"'
-                    class='my-1 px-2'
-                    :class='{
-                        "bg-gray-500 rounded-bottom": mode === "mgrs",
-                        "cursor-pointer": mode !== "mgrs",
-                    }'
-                    @click='mode = "mgrs"'
-                >MGRS</span>
-                <span
-                    v-if='modes.includes("utm")'
-                    v-tooltip='"Universal Transverse Mercator"'
-                    class='my-1 px-2'
-                    :class='{
-                        "bg-gray-500 rounded-bottom": mode === "utm",
-                        "cursor-pointer": mode !== "utm",
-                    }'
-                    @click='mode = "utm"'
-                >UTM</span>
+                <div role='menu'>
+                    <span
+                        v-if='modes.includes("dd")'
+                        v-tooltip='"Decimal Degrees"'
+                        role='menuitem'
+                        tabindex='0'
+                        class='my-1 px-2'
+                        :class='{
+                            "bg-gray-500 rounded-bottom": mode === "dd",
+                            "cursor-pointer": mode !== "dd",
+                        }'
+                        @click='mode = "dd"'
+                    >DD</span>
+                    <span
+                        v-if='modes.includes("dms")'
+                        v-tooltip='"Decimal Minutes Seconds"'
+                        class='my-1 px-2'
+                        role='menuitem'
+                        tabindex='0'
+                        :class='{
+                            "bg-gray-500 rounded-bottom": mode === "dms",
+                            "cursor-pointer": mode !== "dms",
+                        }'
+                        @click='mode = "dms"'
+                    >DMS</span>
+                    <span
+                        v-if='modes.includes("mgrs")'
+                        v-tooltip='"Military Grid Reference System"'
+                        class='my-1 px-2'
+                        role='menuitem'
+                        tabindex='0'
+                        :class='{
+                            "bg-gray-500 rounded-bottom": mode === "mgrs",
+                            "cursor-pointer": mode !== "mgrs",
+                        }'
+                        @click='mode = "mgrs"'
+                    >MGRS</span>
+                    <span
+                        v-if='modes.includes("utm")'
+                        v-tooltip='"Universal Transverse Mercator"'
+                        class='my-1 px-2'
+                        role='menuitem'
+                        tabindex='0'
+                        :class='{
+                            "bg-gray-500 rounded-bottom": mode === "utm",
+                            "cursor-pointer": mode !== "utm",
+                        }'
+                        @click='mode = "utm"'
+                    >UTM</span>
+                </div>
             </template>
         </div>
     </div>
