8c647f772b1f7ac43aa09b092c0379f3e0c06b2b
ingalls <nick@ingalls.ca>
2025-01-30 16:28:27 -0700

    Update Schema

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -2,11 +2,11 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    "rules": {
-        "@typescript-eslint/no-explicit-any": "warn"
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            "@typescript-eslint/no-explicit-any": "warn"
+        }
     }
-  }
 );
