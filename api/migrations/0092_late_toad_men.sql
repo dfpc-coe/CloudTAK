a2b08dc639a3091c6fdf0dfd95c6092a613cbc16
ingalls <nick@ingalls.ca>
2025-02-04 19:57:53 -0700

    Custom DB Migration

---
+++ api/migrations/0092_late_toad_men.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "basemaps_collection" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
+DROP TABLE "basemaps_collection" CASCADE;--> statement-breakpoint
+ALTER TABLE "basemaps" DROP CONSTRAINT "basemaps_collection_basemaps_collection_id_fk";
+--> statement-breakpoint
+ALTER TABLE "basemaps" ALTER COLUMN "collection" SET DATA TYPE text;--> statement-breakpoint
+ALTER TABLE "errors" ALTER COLUMN "id" SET DATA TYPE serial;
