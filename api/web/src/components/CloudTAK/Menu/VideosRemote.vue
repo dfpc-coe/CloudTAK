cf204bf5060929c0d88ad43a60e82d3d81107110
ingalls <nick@ingalls.ca>
2025-02-25 14:15:19 -0700

    Add ability to create remote feeds

---
+++ api/web/src/components/CloudTAK/Menu/VideosRemote.vue
@@ -0,0 +1,123 @@
+<template>
+    <MenuTemplate
+        name='Remote Video Connection'
+        :loading='loading'
+        :err='err'
+    >
+        <template #buttons>
+            <TablerDelete
+                v-if='route.params.connectionid !== "new"'
+                displaytype='icon'
+                @delete='deleteConnection'
+            />
+        </template>
+        <template #default>
+            <div class='row g-2 px-2'>
+                <div class='col-12'>
+                    <TablerInput
+                        label='Alias'
+                        v-model='connection.alias'
+                    >
+                        <TablerToggle
+                            label='Active'
+                            v-model='connection.active'
+                        />
+                    </TablerInput>
+                </div>
+                <div class='col-12 d-flex pt-3'>
+                    <div class='ms-auto'>
+                        <button
+                            class='btn btn-primary'
+                            @click='saveConnection'
+                        >
+                            Save Feed
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </template>
+    </MenuTemplate>
+</template>
+
+<script setup lang='ts'>
+import MenuTemplate from '../util/MenuTemplate.vue';
+import { std } from '../../../std.ts';
+import { useRoute, useRouter } from 'vue-router';
+import {
+    TablerInput,
+    TablerDelete,
+    TablerToggle,
+    TablerIconButton
+} from '@tak-ps/vue-tabler';
+import {
+    IconPlus,
+    IconVideo,
+    IconRefresh,
+    IconServer2,
+} from '@tabler/icons-vue';
+
+import { ref, computed, onMounted } from 'vue'
+
+const route = useRoute();
+const router = useRouter();
+const loading = ref(String(route.params.connectionid) !== "new");
+const err = ref<Error | undefined>(undefined);
+const connection = ref({
+    active: true,
+    alias: '',
+    feeds: []
+})
+
+onMounted(async () => {
+    if (String(route.params.connectionid) !== "new") {
+        await fetchConnection()
+    }
+});
+
+async function fetchConnection() {
+    loading.value = true;
+
+    try {
+        connection.value = await std(`/api/marti/video/${route.params.connectionid}`, {
+            method: 'GET'
+        });
+    } catch (err) {
+        throw err;
+    } finally {
+        loading.value = false;
+    }
+}
+
+async function saveConnection() {
+    loading.value = true;
+
+    try {
+        await std('/api/marti/video', {
+            method: 'POST',
+            body: connection.value
+        });
+
+        router.push(`/menu/videos`)
+    } catch (err) {
+        throw err;
+    } finally {
+        loading.value = false;
+    }
+}
+
+async function deleteConnection(): Promise<void> {
+    loading.value = true;
+
+    try {
+        await std(`/api/marti/video/${route.params.connectionid}`, {
+            method: 'DELETE'
+        });
+
+        router.push(`/menu/videos`)
+    } catch (err) {
+        loading.value = false;
+
+        throw err;
+    }
+}
+</script>
