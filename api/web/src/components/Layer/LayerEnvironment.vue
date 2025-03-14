59c12e3799b5e0f5e2cb5eb0e770e1987d96d7a8
ingalls <nick@ingalls.ca>
2025-03-14 09:36:41 -0600

    Hide for capabilities

--- api/web/src/components/Layer/LayerEnvironment.vue
+++ api/web/src/components/Layer/LayerEnvironment.vue
@@ -19,6 +19,7 @@
                         />
                     </TablerIconButton>
                     <TablerIconButton
+                        v-if='props.capabilities'
                         title='Edit'
                         @click='disabled = false'
                     >
