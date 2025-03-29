996f19f723f4f39a80195dcfa0001e73cd9edbc0
ingalls <nick@ingalls.ca>
2025-03-28 20:55:40 -0600

    Custom DB Migration

---
+++ api/migrations/0096_moaning_dexter_bennett.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "palette" (
+	"uuid" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "palette_feature" (
+	"uuid" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"pallete" text NOT NULL,
+	"type" text NOT NULL,
+	"style" json DEFAULT '{}'::json NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "palette_feature" ADD CONSTRAINT "palette_feature_pallete_palette_uuid_fk" FOREIGN KEY ("pallete") REFERENCES "public"."palette"("uuid") ON DELETE no action ON UPDATE no action;
