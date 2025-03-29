c05312cf56149853495bea304c5f36d26cf02609
ingalls <nick@ingalls.ca>
2025-03-28 20:53:44 -0600

    Remove Migration

--- api/migrations/0096_last_stranger.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "palette" (
-	"uuid" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"pallete" text NOT NULL,
-	"type" text NOT NULL,
-	"style" json DEFAULT '{}'::json NOT NULL
-);
---> statement-breakpoint
-ALTER TABLE "palette" ADD CONSTRAINT "palette_pallete_palette_uuid_fk" FOREIGN KEY ("pallete") REFERENCES "public"."palette"("uuid") ON DELETE no action ON UPDATE no action;
