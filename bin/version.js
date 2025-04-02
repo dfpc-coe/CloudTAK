80536002aa0699a74cdc06d7f8207d3f0602fe98
ingalls <nick@ingalls.ca>
2025-04-02 13:13:22 -0600

    Remove CP

--- bin/version.js
+++ bin/version.js
@@ -1,6 +1,4 @@
 import fs from 'node:fs/promises';
-import CP from 'child_process';
-
 
 const pkg_root = JSON.parse(String(await fs.readFile(new URL('../package.json', import.meta.url))));
 const pkg_api = JSON.parse(String(await fs.readFile(new URL('../api/package.json', import.meta.url))));
@@ -11,8 +9,3 @@ pkg_web.version = pkg_root.version;
 
 await fs.writeFile(new URL('../api/package.json', import.meta.url), JSON.stringify(pkg_api, null, 4));
 await fs.writeFile(new URL('../api/web/package.json', import.meta.url), JSON.stringify(pkg_web, null, 4));
-
-console.log(String(CP.execSync(`
-    git commit -am "Update Version" && git push
-`)))
-
