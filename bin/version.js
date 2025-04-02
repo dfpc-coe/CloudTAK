f69283b281f5907f2b66c6961efbac2220416303
ingalls <nick@ingalls.ca>
2025-04-02 10:46:18 -0600

    Update Versions

--- bin/version.js
+++ bin/version.js
@@ -1,12 +1,12 @@
 import path from 'node:path';
 import fs from 'node:fs/promises';
 
-const pkg_root = JSON.parse(String(await fs.readFile(path.resolve('../package.json'))));
-const pkg_api = JSON.parse(String(await fs.readFile(path.resolve('../api/package.json'))));
-const pkg_web = JSON.parse(String(await fs.readFile(path.resolve('../api/web/package.json'))));
+const pkg_root = JSON.parse(String(await fs.readFile(new URL('../package.json', import.meta.url))));
+const pkg_api = JSON.parse(String(await fs.readFile(new URL('../api/package.json', import.meta.url))));
+const pkg_web = JSON.parse(String(await fs.readFile(new URL('../api/web/package.json', import.meta.url))));
 
-pkg_api.version = pkg_root;
-pkg_web.version = pkg_root;
+pkg_api.version = pkg_root.version;
+pkg_web.version = pkg_root.version;
 
-await fs.writeFile(path.resolve('../api/package.json'), JSON.stringify(pkg_api, null, 4));
-await fs.writeFile(path.resolve('../api/web/package.json'), JSON.stringify(pkg_web, null, 4));
+await fs.writeFile(new URL('../api/package.json', import.meta.url), JSON.stringify(pkg_api, null, 4));
+await fs.writeFile(new URL('../api/web/package.json', import.meta.url), JSON.stringify(pkg_web, null, 4));
