642725dc356df214701f10091058d7234b02e811
ingalls <nick@ingalls.ca>
2025-04-14 13:55:37 -0600

    Start merging Data & Events

--- tasks/events/eslint.config.js
+++ tasks/events/eslint.config.js
@@ -1,12 +1,18 @@
+import globals from 'globals';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    "rules": {
-        "@typescript-eslint/no-explicit-any": "warn"
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            "@typescript-eslint/no-explicit-any": "warn"
+        },
+        languageOptions: {
+            globals: {
+                ...globals.node
+            }
+        },
     }
-  }
 );
