d94aa249db83642ee2fbec43046e5dbdd466a79d
ingalls <nick@ingalls.ca>
2025-04-28 19:42:31 -0600

    Add Repeater Delete endpoint

--- api/routes/server-repeater.ts
+++ api/routes/server-repeater.ts
@@ -1,6 +1,8 @@
 import { Type } from '@sinclair/typebox'
 import Err from '@openaddresses/batch-error';
 import Auth from '../lib/auth.js';
+import Schema from '@openaddresses/batch-schema';
+import { StandardResponse } from '../lib/types.js';
 import Config from '../lib/config.js';
 import { Repeater } from '@tak-ps/node-tak/lib/api/repeater';
 import { TAKAPI, APIAuthCertificate } from '@tak-ps/node-tak';
@@ -8,7 +10,7 @@ import { TAKAPI, APIAuthCertificate } from '@tak-ps/node-tak';
 export default async function router(schema: Schema, config: Config) {
     await schema.get('/server/repeater', {
         name: 'List Repeaters',
-        group: 'Server',
+        group: 'ServerRepeater',
         description: 'Get Repeater List',
         res: Type.Object({
             total: Type.Integer(),
@@ -26,6 +28,32 @@ export default async function router(schema: Schema, config: Config) {
             res.json({
                 total: list.data.length,
                 items: list.data
+            })
+        } catch (err) {
+             Err.respond(err, res);
+        }
+    });
+
+    await schema.delete('/server/repeater/:uid', {
+        name: 'Delete Repeater',
+        group: 'ServerRepeater',
+        description: 'Delete Repeater',
+        params: Type.Object({
+            uid: Type.String()
+        }),
+        res: StandardResponse
+    }, async (req, res) => {
+        try {
+            await Auth.as_user(config, req, { admin: true });
+
+            const auth = config.serverCert();
+            const api = await TAKAPI.init(new URL(String(config.server.api)), new APIAuthCertificate(auth.cert, auth.key));
+
+            await api.Repeater.delete(req.params.uid)
+
+            res.json({
+                status: 200,
+                message: 'Repeater Deleted'
             })
         } catch (err) {
              Err.respond(err, res);
