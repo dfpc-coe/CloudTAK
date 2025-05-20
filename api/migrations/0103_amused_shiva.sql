ad06c8af437090e76badb0554f939e813cb14af3
ingalls <nick@ingalls.ca>
2025-05-20 09:55:55 -0600

    Custom DB Migration

---
+++ api/migrations/0103_amused_shiva.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "basemaps" ADD COLUMN "scheme" text DEFAULT 'xyz' NOT NULL;--> statement-breakpoint
+ALTER TABLE "profile" ADD COLUMN "tak_type" text DEFAULT 'a-f-G-E-V-C' NOT NULL;--> statement-breakpoint
+ALTER TABLE "basemaps" DROP COLUMN "schema";
