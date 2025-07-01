c166463b205544b75a6b276183b47574e2c6112f
ingalls <nick@ingalls.ca>
2025-07-01 10:05:01 -0600

    Add new pages files

---
+++ api/web/src/pages/docs/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div id='swagger'/>
+    </div>
+</template>
+
+<script setup lang='ts'>
+import { stdurl } from '../../std.ts'
+import { onMounted } from 'vue'
+import SwaggerUI from 'swagger-ui'
+import 'swagger-ui/dist/swagger-ui.css';
+
+onMounted(() => {
+  SwaggerUI({
+    dom_id: '#swagger',
+    url: String(stdurl('/api/openapi'))
+  })
+})
+</script>
