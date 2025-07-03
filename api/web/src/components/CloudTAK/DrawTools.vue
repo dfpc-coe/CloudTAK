3561ea175752ddd89a9295a4a342f10a5d9beabe
ingalls <nick@ingalls.ca>
2025-07-03 10:18:10 -0600

    Migrate Draw Tools

---
+++ api/web/src/components/CloudTAK/DrawTools.vue
@@ -0,0 +1,145 @@
+<template>
+    <TablerDropdown>
+        <template #default>
+            <TablerIconButton
+                title='Geometry Editing'
+                class='mx-2 cursor-pointer hover-button'
+                @click='closeAllMenu'
+            >
+                <IconPencil
+                    :size='40'
+                    stroke='1'
+                />
+            </TablerIconButton>
+        </template>
+        <template #dropdown>
+            <div
+                class='card py-1'
+                style='min-width: 300px;'
+            >
+                <div class='card-body'>
+                    <div class='card-title'>
+                        Drawing Tools
+                    </div>
+                </div>
+                <div class='card-body'>
+                    <div
+                        class='col-12 py-1 px-2 hover-button cursor-pointer user-select-none'
+                        @click='pointInput = true'
+                    >
+                        <IconCursorText
+                            :size='25'
+                            stroke='1'
+                        />
+                        <span class='ps-2'>Coordinate Input</span>
+                    </div>
+                    <div
+                        class='col-12 py-1 px-2 hover-button cursor-pointer user-select-none'
+                        @click='rangeInput = true'
+                    >
+                        <IconCompass
+                            :size='25'
+                            stroke='1'
+                        />
+                        <span class='ps-2'>Range &amp; Bearing</span>
+                    </div>
+                    <div
+                        class='col-12 py-1 px-2 hover-button cursor-pointer user-select-none'
+                        @click='mapStore.draw.start(DrawToolMode.POINT)'
+                    >
+                        <IconPoint
+                            :size='25'
+                            stroke='1'
+                        />
+                        <span class='ps-2'>Draw Point</span>
+                    </div>
+                    <div
+                        class='col-12 py-1 px-2 hover-button cursor-pointer user-select-none'
+                        @click='mapStore.draw.start(DrawToolMode.LINESTRING)'
+                    >
+                        <IconLine
+                            :size='25'
+                            stroke='1'
+                        />
+                        <span class='ps-2'>Draw Line</span>
+                    </div>
+                    <div
+                        class='col-12 py-1 px-2 hover-button cursor-pointer user-select-none'
+                        @click='mapStore.draw.start(DrawToolMode.POLYGON)'
+                    >
+                        <IconPolygon
+                            :size='25'
+                            stroke='1'
+                        />
+                        <span class='ps-2'>Draw Polygon</span>
+                    </div>
+                    <div
+                        class='col-12 py-1 px-2 hover-button cursor-pointer user-select-none'
+                        @click='mapStore.draw.start(DrawToolMode.RECTANGLE)'
+                    >
+                        <IconVector
+                            :size='25'
+                            stroke='1'
+                        />
+                        <span class='ps-2'>Draw Rectangle</span>
+                    </div>
+                    <div
+                        class='col-12 py-1 px-2 hover-button cursor-pointer user-select-none'
+                        @click='mapStore.draw.start(DrawToolMode.CIRCLE)'
+                    >
+                        <IconCircle
+                            :size='25'
+                            stroke='1'
+                        />
+                        <span class='ps-2'>Draw Circle</span>
+                    </div>
+                    <div
+                        class='col-12 py-1 px-2 hover-button cursor-pointer user-select-none'
+                        @click='mapStore.draw.start(DrawToolMode.SECTOR)'
+                    >
+                        <IconCone
+                            :size='25'
+                            stroke='1'
+                        />
+                        <span class='ps-2'>Draw Sector</span>
+                    </div>
+                    <div
+                        class='col-12 py-1 px-2 hover-button cursor-pointer user-select-none'
+                        @click='mapStore.draw.start(DrawToolMode.FREEHAND)'
+                    >
+                        <IconLasso
+                            :size='25'
+                            stroke='1'
+                        />
+                        <span class='ps-2'>Lasso Select</span>
+                    </div>
+                </div>
+            </div>
+        </template>
+    </TablerDropdown>
+</template>
+
+<script setup lang='ts'>
+import { useRouter } from 'vue-router';
+import {
+    IconLasso,
+    IconCone,
+    IconCircle,
+    IconVector,
+    IconPolygon,
+    IconLine,
+    IconPoint,
+    IconCompass,
+    IconCursorText,
+    IconPencil,
+} from '@tabler/icons-vue';
+import { useMapStore } from '../../stores/map';
+import {
+    TablerIconButton,
+    TablerDropdown,
+} from '@tak-ps/vue-tabler';
+
+const mapStore = useMapStore();
+
+const router = useRouter();
+</script>
