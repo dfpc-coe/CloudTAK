28e60e8e32670d7a816d18fd16b35381378ed53d
Christian Elsen <chris@chriselsen.net>
2025-07-29 20:24:56 -0700

    fix: Update the tests to match what the functions actually return

--- api/web/src/base/utils/__tests__/cot-sidc-mapping.test.ts
+++ api/web/src/base/utils/__tests__/cot-sidc-mapping.test.ts
@@ -7,9 +7,9 @@ import { getIconSourceType, getIconSidc, canGenerateIcon } from '../icon-utils.t
 
 describe('CoT to SIDC Mapping', () => {
     test('should map known CoT types to SIDC', () => {
-        expect(getSidcForCot('a-f-S-X-M-T')).toBe('130330000014011100000000000000');
         expect(getSidcForCot('a-f-G-U-C-I')).toBe('13060000121100000000000000000000');
         expect(getSidcForCot('a-h-A-M-F-Q')).toBe('14010000121100000000000000000000');
+        expect(getSidcForCot('a-f-S-X-M-T-O')).toBe('130330000014011100000000000000');
     });
 
     test('should return undefined for unknown CoT types', () => {
@@ -18,14 +18,14 @@ describe('CoT to SIDC Mapping', () => {
     });
 
     test('should correctly identify mapped CoT types', () => {
-        expect(hasSidcMapping('a-f-S-X-M-T')).toBe(true);
+        expect(hasSidcMapping('a-f-G-U-C-I')).toBe(true);
         expect(hasSidcMapping('unknown-type')).toBe(false);
     });
 });
 
 describe('Icon Utils', () => {
     test('should identify icon source types correctly', () => {
-        expect(getIconSourceType('a-f-S-X-M-T')).toBe('cot-sidc');
+        expect(getIconSourceType('a-f-G-U-C-I')).toBe('cot-sidc');
         expect(getIconSourceType('2525D:10031000001211000000')).toBe('2525d');
         expect(getIconSourceType('color-square-38-#ff0000')).toBe('color-square-38');
         expect(getIconSourceType('color-square-#ff0000')).toBe('color-square');
@@ -33,13 +33,13 @@ describe('Icon Utils', () => {
     });
 
     test('should get SIDC codes correctly', () => {
-        expect(getIconSidc('a-f-S-X-M-T')).toBe('130330000014011100000000000000');
+        expect(getIconSidc('a-f-G-U-C-I')).toBe('13060000121100000000000000000000');
         expect(getIconSidc('2525D:10031000001211000000')).toBe('10031000001211000000');
         expect(getIconSidc('unknown-icon')).toBeUndefined();
     });
 
     test('should identify generatable icons', () => {
-        expect(canGenerateIcon('a-f-S-X-M-T')).toBe(true);
+        expect(canGenerateIcon('a-f-G-U-C-I')).toBe(true);
         expect(canGenerateIcon('2525D:10031000001211000000')).toBe(true);
         expect(canGenerateIcon('color-square-#ff0000')).toBe(true);
         expect(canGenerateIcon('sprite-icon')).toBe(false);
