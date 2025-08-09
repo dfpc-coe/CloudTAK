2cf4b9d658a62b62177e294d7d7532091d6e85b4
ingalls <nick@ingalls.ca>
2025-08-09 08:27:58 -0600

    Add Video Table

---
+++ api/web/src/components/VideoWall.vue
@@ -0,0 +1,17 @@
+<template>
+    <VideoWall
+        v-if='user'
+        :user='user'
+    />
+</template>
+
+<script setup lang='ts'>
+import VideoWall from './VideoWall/Main.vue';
+
+defineProps({
+    user: {
+        type: Object,
+        required: true
+    }
+});
+</script>
