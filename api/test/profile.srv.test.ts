22ec4380361f25932a546ae02f03f8f774c6e313
ingalls <nick@ingalls.ca>
2025-08-18 11:48:25 -0600

    Add Swagger

--- api/test/profile.srv.test.ts
+++ api/test/profile.srv.test.ts
@@ -51,4 +51,51 @@ test('GET: api/profile', async (t) => {
     t.end();
 });
 
+test('PATCH: api/profile', async (t) => {
+    try {
+        const res = await flight.fetch('/api/profile', {
+            method: 'PATCH',
+            auth: {
+                bearer: flight.token.admin
+            },
+            body: {
+                tak_callsign: 'Test Callsign'
+            }
+        }, true);
+
+        t.ok(res.body.last_login);
+        delete res.body.last_login;
+        t.ok(res.body.created);
+        delete res.body.created;
+        t.ok(res.body.updated);
+        delete res.body.updated;
+
+        t.deepEquals(res.body, {
+            active: false,
+            username: 'admin@example.com',
+            phone: '',
+            tak_callsign: 'Test Callsign',
+            tak_remarks: 'CloudTAK User',
+            tak_group: 'Orange',
+            tak_role: 'Team Member',
+            tak_type: 'a-f-G-E-V-C',
+            tak_loc: null,
+            tak_loc_freq: 2000,
+            display_stale: '10 Minutes',
+            display_distance: 'mile',
+            display_elevation: 'feet',
+            display_speed: 'mi/h',
+            display_projection: 'globe',
+            display_zoom: 'conditional',
+            display_text: 'Medium',
+            system_admin: true,
+            agency_admin: []
+        });
+    } catch (err) {
+        t.error(err, 'no error');
+    }
+
+    t.end();
+});
+
 flight.landing();
