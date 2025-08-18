6168e3cb25290c49455b79df7391249d13ca7326
ingalls <nick@ingalls.ca>
2025-08-18 04:25:31 -0600

    Update to events

--- cloudformation/lib/alarms.js
+++ cloudformation/lib/alarms.js
@@ -16,24 +16,5 @@ export default {
                 TopicName: cf.join([cf.stackName, '-low-urgency'])
             }
         },
-
-        EventAlarm: {
-            Type: 'AWS::CloudWatch::Alarm',
-            Properties: {
-                AlarmName: cf.join('-', [cf.stackName, 'EventLambda']),
-                Namespace: 'AWS/Lambda',
-                MetricName: 'Errors',
-                ComparisonOperator: 'GreaterThanThreshold',
-                Threshold: 0,
-                EvaluationPeriods: 1,
-                Statistic: 'Maximum',
-                Period: 60,
-                AlarmActions: [cf.ref('HighUrgencyAlarmTopic')],
-                Dimensions: [{
-                    Name: 'FunctionName',
-                    Value: cf.join([cf.stackName, '-events'])
-                }]
-            }
-        }
     }
 };
