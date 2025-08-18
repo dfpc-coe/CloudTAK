6f759a26ac67c86850d095e249fcb995303f0662
ingalls <nick@ingalls.ca>
2025-08-18 05:35:14 -0600

    Two basic int. tests

---
+++ api/test/profile-interest.srv.test.ts
@@ -0,0 +1,30 @@
+import test from 'tape';
+import Flight from './flight.js';
+
+const flight = new Flight();
+
+flight.init();
+flight.takeoff();
+flight.user();
+
+test('GET: api/profile/interest', async (t) => {
+    try {
+        const res = await flight.fetch('/api/profile/interest', {
+            method: 'GET',
+            auth: {
+                bearer: flight.token.admin
+            }
+        }, true);
+
+        t.deepEquals(res.body, {
+            total: 0,
+            items: []
+        });
+    } catch (err) {
+        t.error(err, 'no error');
+    }
+
+    t.end();
+});
+
+flight.landing();
