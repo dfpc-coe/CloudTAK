6c945a1b084d5c8fbedd1ca5787ad93bc0e01f42
ingalls <nick@ingalls.ca>
2025-08-25 12:44:40 -0400

    Sketch out Latency Checking

--- api/web/src/workers/atlas-connection.ts
+++ api/web/src/workers/atlas-connection.ts
@@ -10,25 +10,38 @@ import type { Feature } from '../types.ts';
 export default class AtlasConnection {
     atlas: Atlas;
 
+    connection?: string;
+
     isDestroyed: boolean;
     isOpen: boolean;
     ws: WebSocket | undefined;
 
+    timings: number[]; // last 10 latency timings
+    latencyThreshold: 1000; // ms to consider a connection to be high latency
+    reconectThreshold: 5000; // ms to consider a connection to be considered lost
+
     constructor(atlas: Atlas) {
         this.atlas = atlas;
 
+        this.timings = [];
         this.isDestroyed = false;
         this.isOpen = false;
         this.ws = undefined;
     }
 
     // COTs are submitted to pending and picked up by the partial update code every .5s
-    connect(connection: string) {
+    connect(connection?: string) {
         this.isDestroyed = false;
+
+        if (connection) this.connection = connection;
+
+        if (!this.connection) {
+            throw new Error('No connection specified');
+        }
 
         const url = stdurl('/api');
         url.searchParams.append('format', 'geojson');
-        url.searchParams.append('connection', connection);
+        url.searchParams.append('connection', this.connection);
         url.searchParams.append('token', this.atlas.token);
 
         if (self.location.protocol === 'http:') {
@@ -49,13 +62,7 @@ export default class AtlasConnection {
         });
 
         this.ws.addEventListener('close', () => {
-            // Otherwise the user is probably logged out
-            if (!this.isDestroyed) {
-                this.connect(connection);
-            }
-
-            this.atlas.postMessage({ type: WorkerMessageType.Connection_Close });
-            this.isOpen = false;
+            this.reconnect();
         });
 
         this.ws.addEventListener('message', async (msg) => {
@@ -104,6 +111,24 @@ export default class AtlasConnection {
                         }
                     });
                 }
+            } else if (body.type === 'ping') {
+                const data = body.data as  { time: string };
+                const sentTime = new Date(data.time).getTime();
+                const receivedTime = Date.now();
+                const latency = receivedTime - sentTime;
+                this.timings.push(latency);
+
+                if (this.timings.length > 10) {
+                    this.timings.shift();
+                }
+
+                if (this.timings.length >= 10 && this.isReconnectLatency()) {
+                    console.error('Reconnect Latency Detected');
+                    this.close();
+                } else if (this.timings.length >= 10 && this.isHighLatency()) {
+                    console.error('High Latency Detected');
+                    this.atlas.postMessage({ type: WorkerMessageType.Connection_Latent });
+                }
             } else if (body.type === 'task') {
                 const task = body.data as Feature;
 
@@ -147,14 +172,58 @@ export default class AtlasConnection {
         });
     }
 
-    destroy() {
-        this.isDestroyed = true;
+    reconnect(): void {
+        this.timings = [];
+
+        this.atlas.postMessage({ type: WorkerMessageType.Connection_Close });
+        this.isOpen = false;
+
+        // Reconnects are not allowed if the connection is destroyed
+        if (this.isDestroyed) return;
+
+        this.connect();
+    }
+
+    /**
+     * Manually trigger a close which will allow reconnects
+     */
+    close(): void {
+        this.timings = [];
 
         if (this.ws) {
             this.ws.close();
         }
     }
 
+    /**
+     * Destory the WebSocket connection and prevent reconnects
+     */
+    destroy() {
+        this.isDestroyed = true;
+        this.connection = undefined;
+
+        this.close();
+    }
+
+    /**
+     * Deterine if the connection has high latency
+     */
+    isHighLatency(): boolean {
+        const sum = this.timings.reduce((acc, cur) => acc + cur, 0);
+
+        const avg = sum / this.timings.length;
+        return avg > this.latencyThreshold;
+    }
+
+    /**
+     * Deterine if the connection has high latency & exceeds the reconnect threshold
+     */
+    isReconnectLatency(): boolean {
+        const sum = this.timings.reduce((acc, cur) => acc + cur, 0);
+
+        const avg = sum / this.timings.length;
+        return avg > this.reconnectThreshold;
+    }
 
     sendCOT(data: object, type = 'cot') {
         if (!this.ws || this.ws.readyState !== WebSocket.OPEN) return;
