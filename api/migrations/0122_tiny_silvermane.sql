2e258d41f97a5e119b42794612ddad6cc880e364
ingalls <nick@ingalls.ca>
2025-08-25 19:51:26 -0400

    Custom DB Migration

---
+++ api/migrations/0122_tiny_silvermane.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "imports" RENAME COLUMN "mode" TO "source";--> statement-breakpoint
+ALTER TABLE "imports" RENAME COLUMN "mode_id" TO "source_id";--> statement-breakpoint
+ALTER TABLE "imports" DROP COLUMN "batch";
