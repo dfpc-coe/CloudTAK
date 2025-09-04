66664755ee857c3677e016609e54845b43203c17
ingalls <nick@ingalls.ca>
2025-09-04 09:40:33 -0600

    Remove custom 0118 migration

--- api/migrations/0118_add_agol_oauth2_config.sql
+++
@@ -1,4 +0,0 @@
--- Add OAuth2 configuration settings for ArcGIS Online
-INSERT INTO "settings" ("key", "value") VALUES ('agol::auth_method', 'oauth2') ON CONFLICT ("key") DO NOTHING;
-INSERT INTO "settings" ("key", "value") VALUES ('agol::client_id', '') ON CONFLICT ("key") DO NOTHING;
-INSERT INTO "settings" ("key", "value") VALUES ('agol::client_secret', '') ON CONFLICT ("key") DO NOTHING;
