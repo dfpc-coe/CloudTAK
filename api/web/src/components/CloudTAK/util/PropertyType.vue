d81b9ca4c45fa8cc0f7a35bce8714d7f0b23190a
ingalls <nick@ingalls.ca>
2025-09-13 11:18:07 -0600

    Background Color Generator

--- api/web/src/components/CloudTAK/util/PropertyType.vue
+++ api/web/src/components/CloudTAK/util/PropertyType.vue
@@ -13,8 +13,11 @@
                 class='ms-2 me-1'
             />
             <label class='subheader user-select-none'>Type</label>
-            <div class='mx-2'>
-                <div class='bg-gray-500 rounded-top py-2 px-2 text-truncate d-flex align-items-center user-select-none'>
+            <div class='mx-2 mt-1'>
+                <div
+                    class='rounded py-2 px-2 text-truncate d-flex align-items-center user-select-none'
+                    :class='background'
+                >
                     <FeatureIcon
                         :key='config.type'
                         :feature='{ properties: { icon: config.type } }'
@@ -44,7 +47,7 @@
                                 :modelValue='StandardAffiliationInverse[config.affiliation]'
                                 :default='StandardAffiliation.Friendly'
                                 :options='Object.keys(StandardAffiliation)'
-                                @update:modelValue='config.affiliation = StandardAffiliation[$event]'
+                                @update:modelValue='updateAffiliation($event)'
                             />
                         </div>
                         <div class='col-12'>
@@ -57,21 +60,33 @@
                         </div>
                     </div>
 
-                    <template v-for='item of list.items'>
-                        <div
-                            class='d-flex align-items-center px-2 py-2 hover cursor-pointer rounded'
-                            @click='updateType(item)'
+                    <TablerLoading
+                        v-if='loading'
+                    />
+                    <TablerNone
+                        v-else-if='list.total === 0'
+                        label='Types Found'
+                        :create='false'
+                    />
+                    <template v-else>
+                        <template
+                            v-for='item of list.items'
                         >
-                            <FeatureIcon
-                                :key='item.cot'
-                                :feature='{ properties: { icon: item.cot } }'
-                            />
-
                             <div
-                                class='mx-2'
-                                v-text='item.full'
-                            />
-                        </div>
+                                class='d-flex align-items-center px-2 py-2 hover cursor-pointer rounded'
+                                @click='updateType(item)'
+                            >
+                                <FeatureIcon
+                                    :key='item.cot'
+                                    :feature='{ properties: { icon: item.cot } }'
+                                />
+
+                                <div
+                                    class='mx-2'
+                                    v-text='item.full'
+                                />
+                            </div>
+                        </template>
                     </template>
                 </template>
             </template>
@@ -80,7 +95,7 @@
 </template>
 
 <script setup lang='ts'>
-import { ref, watch, onMounted } from 'vue';
+import { ref, watch, computed, onMounted } from 'vue';
 import { std, stdurl } from '../../../std.ts';
 import type { COTType, COTTypeList } from '../../../types.ts';
 import FeatureIcon from './FeatureIcon.vue';
@@ -88,33 +103,13 @@ import {
     IconChartGridDots
 } from '@tabler/icons-vue';
 import {
+    TablerNone,
     TablerEnum,
     TablerInput,
+    TablerLoading,
     TablerSlidedown,
     TablerInlineAlert,
 } from '@tak-ps/vue-tabler';
-
-const props = defineProps({
-    modelValue: {
-        type: String,
-        required: true
-    },
-    hover: {
-        type: Boolean,
-        default: true
-    }
-})
-
-const emit = defineEmits(['update:modelValue']);
-
-const meta = ref<COTType | undefined>();
-
-const config = ref({
-    affiliation: props.modelValue.split('-')[1],
-    type: props.modelValue
-})
-
-const loading = ref(true);
 
 const StandardAffiliation: Record<string, string> = {
     'Pending': 'p',
@@ -133,6 +128,32 @@ const StandardAffiliationInverse = Object.fromEntries(
     Object.entries(StandardAffiliation).map(([key, value]) => [value, key])
 );
 
+const props = defineProps({
+    modelValue: {
+        type: String,
+        required: true
+    },
+    hover: {
+        type: Boolean,
+        default: true
+    }
+})
+
+const emit = defineEmits(['update:modelValue']);
+
+const meta = ref<COTType | undefined>();
+
+const config = ref({
+    affiliation: 'u',
+    type: props.modelValue
+})
+
+if (config.value.type.startsWith('a-') && StandardAffiliationInverse[config.value.type.split('-')[1]]) {
+    config.value.affiliation = config.value.type.split('-')[1];
+}
+
+const loading = ref(true);
+
 const paging = ref({
     filter: '',
 });
@@ -146,25 +167,19 @@ watch(props, async () => {
     if (!props.modelValue) return;
 
     if (props.modelValue !== config.value.type) {
-        config.value.affiliation = props.modelValue.split('-')[1];
+        if (
+            props.modelValue.startsWith('a-')
+            && StandardAffiliationInverse[props.modelValue.split('-')[1]]
+            && config.value.affiliation !== props.modelValue.split('-')[1]
+        ) {
+            config.value.affiliation = config.value.type.split('-')[1];
+            await fetchList();
+        }
+
         config.value.type = props.modelValue;
 
         await fetchType();
-        await fetchList();
     }
-});
-
-watch(config.value, async () => {
-    if (config.value.type.split(['-'])[1] !== config.value.affiliation) {
-        config.value.type = config.value.type.replace(/^.-.-/, `a-${config.value.affiliation}-`);
-    }
-
-    emit('update:modelValue', config.value.type);
-
-    await fetchType();
-    await fetchList();
-}, {
-    deep: true
 });
 
 watch(paging.value, async () => {
@@ -176,9 +191,36 @@ onMounted(async () => {
     await fetchList();
 });
 
+const background = computed(() => {
+    if (config.value.affiliation === 'f' || config.value.affiliation === 'a') {
+        return 'border border-blue bg-blue-lt text-blue-fg';
+    } else if (config.value.affiliation === 'n') {
+        return 'border border-green bg-green-lt text-green-fg';
+    } else if (config.value.affiliation === 'h' || config.value.affiliation === 's' || config.value.affiliation === 'j' || config.value.affiliation === 'k') {
+        return 'border border-red bg-red-lt text-red-fg';
+    } else if (config.value.affiliation === 'p' || config.value.affiliation === 'u') {
+        return 'border border-yellow bg-yellow-lt text-yellow-fg';
+    } else {
+        return 'bg-gray-500';
+    }
+});
+
 function updateType(item: COTType) {
     meta.value = item;
     config.value.type = item.cot;
+
+    emit('update:modelValue', item.cot);
+}
+
+async function updateAffiliation(affil: string) {
+    if (StandardAffiliation[affil] === undefined) return;
+
+    config.value.affiliation = StandardAffiliation[affil]
+    config.value.type = config.value.type.replace(/^a-.-/, `a-${config.value.affiliation}-`);
+
+    emit('update:modelValue', config.value.type);
+
+    await fetchList();
 }
 
 async function fetchType() {
@@ -192,15 +234,12 @@ async function fetchList() {
     const url = stdurl('/api/type/cot');
     url.searchParams.append('filter', paging.value.filter);
 
-    if (config.value.type.startsWith('a-')) {
-        url.searchParams.append('identity', config.value.affiliation);
-    } else {
-        url.searchParams.append('identity', 'f');
-    }
-
+    url.searchParams.append('identity', config.value.affiliation);
     url.searchParams.append('domain', 'a');
 
     list.value = await std(url) as COTTypeList;
+
+    loading.value = false;
 }
 
 </script>
