28ca4689f64e7753cef34cf2805b09ca93bf4710
ingalls <nick@ingalls.ca>
2025-09-30 17:37:43 -0600

    Add Sorted Column

--- api/migrations/0126_glamorous_sasquatch.sql
+++ api/migrations/0126_glamorous_sasquatch.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "profile_features" DROP CONSTRAINT "profile_features_username_id_unique";--> statement-breakpoint
+ALTER TABLE "profile_features" DROP CONSTRAINT "profile_features_pkey";--> statement-breakpoint
 ALTER TABLE "profile_features" ADD CONSTRAINT "profile_features_username_id_pk" PRIMARY KEY("username","id");
