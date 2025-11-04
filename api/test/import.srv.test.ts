0d3f29813c6b8f3921dd35d847bfd1f6cc416a4a
ingalls <nick@ingalls.ca>
2025-11-04 15:27:42 -0700

    Add Import Notify

--- api/test/import.srv.test.ts
+++ api/test/import.srv.test.ts
@@ -55,11 +55,11 @@ test('POST: api/import', async (t) => {
             updated: '2025-09-12T00:12:46.016Z',
             name: 'test.zip',
             status: 'Empty',
-            error: '',
+            error: null,
             result: {},
             username: 'admin@example.com',
             source: 'Upload',
-            source_id: '',
+            source_id: null,
             config: {}
         });
     } catch (err) {
@@ -90,11 +90,11 @@ test(`GET: api/import/<id>`, async (t) => {
             updated: '2025-09-12T00:12:46.016Z',
             name: 'test.zip',
             status: 'Empty',
-            error: '',
+            error: null,
             result: {},
             username: 'admin@example.com',
             source: 'Upload',
-            source_id: '',
+            source_id: null,
             config: {}
         });
     } catch (err) {
@@ -128,11 +128,49 @@ test(`PATCH: api/import/<id>`, async (t) => {
             updated: '2025-09-12T00:12:46.016Z',
             name: 'test.zip',
             status: 'Running',
-            error: '',
+            error: null,
             result: {},
             username: 'admin@example.com',
             source: 'Upload',
-            source_id: '',
+            source_id: null,
+            config: {}
+        });
+    } catch (err) {
+        t.error(err, 'no error');
+    }
+
+    t.end();
+});
+
+test(`PATCH: api/import/<id> - Success`, async (t) => {
+    try {
+        const res = await flight.fetch(`/api/import/${id}`, {
+            method: 'PATCH',
+            auth: {
+                bearer: flight.token.admin
+            },
+            body: {
+                status: 'Success'
+            }
+        }, true);
+
+        t.ok(res.body.id, 'has id');
+        t.ok(res.body.created, 'has created');
+        res.body.created = '2025-09-12T00:12:46.016Z';
+        t.ok(res.body.updated, 'has updated');
+        res.body.updated = '2025-09-12T00:12:46.016Z';
+
+        t.deepEquals(res.body, {
+            id: id,
+            created: '2025-09-12T00:12:46.016Z',
+            updated: '2025-09-12T00:12:46.016Z',
+            name: 'test.zip',
+            status: 'Success',
+            error: null,
+            result: {},
+            username: 'admin@example.com',
+            source: 'Upload',
+            source_id: null,
             config: {}
         });
     } catch (err) {
