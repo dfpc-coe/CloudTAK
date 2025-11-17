31013761eaadb530d95153b9733be8bf1da6a376
ingalls <nick@ingalls.ca>
2025-11-16 18:52:50 -0700

    Add Attachemnts Tests

--- api/test/attachments.src.test.ts
+++ api/test/attachments.src.test.ts
@@ -4,6 +4,10 @@ import Sinon from 'sinon';
 import {
     S3Client
 } from '@aws-sdk/client-s3';
+import { FormData } from 'undici';
+import { Readable } from 'node:stream';
+import S3 from '../lib/aws/s3.js';
+import { DataPackage } from '@tak-ps/node-cot';
 
 const flight = new Flight();
 
@@ -86,5 +90,130 @@ test('GET: api/attachments - result', async (t) => {
     t.end();
 });
 
+
+test('PUT: api/attachment - success', async (t) => {
+    try {
+        const s3Stub = Sinon.stub(S3, 'put').resolves();
+        const hashStub = Sinon.stub(DataPackage, 'hash').resolves('hash-123');
+
+        const body = new FormData();
+        body.append('file', new Blob(['file-content'], {
+            type: 'text/plain'
+        }), 'image.png');
+
+        const res = await flight.fetch('/api/attachment', {
+            method: 'PUT',
+            auth: {
+                bearer: flight.token.admin
+            },
+            body
+        }, true);
+
+        t.deepEquals(res.body, { hash: 'hash-123' });
+        t.ok(s3Stub.calledOnce);
+        t.ok(hashStub.calledOnce);
+        t.equals(s3Stub.firstCall.args[0], 'attachment/hash-123/image.png');
+    } catch (err) {
+        t.error(err, 'no error');
+    }
+
+    Sinon.restore();
+    t.end();
+});
+
+test('PUT: api/attachment - unsupported content type', async (t) => {
+    try {
+        const res = await flight.fetch('/api/attachment', {
+            method: 'PUT',
+            auth: {
+                bearer: flight.token.admin
+            },
+            body: {
+                foo: 'bar'
+            }
+        }, false);
+
+        t.equals(res.status, 400);
+        t.equals(res.body.message, 'Unsupported Content-Type');
+    } catch (err) {
+        t.error(err, 'no error');
+    }
+
+    t.end();
+});
+
+test('GET: api/attachment/:hash - stream', async (t) => {
+    try {
+        Sinon.stub(S3Client.prototype, 'send').callsFake((command) => {
+            if (command.constructor.name === 'ListObjectsV2Command') {
+                t.deepEquals(command.input, {
+                    Bucket: 'fake-asset-bucket',
+                    Prefix: 'attachment/stream-hash/'
+                });
+                return Promise.resolve({
+                    Contents: [{
+                        Key: 'attachment/stream-hash/data.txt'
+                    }]
+                });
+            } else if (command.constructor.name === 'GetObjectCommand') {
+                t.deepEquals(command.input, {
+                    Bucket: 'fake-asset-bucket',
+                    Key: 'attachment/stream-hash/data.txt'
+                });
+                return Promise.resolve({
+                    Body: Readable.from(['file-body'])
+                });
+            }
+
+            throw new Error('Unexpected command');
+        });
+
+        const res = await flight.fetch('/api/attachment/stream-hash', {
+            method: 'GET',
+            auth: {
+                bearer: flight.token.admin
+            }
+        }, {
+            json: false
+        });
+
+        t.equals(res.status, 200);
+        t.equals(res.body, 'file-body');
+    } catch (err) {
+        t.error(err, 'no error');
+    }
+
+    Sinon.restore();
+    t.end();
+});
+
+test('GET: api/attachment/:hash - missing', async (t) => {
+    try {
+        Sinon.stub(S3Client.prototype, 'send').callsFake((command) => {
+            if (command.constructor.name === 'ListObjectsV2Command') {
+                return Promise.resolve({
+                    Contents: []
+                });
+            }
+
+            throw new Error('Unexpected command');
+        });
+
+        const res = await flight.fetch('/api/attachment/missing', {
+            method: 'GET',
+            auth: {
+                bearer: flight.token.admin
+            }
+        }, false);
+
+        t.equals(res.status, 404);
+        t.equals(res.body.message, 'Attachment not found');
+    } catch (err) {
+        t.error(err, 'no error');
+    }
+
+    Sinon.restore();
+    t.end();
+});
 
 flight.landing();
