c2678acdf500f17290bdebb3a72a66a003dc2ad0
ingalls <nick@ingalls.ca>
2025-11-18 15:19:51 -0700

    Force Refresh

--- api/web/vite.config.ts
+++ api/web/vite.config.ts
@@ -49,9 +49,9 @@ export default defineConfig(({ mode }) => {
                     video: path.resolve(__dirname, 'video.html'),
                 },
                 output: {
-                    entryFileNames: `assets/[name].js`,
-                    chunkFileNames: `assets/[name].js`,
-                    assetFileNames: `assets/[name].[ext]`
+                    entryFileNames: `assets/[name]-[hash].js`,
+                    chunkFileNames: `assets/[name]-[hash].js`,
+                    assetFileNames: `assets/[name]-[hash].[ext]`
                 }
             },
         },
