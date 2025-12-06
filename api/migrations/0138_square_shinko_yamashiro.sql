a7c0c8cbbbf588e5c6b66c62027d1e6f9566cd94
ingalls <nick@ingalls.ca>
2025-12-06 09:17:26 -0700

    Custom DB Migration

---
+++ api/migrations/0138_square_shinko_yamashiro.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "basemaps" ADD COLUMN "iconset" text;--> statement-breakpoint
+ALTER TABLE "profile_overlays" ADD COLUMN "iconset" text;--> statement-breakpoint
+ALTER TABLE "basemaps" ADD CONSTRAINT "basemaps_iconset_iconsets_uid_fk" FOREIGN KEY ("iconset") REFERENCES "public"."iconsets"("uid") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "profile_overlays" ADD CONSTRAINT "profile_overlays_iconset_iconsets_uid_fk" FOREIGN KEY ("iconset") REFERENCES "public"."iconsets"("uid") ON DELETE no action ON UPDATE no action;
