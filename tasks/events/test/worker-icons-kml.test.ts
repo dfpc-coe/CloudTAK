3ca62a99f8fad3eb8333b36fed763bd444111e10
ingalls <nick@ingalls.ca>
2025-12-18 14:33:15 -0700

    Add initial icons kml file

---
+++ tasks/events/test/worker-icons-kml.test.ts
@@ -0,0 +1,198 @@
+import test from 'tape';
+import Worker from '../src/worker.js';
+import fs from 'node:fs';
+import Sinon from 'sinon';
+import {
+    S3Client,
+    GetObjectCommand,
+    PutObjectCommand,
+    CreateMultipartUploadCommand,
+    UploadPartCommand,
+    CompleteMultipartUploadCommand
+} from '@aws-sdk/client-s3';
+import { MockAgent, setGlobalDispatcher, getGlobalDispatcher } from 'undici';
+
+test(`Worker Import: KML-Samples.kml`, async (t) => {
+    let id: string;
+
+    const mockAgent = new MockAgent();
+    const originalDispatcher = getGlobalDispatcher();
+
+    mockAgent.disableNetConnect();
+
+    setGlobalDispatcher(mockAgent);
+
+    const mockPool = mockAgent.get('http://localhost:5001');
+
+    mockPool.intercept({
+        path: /profile\/asset/,
+        method: 'POST'
+    }).reply((req) => {
+        const body = JSON.parse(req.body) as {
+            id: string,
+            name: string
+        };
+
+        t.equal(body.name, 'ba58a298-a3fe-46b4-a29a-9dd33fbb2139.kml', 'Name should be ba58a298-a3fe-46b4-a29a-9dd33fbb2139.kml');
+
+        id = body.id;
+
+        return {
+            statusCode: 200,
+            data: JSON.stringify({
+                id: body.id,
+                artifacts: []
+            })
+        };
+    });
+
+    mockPool.intercept({
+        path: /profile\/asset\//,
+        method: 'PATCH'
+    }).reply((req) => {
+        const body = JSON.parse(req.body) as {
+            artifacts: Array<{ ext: string }>
+        };
+
+        t.deepEquals(body, {
+            artifacts: [ { ext: '.geojsonld' } ]
+        });
+
+        return {
+            statusCode: 200,
+            data: JSON.stringify({
+                id: id,
+                artifacts: [{
+                    ext: '.geojsonld'
+                }]
+            })
+        };
+    });
+
+    mockPool.intercept({
+        path: /profile\/asset\//,
+        method: 'PATCH'
+    }).reply((req) => {
+        const body = JSON.parse(req.body) as {
+            artifacts: Array<{ ext: string }>
+        };
+
+        t.deepEquals(body, {
+            artifacts: [ { ext: '.geojsonld' }, { ext: '.pmtiles' } ]
+        });
+
+        return {
+            statusCode: 200,
+            data: JSON.stringify({
+                id: id,
+                artifacts: [{
+                    ext: '.geojsonld'
+                }, {
+                    ext: '.pmtiles'
+                }]
+            })
+        };
+    });
+
+    const ExternalOperations = [
+            (command) => {
+                t.ok(command instanceof GetObjectCommand);
+                t.deepEquals(command.input, {
+                    Bucket: 'test-bucket',
+                    Key: `import/ba58a298-a3fe-46b4-a29a-9dd33fbb2139.kml`
+                });
+
+                return Promise.resolve({
+                    Body: fs.createReadStream(new URL(`./fixtures/KML-Samples.kml`, import.meta.url))
+                })
+            },
+            (command) => {
+                if (command instanceof CreateMultipartUploadCommand) {
+                    t.equals(command.input.Bucket, 'test-bucket');
+                    t.ok(command.input.Key.startsWith(`profile/admin@example.com/`))
+                    t.ok(command.input.Key.endsWith('.kml'))
+                    return Promise.resolve({ UploadId: '123' });
+                }
+                t.ok(command instanceof PutObjectCommand);
+                t.equals(command.input.Bucket, 'test-bucket');
+                t.ok(command.input.Key.startsWith(`profile/admin@example.com/`))
+                t.ok(command.input.Key.endsWith('.kml'))
+
+                return Promise.resolve({ ETag: '"123"' })
+            },
+            (command) => {
+                if (command instanceof CreateMultipartUploadCommand) {
+                    t.equals(command.input.Bucket, 'test-bucket');
+                    t.ok(command.input.Key.startsWith(`profile/admin@example.com/`))
+                    t.ok(command.input.Key.endsWith('.geojsonld'))
+                    return Promise.resolve({ UploadId: '123' });
+                }
+                t.ok(command instanceof PutObjectCommand);
+                t.equals(command.input.Bucket, 'test-bucket');
+                t.ok(command.input.Key.startsWith(`profile/admin@example.com/`))
+                t.ok(command.input.Key.endsWith('.geojsonld'))
+
+                return Promise.resolve({ ETag: '"123"' })
+            },
+            (command) => {
+                if (command instanceof CreateMultipartUploadCommand) {
+                    t.equals(command.input.Bucket, 'test-bucket');
+                    t.ok(command.input.Key.startsWith(`profile/admin@example.com/`))
+                    t.ok(command.input.Key.endsWith('.pmtiles'))
+                    return Promise.resolve({ UploadId: '123' });
+                }
+                t.ok(command instanceof PutObjectCommand);
+                t.equals(command.input.Bucket, 'test-bucket');
+                t.ok(command.input.Key.startsWith(`profile/admin@example.com/`))
+                t.ok(command.input.Key.endsWith('.pmtiles'))
+
+                return Promise.resolve({ ETag: '"123"' })
+            },
+    ].reverse();
+
+    Sinon.stub(S3Client.prototype, 'send').callsFake(async (command) => {
+        if (command instanceof UploadPartCommand) {
+             return { ETag: '"123"' };
+        }
+        if (command instanceof CompleteMultipartUploadCommand) {
+             return { Location: '...' };
+        }
+
+        const validator = ExternalOperations.pop();
+        if (!validator) throw new Error(`Unexpected command: ${command.constructor.name}`);
+        
+        return validator(command);
+    });
+
+    const worker = new Worker({
+        api: 'http://localhost:5001',
+        secret: 'coe-wildland-fire',
+        bucket: 'test-bucket',
+        job: {
+            id: 'ba58a298-a3fe-46b4-a29a-9dd33fbb2139',
+            created: '2025-08-25T18:08:21.563Z',
+            updated: '2025-08-25T18:08:21.563Z',
+            status: 'Running',
+            error: null,
+            result: {},
+            name: `KML-Samples.kml`,
+            username: 'admin@example.com',
+            source: 'Upload',
+            config: {},
+            source_id: null,
+        }
+    });
+
+    worker.on('error', (err) => {
+        t.error(err);
+    });
+
+    worker.on('success', () => {
+        Sinon.restore();
+        setGlobalDispatcher(originalDispatcher);
+        mockAgent.close();
+        t.end()
+    });
+
+    await worker.process()
+});
