7840c49e008bc69906e33d764976f41435b66fdb
ingalls <nick@ingalls.ca>
2026-01-13 17:49:03 -0700

    idempotent db migrations

--- api/migrations/0143_careful_firedrake.sql
+++ api/migrations/0143_careful_firedrake.sql
@@ -1,2 +1,6 @@
-ALTER TABLE "mission_template_log" ADD COLUMN "template" uuid NOT NULL;--> statement-breakpoint
-ALTER TABLE "mission_template_log" ADD CONSTRAINT "mission_template_log_template_mission_template_id_fk" FOREIGN KEY ("template") REFERENCES "public"."mission_template"("id") ON DELETE no action ON UPDATE no action;
+ALTER TABLE "mission_template_log" ADD COLUMN IF NOT EXISTS "template" uuid NOT NULL;--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "mission_template_log" ADD CONSTRAINT "mission_template_log_template_mission_template_id_fk" FOREIGN KEY ("template") REFERENCES "public"."mission_template"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
