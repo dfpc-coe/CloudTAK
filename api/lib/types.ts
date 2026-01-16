4fd6c9745e1d9b5a511be4c5d414fa4b42cd12b5
ingalls <nick@ingalls.ca>
2026-01-15 19:49:11 -0700

    Update Basemap Test

--- api/lib/types.ts
+++ api/lib/types.ts
@@ -378,4 +378,5 @@ export const BasemapResponse = createSelectSchema(schemas.Basemap, {
     styles: Type.Array(Type.Unknown()),
     collection: Type.Optional(Type.Union([Type.Null(), Type.String()])),
     sharing_token: Type.Optional(Type.Union([Type.Null(), Type.String()])),
+    snapping_layer: Type.Union([Type.Null(), Type.String()]),
 });
