65907fe8a07c51114fe86c4d39b811e43b8e2739
ingalls <nick@ingalls.ca>
2026-02-11 11:27:35 -0700

    Full Config Caching

--- api/routes/config.ts
+++ api/routes/config.ts
@@ -133,6 +133,7 @@ export const FullConfigDefaults: Partial<Static<typeof FullConfig>> = {
     'login::username': 'Username or Email',
     'login::brand::enabled': 'default',
     'login::background::enabled': false,
+    'login::background::color': '#03384f',
     'login::brand::logo': `data:image/svg+xml;base64,${fs.readFileSync(new URL('../web/public/CloudTAKLogo.svg', import.meta.url)).toString('base64')}`
 };
 
