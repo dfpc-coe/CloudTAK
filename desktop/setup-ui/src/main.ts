ae746bf29df6df87722b4d2bfbf790647815eafc
ingalls <nick@ingalls.ca>
2026-02-22 17:24:41 -0700

    Remove unecessary scaffoling

--- desktop/setup-ui/src/main.ts
+++ desktop/setup-ui/src/main.ts
@@ -3,7 +3,5 @@ import App from './App.vue'
 
 import '@tabler/core/dist/css/tabler.min.css'
 
-
 const app = createApp(App)
 app.mount('#app')
-// app.use(Tabler) // Commenting out the use of Tabler since it's unused
