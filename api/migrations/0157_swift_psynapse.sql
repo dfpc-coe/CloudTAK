b96359a2791ac5e0c6c6fcc779c9312705294154
ingalls <nick@ingalls.ca>
2026-03-04 09:28:29 -0700

    Make SQL Migration idempotent

--- api/migrations/0157_swift_psynapse.sql
+++ api/migrations/0157_swift_psynapse.sql
@@ -1,3 +1,9 @@
-ALTER TABLE "connection_features" ADD COLUMN "layer" integer;--> statement-breakpoint
-ALTER TABLE "connection_features" ADD CONSTRAINT "connection_features_layer_layers_id_fk" FOREIGN KEY ("layer") REFERENCES "public"."layers"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-CREATE INDEX "connection_features_connection_layer_idx" ON "connection_features" USING btree ("connection","layer");
+ALTER TABLE "connection_features" ADD COLUMN IF NOT EXISTS "layer" integer;--> statement-breakpoint
+DO $$ BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_constraint WHERE conname = 'connection_features_layer_layers_id_fk'
+    ) THEN
+        ALTER TABLE "connection_features" ADD CONSTRAINT "connection_features_layer_layers_id_fk" FOREIGN KEY ("layer") REFERENCES "public"."layers"("id") ON DELETE no action ON UPDATE no action;
+    END IF;
+END $$;--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "connection_features_connection_layer_idx" ON "connection_features" USING btree ("connection","layer");
