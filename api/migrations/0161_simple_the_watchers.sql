3c389cf1679484e0341e989623af5f900cc215a1
ingalls <nick@ingalls.ca>
2026-03-30 14:03:23 -0600

    Update protocol

--- api/migrations/0161_simple_the_watchers.sql
+++ api/migrations/0161_simple_the_watchers.sql
@@ -1,1 +1,5 @@
-ALTER TABLE "basemaps" ADD COLUMN "protocol" text DEFAULT 'zxy' NOT NULL;
+ALTER TABLE "basemaps" ADD COLUMN IF NOT EXISTS "protocol" text DEFAULT 'zxy' NOT NULL;
+
+UPDATE "basemaps" SET "protocol" = 'featureserver' WHERE url ~ '\/FeatureServer\/[0-9]+$';
+UPDATE "basemaps" SET "protocol" = 'mapserver'     WHERE url ~ '\/MapServer\/[0-9]+$';
+UPDATE "basemaps" SET "protocol" = 'imageserver'   WHERE url ~ '\/ImageServer$';
