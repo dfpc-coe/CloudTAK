4922d22d3fa27040379f11465dafe558edc1aadb
ingalls <nick@ingalls.ca>
2026-04-19 12:08:09 -0600

    Move from typebox inference to explicitly defined ProfileFile Type

--- api/lib/types.ts
+++ api/lib/types.ts
@@ -360,19 +360,21 @@ export const JobResponse = Type.Object({
     updated: Type.Optional(Type.Integer())
 });
 
-export const ProfileFileResponse = Type.Intersect([
-    createSelectSchema(schemas.ProfileFile, {
-        id: Type.String(),
-        iconset: Type.Union([Type.Null(), Type.String()]),
-        artifacts: Type.Array(Type.Object({
-            ext: Type.String(),
-            size: Type.Integer(),
-        }))
-    }),
-    Type.Object({
-        channels: Type.Array(Type.String())
-    })
-])
+export const ProfileFileResponse = Type.Object({
+    id: Type.String(),
+    created: Type.String(),
+    updated: Type.String(),
+    username: Type.String(),
+    path: Type.String(),
+    name: Type.String(),
+    iconset: Type.Union([Type.Null(), Type.String()]),
+    size: Type.Integer(),
+    channels: Type.Array(Type.String()),
+    artifacts: Type.Array(Type.Object({
+        ext: Type.String(),
+        size: Type.Integer(),
+    }))
+});
 
 export const AssetResponse = Type.Object({
     name: Type.String({ "description": "The filename of the asset" }),
