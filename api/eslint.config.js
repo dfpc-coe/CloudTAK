6f684e5f33b27075be046c97eae18046b032203f
ingalls <nick@ingalls.ca>
2026-05-22 08:40:42 -0600

    Add Stylistic

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -1,9 +1,15 @@
 import eslint from '@eslint/js';
+import stylelint from '@stylistic/eslint-plugin';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
+    ...stylistic.configs.customize({
+        indent: 4,
+        quotes: 'single',
+        semi: true,
+    }),
     {
         rules: {
             "@typescript-eslint/no-explicit-any": "warn"
