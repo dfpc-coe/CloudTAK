924b894bf1f514838a2a5cd80d44f157d4b2e761
ingalls <nick@ingalls.ca>
2026-05-28 13:00:57 -0600

    Remove unused AtlasIcon

--- api/web/src/workers/atlas.ts
+++ api/web/src/workers/atlas.ts
@@ -9,9 +9,9 @@ import AtlasProfile from './atlas-profile.ts';
 import type { ProfileLocationState } from './atlas-profile.ts';
 import AtlasDatabase from './atlas-database.ts';
 import AtlasConnection from './atlas-connection.ts';
-import AtlasIcons from './atlas-icons.ts';
 import { CloudTAKTransferHandler } from '../base/handler.ts';
 import { db } from '../database.ts';
+import Icon from '../base/icon.ts';
 
 export default class Atlas {
     channel: BroadcastChannel;
@@ -23,7 +23,6 @@ export default class Atlas {
     db = Comlink.proxy(new AtlasDatabase(this));
     conn = Comlink.proxy(new AtlasConnection(this));
     profile = Comlink.proxy(new AtlasProfile(this));
-    icons = Comlink.proxy(new AtlasIcons(this));
 
     constructor() {
         this.channel = new BroadcastChannel('cloudtak');
@@ -75,7 +74,7 @@ export default class Atlas {
 
             this.username = await this.profile.init();
 
-            void this.icons.hydrate()
+            void Icon.hydrate({ token: authToken })
                 .catch((err: unknown) => {
                     console.error('Failed to hydrate iconsets after startup', err);
                 });
