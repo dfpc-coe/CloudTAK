4f7e99461c205eb566eabd357c231004f3fe57d9
ingalls <nick@ingalls.ca>
2026-06-07 08:42:36 -0600

    Login Sessions

--- api/web/src/components/Admin/AdminUserSession.vue
+++ api/web/src/components/Admin/AdminUserSession.vue
@@ -12,162 +12,25 @@
         v-if='isOpen'
         class='col-lg-12 card-body border rounded'
     >
-        <TablerLoading v-if='loading' />
-        <TablerAlert
-            v-else-if='err'
-            :err='err'
-        />
-        <template v-else>
-            <TablerNone
-                v-if='!list.items.length'
-                label='No Login Sessions'
-                :create='false'
-            />
-            <div
-                v-else
-                class='table-responsive'
-            >
-                <table class='table card-table table-hover table-vcenter datatable'>
-                    <TableHeader
-                        v-model:sort='paging.sort'
-                        v-model:order='paging.order'
-                        v-model:header='header'
-                    />
-                    <tbody>
-                        <tr
-                            v-for='session in list.items'
-                            :key='session.id'
-                        >
-                            <template v-for='h in header'>
-                                <template v-if='h.display'>
-                                    <td>
-                                        <div
-                                            v-if='h.name === "created"'
-                                            class='d-flex align-items-center'
-                                        >
-                                            <StatusDot
-                                                :dark='true'
-                                                :status='session.active ? "Success" : "Unknown"'
-                                            />
-                                            <span
-                                                class='mx-2'
-                                                v-text='new Date(session.created).toLocaleString()'
-                                            />
-                                        </div>
-                                        <span
-                                            v-else
-                                            v-text='session[h.name]'
-                                        />
-                                    </td>
-                                </template>
-                            </template>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <TableFooter
-                :limit='paging.limit'
-                :total='list.total'
-                @page='paging.page = $event'
-            />
-        </template>
+        <LoginSessionsList :username='username' />
     </div>
 </template>
 
 <script setup lang='ts'>
-import { ref, watch } from 'vue';
-import { server } from '../../std.ts';
-import TableHeader from '../util/TableHeader.vue';
-import TableFooter from '../util/TableFooter.vue';
-import StatusDot from '../util/StatusDot.vue';
-import {
-    TablerNone,
-    TablerLoading,
-    TablerAlert,
-} from '@tak-ps/vue-tabler';
+import { ref } from 'vue';
+import LoginSessionsList from '../util/LoginSessionsList.vue';
 import {
     IconChevronDown,
     IconChevronRight,
 } from '@tabler/icons-vue';
 
-const props = defineProps<{
+defineProps<{
     username: string;
 }>();
 
-interface Session {
-    id: number;
-    username: string;
-    created: string;
-    ip: string;
-    device_type: string;
-    browser: string;
-    os: string;
-    user_agent: string;
-    active: boolean;
-    [key: string]: unknown;
-}
-
-type Header = { name: string & keyof Session; display: boolean };
-type SessionSort = 'id' | 'username' | 'created' | 'ip' | 'device_type' | 'browser' | 'os' | 'user_agent' | 'enableRLS';
-
 const isOpen = ref(false);
-const loading = ref(false);
-const err = ref<Error | null>(null);
-const list = ref<{ total: number; items: Session[] }>({ total: 0, items: [] });
-const header = ref<Header[]>([
-    { name: 'created', display: true },
-    { name: 'ip', display: true },
-    { name: 'browser', display: true },
-    { name: 'os', display: true },
-    { name: 'device_type', display: true },
-]);
-const paging = ref({
-    limit: 10,
-    page: 0,
-    sort: 'created' as SessionSort,
-    order: 'desc' as 'asc' | 'desc',
-});
 
 function toggle(): void {
     isOpen.value = !isOpen.value;
-}
-
-watch(isOpen, (newState) => {
-    if (newState && !list.value.items.length) {
-        paging.value.page = 0;
-        void fetchSessions();
-    }
-});
-
-watch(paging.value, () => {
-    void fetchSessions();
-});
-
-async function fetchSessions(): Promise<void> {
-    loading.value = true;
-    err.value = null;
-
-    try {
-        const res = await server.GET('/api/user/{:username}/session', {
-            params: {
-                path: {
-                    ':username': props.username
-                },
-                query: {
-                    limit: paging.value.limit,
-                    page: paging.value.page,
-                    sort: paging.value.sort,
-                    order: paging.value.order
-                }
-            }
-        });
-
-        if (res.error) throw new Error(res.error.message);
-        list.value = res.data as { total: number; items: Session[] };
-    } catch (error) {
-        err.value = error instanceof Error ? error : new Error(String(error));
-    }
-
-    loading.value = false;
 }
 </script>
