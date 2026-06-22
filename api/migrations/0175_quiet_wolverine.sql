2edc468b937b28b8185dfb041c1de9ba15c9f951
ingalls <nick@ingalls.ca>
2026-06-22 15:28:18 -0600

    Custom DB Migration

---
+++ api/migrations/0175_quiet_wolverine.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "profile_paging" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"username" text NOT NULL,
+	"created" timestamp with time zone DEFAULT Now() NOT NULL,
+	"updated" timestamp with time zone DEFAULT Now() NOT NULL,
+	"enabled" boolean DEFAULT false NOT NULL,
+	"type" text NOT NULL,
+	"value" text DEFAULT '' NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "profile_paging" ADD CONSTRAINT "profile_paging_username_profile_username_fk" FOREIGN KEY ("username") REFERENCES "public"."profile"("username") ON DELETE no action ON UPDATE no action;
