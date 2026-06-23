6dbd3aab50dfc65ec1dce2a21296470be91153c9
ingalls <nick@ingalls.ca>
2026-06-22 18:25:48 -0600

    Recreate Migrations

--- api/migrations/0176_past_emma_frost.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "profile_paging" ADD COLUMN IF NOT EXISTS "seed" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "profile_paging" ADD COLUMN IF NOT EXISTS "verified" boolean DEFAULT false NOT NULL;
--- api/migrations/0177_foamy_whizzer.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "profile_sessions" DROP COLUMN IF EXISTS "fcm";
