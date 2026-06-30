dc9f0c3dcc74225722d7b91678de63f9bb676715
ingalls <nick@ingalls.ca>
2026-06-30 09:27:57 -0600

    Update Capacitor Config

--- bin/version.js
+++ bin/version.js
@@ -3,13 +3,16 @@ import fs from 'node:fs/promises';
 const pkg_root = JSON.parse(String(await fs.readFile(new URL('../package.json', import.meta.url))));
 const pkg_api = JSON.parse(String(await fs.readFile(new URL('../api/package.json', import.meta.url))));
 const pkg_web = JSON.parse(String(await fs.readFile(new URL('../api/web/package.json', import.meta.url))));
+const capacitor = JSON.parse(String(await fs.readFile(new URL('../capacitor.config.json', import.meta.url))));
 
 console.error('ok version - ' + pkg_root.version);
 
 pkg_api.version = pkg_root.version;
 pkg_web.version = pkg_root.version;
+capacitor.plugins.CapacitorUpdater.version = pkg_root.version;
 
 await fs.writeFile(new URL('../api/package.json', import.meta.url), JSON.stringify(pkg_api, null, 4));
 await fs.writeFile(new URL('../api/web/package.json', import.meta.url), JSON.stringify(pkg_web, null, 4));
+await fs.writeFile(new URL('../capacitor.config.json', import.meta.url), JSON.stringify(capacitor, null, 4));
 
 console.error('ok saved');
