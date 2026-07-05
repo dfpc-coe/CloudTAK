18081c71a94f6f6c352cafea96fdbf51d4718876
ingalls <nick@ingalls.ca>
2026-07-05 15:34:14 -0600

    Add CodeCov

--- api/web/vite.config.ts
+++ api/web/vite.config.ts
@@ -129,6 +129,16 @@ export default defineConfig(({ mode }) => {
                 inline: ['@tak-ps/vue-tabler']
             },
             setupFiles: ['./src/test/setup.ts'],
+            coverage: {
+                provider: 'v8',
+                reporter: ['text', 'lcov'],
+                include: ['src/**'],
+                exclude: [
+                    'src/test/**',
+                    '**/*.spec.ts',
+                    '**/*.d.ts',
+                ],
+            },
         },
     };
 })
