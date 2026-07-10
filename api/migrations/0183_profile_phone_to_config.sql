acc2c78a5744c6de20b64bac17c323e8fc7d92da
ingalls <nick@ingalls.ca>
2026-07-10 12:39:22 -0600

    tak::phone

--- api/migrations/0183_profile_phone_to_config.sql
+++ api/migrations/0183_profile_phone_to_config.sql
@@ -1,5 +1,5 @@
 INSERT INTO "profile_settings" ("username", "key", "value", "updated")
-    SELECT "username", 'tak::phone', to_jsonb("phone")::text, Now()
+    SELECT "username", 'tak::phone', "phone", Now()
         FROM "profile"
         WHERE "phone" IS NOT NULL AND "phone" != ''
 ON CONFLICT ("username", "key") DO UPDATE SET "value" = EXCLUDED."value", "updated" = EXCLUDED."updated";--> statement-breakpoint
