48ddb56436f57a07c6fecd7bd850bbcd44c4736f
ingalls <nick@ingalls.ca>
2026-07-14 17:08:28 -0400

    Add Migration Files

---
+++ api/migrations/0185_core_event_connection.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "core_event" ADD COLUMN "connection" integer;--> statement-breakpoint
+ALTER TABLE "core_event" ADD CONSTRAINT "core_event_connection_connections_id_fk" FOREIGN KEY ("connection") REFERENCES "public"."connections"("id") ON DELETE set null ON UPDATE no action;
