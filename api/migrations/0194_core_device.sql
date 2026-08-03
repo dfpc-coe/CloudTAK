0ff89f43293509e031a86190042dfcf9ce261d36
ingalls <nick@ingalls.ca>
2026-08-03 09:49:17 -0600

    Fix Merge Conflicts

--- api/migrations/0194_core_device.sql
+++ api/migrations/0194_core_device.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "core_device" (
+CREATE TABLE IF NOT EXISTS "core_device" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"created" timestamp with time zone DEFAULT Now() NOT NULL,
 	"updated" timestamp with time zone DEFAULT Now() NOT NULL,
@@ -14,13 +14,29 @@ CREATE TABLE "core_device" (
 	"geometry" GEOMETRY(POINT, 4326)
 );
 --> statement-breakpoint
-CREATE TABLE "core_device_channel" (
+CREATE TABLE IF NOT EXISTS "core_device_channel" (
 	"device" uuid NOT NULL,
 	"channel" bigint NOT NULL,
 	CONSTRAINT "core_device_channel_device_channel_pk" PRIMARY KEY("device","channel")
 );
 --> statement-breakpoint
-ALTER TABLE "core_device" ADD CONSTRAINT "core_device_username_profile_username_fk" FOREIGN KEY ("username") REFERENCES "public"."profile"("username") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "core_device" ADD CONSTRAINT "core_device_connection_connections_id_fk" FOREIGN KEY ("connection") REFERENCES "public"."connections"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "core_device" ADD CONSTRAINT "core_device_event_core_event_id_fk" FOREIGN KEY ("event") REFERENCES "public"."core_event"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "core_device_channel" ADD CONSTRAINT "core_device_channel_device_core_device_id_fk" FOREIGN KEY ("device") REFERENCES "public"."core_device"("id") ON DELETE cascade ON UPDATE no action;
+DO $$ BEGIN
+ ALTER TABLE "core_device" ADD CONSTRAINT "core_device_username_profile_username_fk" FOREIGN KEY ("username") REFERENCES "public"."profile"("username") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "core_device" ADD CONSTRAINT "core_device_connection_connections_id_fk" FOREIGN KEY ("connection") REFERENCES "public"."connections"("id") ON DELETE set null ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "core_device" ADD CONSTRAINT "core_device_event_core_event_id_fk" FOREIGN KEY ("event") REFERENCES "public"."core_event"("id") ON DELETE set null ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "core_device_channel" ADD CONSTRAINT "core_device_channel_device_core_device_id_fk" FOREIGN KEY ("device") REFERENCES "public"."core_device"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
