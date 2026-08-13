38e8b770b31fff4a3144338dfbc1b242abbcf20b
Heather Scott <heather@Mac.localdomain>
2026-08-13 13:55:35 -0700

    remove generate-test-data tool (moved to generate-test-data branch)

--- tasks/events/generate-test-data/src/generate_test_data/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utilities for generating simple spatial test datasets."""
-
-from .generator import make_spatial_file, package_dataset_zip
-
-__all__ = ["make_spatial_file", "package_dataset_zip"]
