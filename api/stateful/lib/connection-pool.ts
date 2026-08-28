7957c9cab107483ebff661e6c3a3c16fe69cd662
ingalls <nick@ingalls.ca>
2026-08-28 11:23:11 -0600

    Remove Orphaned Connection fn

--- api/stateful/lib/connection-pool.ts
+++ api/stateful/lib/connection-pool.ts
@@ -191,20 +191,6 @@ export default class ConnectionPool extends Map<number | string, ConnectionClien
         this.clear();
     }
 
-    async subscription(connection: number | string, name: string): Promise<{
-        name: string;
-        token?: string;
-    }> {
-        const conn = this.get(connection);
-        if (!conn) return { name: name };
-        const sub = await conn.config.subscription(name);
-        if (!sub) return { name: name };
-        return {
-            name: sub.name,
-            token: sub.token || undefined,
-        };
-    }
-
     async activeChannels(connection: number | string, fallbackApi?: TAKAPI): Promise<Set<number>> {
         const conn = this.get(connection);
         if (conn?.channels.size) {
