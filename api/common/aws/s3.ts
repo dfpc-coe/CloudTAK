c34f3d324c72a6ee57cdce59a920c86bf2ef0c9d
ingalls <nick@ingalls.ca>
2026-09-26 10:39:20 -0600

    Minio => Garage

--- api/common/aws/s3.ts
+++ api/common/aws/s3.ts
@@ -19,6 +19,8 @@ export default class S3 {
         if (process.env.AWS_S3_Endpoint) {
             config.endpoint = process.env.AWS_S3_Endpoint;
             config.forcePathStyle = true;
+            config.requestChecksumCalculation = 'WHEN_REQUIRED';
+            config.responseChecksumValidation = 'WHEN_REQUIRED';
 
             if (!process.env.AWS_S3_AccessKeyId || !process.env.AWS_S3_SecretAccessKey) {
                 throw new Error('Cannot use custom S3 Endpoint without providing AWS_S3_AccessKeyId & AWS_S3_SecretAccessKey');
